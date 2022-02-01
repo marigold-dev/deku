4b2dadcea2c653dac30d249fe620db314e6e6a5a
EduardoRFS <theeduardorfs@gmail.com>
2022-02-01 01:13:55 +0000

    ppx_let_binding: supports signatures

--- ppx_let_binding/ppx_let_binding.ml
+++ ppx_let_binding/ppx_let_binding.ml
@@ -16,5 +16,21 @@ let str_let_ident_rule =
   in
   Ppxlib.Context_free.Rule.extension extension
 
+(* on signature [%%let ("ident" : typ)]*)
+let sig_let_ident_rule =
+  let expand ~ctxt name typ =
+    let loc = Expansion_context.Extension.extension_point_loc ctxt in
+    let val_desc = Val.mk ~loc { txt = name; loc } typ in
+    Sig.value val_desc in
+
+  let pattern =
+    Ast_pattern.(single_expr_payload (pexp_constraint (estring __) __)) in
+  let extension =
+    Extension.V3.declare "let" Extension.Context.signature_item pattern expand
+  in
+  Ppxlib.Context_free.Rule.extension extension
+
 let () =
-  Driver.register_transformation ~rules:[str_let_ident_rule] "ppx_let_binding"
+  Driver.register_transformation
+    ~rules:[str_let_ident_rule; sig_let_ident_rule]
+    "ppx_let_binding"
