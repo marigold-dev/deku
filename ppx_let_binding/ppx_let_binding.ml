8c4a754a5d53434ac638c18b812087c8e2135903
EduardoRFS <theeduardorfs@gmail.com>
2022-02-01 01:13:17 +0000

    ppx_let_binding: declare lets with invalid idents

---
+++ ppx_let_binding/ppx_let_binding.ml
@@ -0,0 +1,20 @@
+open Ppxlib
+open Ast_helper
+
+(* on structure [%%let ("ident", value)]*)
+let str_let_ident_rule =
+  let expand ~ctxt name value =
+    let loc = Expansion_context.Extension.extension_point_loc ctxt in
+    let pattern = Pat.var ~loc { txt = name; loc } in
+    [%stri let [%p pattern] = [%e value]] in
+
+  let pattern =
+    Ast_pattern.(single_expr_payload (pexp_tuple (estring __ ^:: __ ^:: nil)))
+  in
+  let extension =
+    Extension.V3.declare "let" Extension.Context.structure_item pattern expand
+  in
+  Ppxlib.Context_free.Rule.extension extension
+
+let () =
+  Driver.register_transformation ~rules:[str_let_ident_rule] "ppx_let_binding"
