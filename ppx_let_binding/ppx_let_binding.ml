6aff299b2e4a7369ad0471830e7f13f3092948ff
EduardoRFS <theeduardorfs@gmail.com>
2022-02-01 01:14:42 +0000

    ppx_let_bindings: add hard coded let extensions
    * let%{await,some,default,ok,assert}

--- ppx_let_binding/ppx_let_binding.ml
+++ ppx_let_binding/ppx_let_binding.ml
@@ -30,7 +30,34 @@ let sig_let_ident_rule =
   in
   Ppxlib.Context_free.Rule.extension extension
 
+(* let%ok p = e1 in e2 *)
+let make_let_binding loc name var value body =
+  let name = { txt = "let." ^ name; loc } in
+  let binding_op = Exp.binding_op name var value loc in
+  Exp.letop ~loc binding_op [] body
+
+let make_let_extension_rule name =
+  let expand ~ctxt expr =
+    let loc = Expansion_context.Extension.extension_point_loc ctxt in
+    match expr with
+    | [%expr
+        let [%p? var] = [%e? value] in
+        [%e? body]] ->
+      make_let_binding loc name var value body
+    | _ ->
+      Location.raise_errorf ~loc
+        "Extension %%%s can only be used with a let, e.g. let%%%s a = b in ..."
+        name name in
+  let extension =
+    Extension.V3.declare name Extension.Context.expression
+      Ast_pattern.(single_expr_payload __)
+      expand in
+  Ppxlib.Context_free.Rule.extension extension
+
+let let_extension_rules =
+  List.map make_let_extension_rule ["await"; "some"; "default"; "ok"; "assert"]
+
 let () =
   Driver.register_transformation
-    ~rules:[str_let_ident_rule; sig_let_ident_rule]
+    ~rules:(str_let_ident_rule :: sig_let_ident_rule :: let_extension_rules)
     "ppx_let_binding"
