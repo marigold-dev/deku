302c83be843b69d8f6b7251bf753a12ab4defd8c
EduardoRFS <theeduardorfs@gmail.com>
2022-02-10 16:59:10 +0000

    tezos: make a Michelson module
    - before this was inside of Pack

--- src/tezos/tezos.mli
+++ src/tezos/tezos.mli
@@ -1,6 +1,7 @@
 module Contract_hash = Contract_hash
 module Address = Address
 module Ticket_id = Ticket_id
+module Michelson = Michelson
 module Pack = Pack
 module Operation_hash = Operation_hash
 module Deku = Deku
