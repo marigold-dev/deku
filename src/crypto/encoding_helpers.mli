88c85456e869e3e223b59c16f80179535502071c
EduardoRFS <theeduardorfs@gmail.com>
2022-02-10 16:41:53 +0000

    crypto: expose Signature.to_raw
    * a signed operation is made by concatening the raw signature

--- src/crypto/encoding_helpers.mli
+++ src/crypto/encoding_helpers.mli
@@ -21,6 +21,11 @@ module Make_b58 : functor
    end)
   -> sig
   open H
+
+  (* TODO: should this be exposed? *)
+  val to_raw : t -> string
+  (** [to_raw t] serialize t to a string containing raw bytes with no prefix *)
+
   val to_string : t -> string
     [@@ocaml.doc " [to_string t] encodeds t as a b58 string "]
   val of_string : string -> t option
