123054e896a540eb6973336d1e240327c89ffd01
EduardoRFS <theeduardorfs@gmail.com>
2022-02-23 14:16:42 -0300

    tezos_rpc: add Listen_to_chain_heads

--- src/tezos_rpc/tezos_rpc.mli
+++ src/tezos_rpc/tezos_rpc.mli
@@ -1,3 +1,6 @@
 module Error = Error
 module Inject_operations = Inject_operations
 module Fetch_block_operations = Fetch_block_operations
+
+module Block_header = Block_header
+module Listen_to_chain_heads = Listen_to_chain_heads
