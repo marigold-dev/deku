26bd505034a5837b6f2b7f95d98509e46196dd4a
EduardoRFS <theeduardorfs@gmail.com>
2022-02-23 16:37:11 -0300

    tezos_rpc: add Fetch_block_header

--- src/tezos_rpc/tezos_rpc.mli
+++ src/tezos_rpc/tezos_rpc.mli
@@ -1,6 +1,7 @@
 module Error = Error
 module Inject_operations = Inject_operations
 module Fetch_block_operations = Fetch_block_operations
+module Fetch_block_header = Fetch_block_header
 
 module Block_header = Block_header
 module Listen_to_chain_heads = Listen_to_chain_heads
