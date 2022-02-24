5e5ee68ab6f6eaee0a02a75ef46c46459f350eda
EduardoRFS <theeduardorfs@gmail.com>
2022-02-24 18:34:16 -0300

    tests: add manual tests for Tezos_rpc
    - this tests Fetch_block_operations

---
+++ tests/test_tezos_rpc.ml
@@ -0,0 +1,36 @@
+open Helpers
+open Crypto
+open Tezos
+open Tezos_rpc
+
+let failwith_err err =
+  let failwith s = Format.kasprintf failwith s in
+  match err with
+  | Tezos_rpc.Error.Json_error _ -> failwith "Json_error"
+  | Piaf_body err -> failwith "Piaf_body(%a)" Piaf.Error.pp_hum err
+  | Piaf_request err -> failwith "Piaf_request(%a)" Piaf.Error.pp_hum err
+  | Response_of_yojson err -> failwith "Response_of_yojson(%s)" err
+
+let node_uri = Uri.of_string "http://localhost:8732"
+let secret =
+  Secret.of_string "edsk4RbgwutwsEdVNuJsE5JDsxeJ6qFcG8F5rKFGnj5finT6FV46sd"
+  |> Option.get
+let key = Key.of_secret secret
+let key_hash = Key_hash.of_key key
+
+let fetch_block_operations ~block_hash () =
+  let open Fetch_block_operations in
+  let%await result = execute ~node_uri ~chain:None ~block_hash in
+  (match result with
+  | Ok operations ->
+    List.iter
+      (fun operation ->
+        Format.printf "operation.hash: %s\n%!"
+          (Operation_hash.to_string operation.hash))
+      operations
+  | Error err -> failwith_err err);
+  Lwt.return_unit
+
+let tests = [ (* fetch_block_operations ~block_hash:None *) ]
+
+let () = Lwt_list.iter_s (fun test -> test ()) tests |> Lwt_main.run
