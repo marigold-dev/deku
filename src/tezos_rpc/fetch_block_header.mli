262997b6d8f9ab6a4582c5bf7c38d31333953ad5
Eduardo Rafael <theeduardorfs@gmail.com>
2022-02-25 15:12:26 +0000

    tezos_rpc: add Fetch_block_header (#448)

---
+++ src/tezos_rpc/fetch_block_header.mli
@@ -0,0 +1,8 @@
+open Tezos
+
+type response = Block_header.t
+val execute :
+  node_uri:Uri.t ->
+  chain:Chain_id.t option ->
+  block_hash:Block_hash.t option ->
+  (response, Error.error) result Lwt.t
