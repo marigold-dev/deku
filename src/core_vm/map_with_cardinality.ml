f7bcfc219a2598c4ae350d68a7ebb9c6bc9167b6
EduardoRFS <theeduardorfs@gmail.com>
2022-02-28 14:01:52 -0300

    core_vm: define Map_with_cardinality

---
+++ src/core_vm/map_with_cardinality.ml
@@ -0,0 +1,43 @@
+module type S = sig
+  type key
+
+  type 'a t
+
+  val empty : 'a t
+
+  (* O(log n) *)
+  val add : key -> 'a -> 'a t -> 'a t
+
+  (* O(1) *)
+  val cardinal : 'a t -> int
+
+  (* O(log n) *)
+  val find : key -> 'a t -> 'a option
+end
+
+(* Map.S + O(1) cardinal*)
+module Make (K : Map.OrderedType) : S with type key = K.t = struct
+  type key = K.t
+
+  module Map = Map.Make (K)
+  type 'a t = {
+    cardinality : int;
+    values : 'a Map.t;
+  }
+
+  let empty = { cardinality = 0; values = Map.empty }
+
+  let add key value t =
+    let { cardinality; values } = t in
+    let cardinality =
+      (* TODO: this could be fused with Ident.Map.add by using Ident.Map.update *)
+      if Map.mem key values then
+        cardinality
+      else
+        cardinality + 1 in
+    let values = Map.add key value values in
+    { cardinality; values }
+
+  let cardinal t = t.cardinality
+  let find key t = Map.find_opt key t.values
+end
---
+++ src/core_vm/map_with_cardinality.mli
@@ -0,0 +1,18 @@
+module type S = sig
+  type key
+
+  type 'a t
+
+  val empty : 'a t
+
+  (* O(log n) *)
+  val add : key -> 'a -> 'a t -> 'a t
+
+  (* O(1) *)
+  val cardinal : 'a t -> int
+
+  (* O(log n) *)
+  val find : key -> 'a t -> 'a option
+end
+
+module Make (K : Map.OrderedType) : S with type key = K.t
