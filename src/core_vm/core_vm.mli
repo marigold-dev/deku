adb1a3fa75b67897ad50b6201dcd54d864a69e21
EduardoRFS <theeduardorfs@gmail.com>
2022-02-28 14:02:28 -0300

    core_vm: define external interface

---
+++ src/core_vm/core_vm.mli
@@ -0,0 +1,31 @@
+module Ast : module type of Ast
+module Gas : module type of Gas
+
+(* ir *)
+type script
+type value
+
+(* compiler *)
+type compile_error = (* user program bugs *)
+  | Undefined_variable
+
+val compile : Gas.t -> Ast.script -> (script, compile_error) result
+val compile_value : Gas.t -> Ast.value -> (value, compile_error) result
+
+(* interpreter *)
+type execution_error =
+  (* interpreter bugs *)
+  | Undefined_variable
+  | Over_applied_primitives
+  (* user program bugs *)
+  | Value_is_not_pair
+  | Value_is_not_int64
+  | Value_is_not_function
+  | Value_is_not_zero
+
+type script_result = {
+  storage : value;
+  operations : unit;
+}
+val execute :
+  Gas.t -> arg:value -> script -> (script_result, execution_error) result
