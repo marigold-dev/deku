646bffaa92b7e2d650fb34621fa4df8d27738363
gsebil08 <gauthier@marigold.dev>
2022-03-08 09:01:10 +0000

    networking(switchFromHttpToUdp): f**k dune

--- src/networking/client.ml
+++ src/networking/client.ml
@@ -1,9 +1,8 @@
-open Lwt_unix
-
 let create_client port =
-  let client_socket = Lwt_unix.socket PF_INET SOCK_DGRAM 0 in
-  let client_addr = Lwt_unix.ADDR_INET (Unix.inet_addr_loopback, port) in
-  let%lwt () = Lwt_unix.bind client_socket client_addr in
+  let open Lwt_unix in
+  let client_socket = socket PF_INET SOCK_DGRAM 0 in
+  let client_addr = ADDR_INET (Unix.inet_addr_loopback, port) in
+  let%lwt () = bind client_socket client_addr in
   Lwt.return client_socket
 
 let send_message = failwith "undefined"
