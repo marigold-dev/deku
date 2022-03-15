99b7d545998806f5907be140311caf423340fd62
Daniel Hines <d4hines@gmail.com>
2022-03-15 16:08:02 -0400

    chore: move start.sh into sandbox.sh as subcommand

--- sandbox.sh
+++ sandbox.sh
@@ -7,8 +7,15 @@ data_directory="data"
 LD_LIBRARY_PATH=$(esy x sh -c 'echo $LD_LIBRARY_PATH')
 export LD_LIBRARY_PATH
 
-PATH=$(esy x sh -c 'echo $PATH')
-export PATH
+DEKU_CLI=$(esy x which deku-cli)
+deku_cli() {
+  eval $DEKU_CLI '"$@"'
+}
+
+DEKU_NODE=$(esy x which deku-node)
+deku_node() {
+  eval $DEKU_NODE '"$@"'
+}
 
 tezos-client() {
   docker exec -it deku_flextesa tezos-client "$@"
@@ -176,20 +183,20 @@ start_deku_cluster() {
   SERVERS=()
   echo "Starting nodes."
   for i in ${VALIDATORS[@]}; do
-    deku-node "$data_directory/$i" &
+    deku_node "$data_directory/$i" &
     SERVERS+=($!)
   done
 
   sleep 1
 
   echo "Producing a block"
-  HASH=$(deku-cli produce-block "$data_directory/0" | awk '{ print $2 }')
+  HASH=$(deku_cli produce-block "$data_directory/0" | awk '{ print $2 }')
 
   sleep 0.1
 
   echo "Signing"
   for i in ${VALIDATORS[@]}; do
-    deku-cli sign-block "$data_directory/$i" $HASH
+    deku_cli sign-block "$data_directory/$i" $HASH
   done
 
   for PID in ${SERVERS[@]}; do
