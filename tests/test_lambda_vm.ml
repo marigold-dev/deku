eed6f823ca831e6fd03170c62e3e6aaef9e43fc5
Andre Popovitch <andre@popovit.ch>
2022-03-19 05:24:25 +0000

    doesn't work, but it will soon

--- tests/test_lambda_vm.ml
+++ tests/test_lambda_vm.ml
@@ -15,7 +15,7 @@ let expect_uint64 =
     check
       (testable
          (fun ppf value -> Fmt.pf ppf "%s" (Stdint.Uint64.to_string value))
-         (fun a b -> Stdint.Uint64.compare a b == 0)))
+         (fun a b -> Stdint.Uint64.compare a b = 0)))
 
 let expect_int64 =
   Alcotest.(
@@ -28,7 +28,7 @@ let expect_uint128 =
     check
       (testable
          (fun ppf value -> Fmt.pf ppf "%s" (Stdint.Uint128.to_string value))
-         (fun a b -> Stdint.Uint128.compare a b == 0)))
+         (fun a b -> Stdint.Uint128.compare a b = 0)))
 
 let expect_int64_pair =
   Alcotest.(
@@ -77,6 +77,19 @@ let lam5 v w x y z f =
       Lam
         (w, Lam (x, Lam (y, Lam (z, f (Var v) (Var w) (Var x) (Var y) (Var z)))))
     )
+let lam6 u v w x y z f =
+  Lam
+    ( u,
+      Lam
+        ( v,
+          Lam
+            ( w,
+              Lam
+                ( x,
+                  Lam
+                    ( y,
+                      Lam (z, f (Var u) (Var v) (Var w) (Var x) (Var y) (Var z))
+                    ) ) ) ) )
 let script x f = { param = x; code = f (Var x) }
 let pair x y : Ast.expr = Ast.Pair { first = x; second = y }
 let app f ls =
@@ -84,8 +97,7 @@ let app f ls =
 let let_in letins x =
   let let_in (var', let') in' = App { funct = Lam (var', in'); arg = let' } in
   List.fold_right let_in letins x
-let ( let* ) (var', let') in' =
-  App { funct = Lam (var', in' (Var var')); arg = let' }
+
 let let' var ~eq ~in' = App { funct = Lam (var, in' (Var var)); arg = eq }
 let fst x = Ast.(App { funct = Prim Fst; arg = x })
 let snd x = Ast.(App { funct = Prim Snd; arg = x })
@@ -98,6 +110,15 @@ let ( / ) x y = app (Prim Div) [x; y]
 
 (* Technically the if is unecessary *)
 let ( == ) x y = if' (x - y) ~then':(Const 0L) ~else':(Const 1L)
+let ( <> ) x y = if' (x - y) ~then':(Const 1L) ~else':(Const 0L)
+let ( && ) x y =
+  if' (app (Prim Land) [x; y]) ~then':(Const 1L) ~else':(Const 0L)
+let ( let* ) (var', let') in' =
+  App { funct = Lam (var', in' (Var var')); arg = let' }
+let ( let*& ) (var', var'', let') in' =
+  let* v1 = (var', fst let') in
+  let* v2 = (var'', snd let') in
+  in' (v1, v2)
 
 let pair_wrapper f =
   script "y" (fun y ->
@@ -192,6 +213,15 @@ let monadic_let =
               let* res = ("res", y + Const 10L) in
               res + Const 5L))
        ~parameter:(Int64 33L) ~expectation:(Ir.V_int64 48L))
+let monadic_bilet =
+  make_test "monadic_let"
+    (expect_script_output
+       ~script:
+         (pair_wrapper (fun y ->
+              let*& v1, v2 = ("v1", "v2", y) in
+              v1 + v2))
+       ~parameter:(Pair (Int64 33L, Int64 11L))
+       ~expectation:(Ir.V_int64 44L))
 
 type stdlib = {
   id : Ast.expr -> Ast.expr;
@@ -200,12 +230,15 @@ type stdlib = {
   add_with_carry : Ast.expr -> Ast.expr -> Ast.expr;
   uncurry : (Ast.expr -> Ast.expr -> Ast.expr) -> Ast.expr -> Ast.expr;
   to_bignat : Ast.expr -> Ast.expr;
+  add_bignat : Ast.expr -> Ast.expr -> Ast.expr;
   zcomb : Ast.expr -> Ast.expr;
 }
 let stdlib x =
   let mklam name x = app (Var name) [x] in
   let mklam2 name x y = app (Var name) [x; y] in
   let mklam3 name x y z = app (Var name) [x; y; z] in
+  let _mklam4 name w x y z = app (Var name) [w; x; y; z] in
+  let mklam5 name v w x y z = app (Var name) [v; w; x; y; z] in
   let ( lsl ) x y = app (Prim Lsl) [x; Const (Int64.of_int y)] in
   let ( lsr ) x y = app (Prim Lsr) [x; Const (Int64.of_int y)] in
   let* _id = ("id", lam "x" (fun x -> x)) in
@@ -245,7 +278,7 @@ let stdlib x =
       ("uncurry", lam2 "f" "tup" (fun f tup -> app f [fst tup; snd tup])) in
     let uncurry = mklam2 "uncurry" in*)
   let uncurry f x = f (fst x) (snd x) in
-  let* _to_bignat = ("to_bignat", lam "x" (fun x -> pair (Const 0L) x)) in
+  let* _to_bignat = ("to_bignat", lam "x" (fun x -> pair (Const 1L) x)) in
   let to_bignat = mklam "to_bignat" in
   let* _triple_add_with_carry =
     ( "triple_add_with_carry",
@@ -257,17 +290,56 @@ let stdlib x =
   let* _add_bignat =
     ( "add_bignat",
       lam2 "x" "y" (fun x y ->
-          let* x_limb = ("x_limb", fst x) in
-          let* y_limb = ("y_limb", fst y) in
-          let* x_body = ("x_body", snd x) in
-          let* y_body = ("y_body", snd y) in
-          let* add_bignat =
-            ( "add_bignat",
-              lam5 "recurse" "x_limb" "y_limb" "x_body" "y_body"
-                (fun recurse x_limb y_limb x_body y_body ->
-                  failwith "also undone") ) in
-          failwith "undone") ) in
-  x { id; lower_bits; higher_bits; add_with_carry; uncurry; to_bignat; zcomb }
+          let*& x_limb, xs = ("x_limb", "xs", x) in
+          let*& y_limb, ys = ("y_limb", "ys", y) in
+let* add_bignat =
+  ( "add_bignat",
+    lam6 "recurse" "x_limb" "xs" "y_limb" "ys" "carry"
+      (fun _recurse x_limb xs y_limb ys carry ->
+        let add_bignat = mklam5 "recurse" in
+        if'
+          (x_limb <> Const 1L && y_limb <> Const 1L)
+          ~then':
+            (let*& x, xs = ("x", "xs", xs) in
+              let*& y, ys = ("y", "ys", ys) in
+              let*& l, carry =
+                ("l", "carry", triple_add_with_carry x y carry) in
+              let*& limb, ls =
+                ( "limb",
+                  "ls",
+                  add_bignat (x_limb - Const 1L) xs (y_limb - Const 1L)
+                    ys carry ) in
+              pair (limb + Const 1L) (pair l ls))
+          ~else':
+            (if'
+                (x_limb <> Const 1L && y_limb == Const 1L)
+                ~then':
+                  (let*& x, xs = ("x", "xs", xs) in
+                  let*& l, carry =
+                    ("l", "carry", add_with_carry x carry) in
+                  let*& limb, ls =
+                    ( "limb",
+                      "ls",
+                      (* this line is probably wrong *)
+                      add_bignat (x_limb - Const 1L) xs (Const 0L)
+                        (Const 0L) carry ) in
+                  pair (limb + Const 1L) (pair l ls))
+                ~else':(pair (Const 1L) (Const 0L)))) ) in
+          let* _add_bignat = ("add_bignat", zcomb add_bignat) in
+          let add_bignat = mklam5 "add_bignat" in
+          add_bignat x_limb xs y_limb ys (Const 0L)) ) in
+  let add_bignat = mklam2 "add_bignat" in
+  x
+    {
+      id;
+      lower_bits;
+      higher_bits;
+      add_with_carry;
+      uncurry;
+      to_bignat;
+      zcomb;
+      add_bignat;
+    }
 
 let test_stdlib =
   let test_stdlib_function description f ~parameter ~expectation =
@@ -294,6 +366,22 @@ let test_stdlib =
                              (pair (Const 0L) (Const 0L))))
                       [y])))
          ~parameter ~expectation) in
+  let numlist_to_bignat_value pair i64 x =
+    let rec makepairs = function
+      | [x] -> i64 x
+      | x :: xs -> pair (i64 x, makepairs xs)
+      | _ ->
+        failwith "`numlist_to_bignat_ast_value` caller passed an empty list"
+    in
+    pair (i64 (List.length x |> Int64.of_int), makepairs x) in
+  let _numlist_to_bignat_ast_value =
+    numlist_to_bignat_value
+      (fun (x, y) -> Ast.Pair (x, y))
+      (fun i -> Ast.Int64 i) in
+  let _numlist_to_bignat_ir_value =
+    numlist_to_bignat_value
+      (fun (x, y) -> Ir.V_pair { first = x; second = y })
+      (fun i -> Ir.V_int64 i) in
   [
     test_stdlib_function "id"
       (fun { id; _ } -> id)
@@ -317,16 +405,16 @@ let test_stdlib =
       ~parameter:(Ast.Pair (Int64 33L, Int64 11L))
       ~expectation:
         (Ir.V_pair { first = Ir.V_int64 44L; second = Ir.V_int64 0L });
-    test_stdlib_function2 "add_with_carry maxint 0"
+    test_stdlib_function2 "add_with_carry maxint64 0"
       (fun { add_with_carry; _ } -> add_with_carry)
       ~parameter:(Ast.Pair (Int64 (-1L), Int64 0L))
       ~expectation:
         (Ir.V_pair { first = Ir.V_int64 (-1L); second = Ir.V_int64 0L });
-    test_stdlib_function2 "add_with_carry maxint 1"
+    test_stdlib_function2 "add_with_carry maxint64 1"
       (fun { add_with_carry; _ } -> add_with_carry)
       ~parameter:(Ast.Pair (Int64 (-1L), Int64 1L))
       ~expectation:(Ir.V_pair { first = Ir.V_int64 0L; second = Ir.V_int64 1L });
-    test_stdlib_function2 "add_with_carry maxint maxint"
+    test_stdlib_function2 "add_with_carry maxint64 maxint64"
       (fun { add_with_carry; _ } -> add_with_carry)
       ~parameter:(Ast.Pair (Int64 (-1L), Int64 (-1L)))
       ~expectation:
@@ -335,7 +423,7 @@ let test_stdlib =
       (fun { to_bignat; _ } -> to_bignat)
       ~parameter:(Ast.Int64 44L)
       ~expectation:
-        (Ir.V_pair { first = Ir.V_int64 0L; second = Ir.V_int64 44L });
+        (Ir.V_pair { first = Ir.V_int64 1L; second = Ir.V_int64 44L });
     test_stdlib_function "uncurry & eq"
       (fun { uncurry; _ } -> uncurry ( == ))
       ~parameter:(Ast.Pair (Int64 33L, Int64 44L))
@@ -355,43 +443,101 @@ let test_stdlib =
                               if' (n == Const 0L) ~then':(Const 1L)
                                 ~else':(n * app recurse [n - Const 1L]))))
                       [param])))
-         ~parameter:(Ast.Int64 10L) ~expectation:(Ir.V_int64 3628800L));
+         ~parameter:(Ast.Int64 10L) ~expectation:(Ir.V_int64 3628800L))
+    (*make_test "add_bignat 1+15"
+        (expect_script_output
+           ~script:
+             (pair_wrapper (fun param ->
+                  stdlib (fun { add_bignat; uncurry; _ } ->
+                      uncurry add_bignat param)))
+           ~parameter:
+             (Ast.Pair
+                ( numlist_to_bignat_ast_value [10L],
+                  numlist_to_bignat_ast_value [5L] ))
+           ~expectation:(numlist_to_bignat_ir_value [15L]));
+      make_test "add_bignat maxint64+1"
+        (expect_script_output
+           ~script:
+             (pair_wrapper (fun param ->
+                  stdlib (fun { add_bignat; uncurry; _ } ->
+                      uncurry add_bignat param)))
+           ~parameter:
+             (Ast.Pair
+                ( numlist_to_bignat_ast_value [-1L],
+                  numlist_to_bignat_ast_value [1L] ))
+           ~expectation:
+             (V_pair
+                {
+                  first = V_int64 2L;
+                  second = V_pair { first = V_int64 0L; second = V_int64 1L };
+                }));
+      make_test "add_bignat maxint64+maxint64"
+        (expect_script_output
+           ~script:
+             (pair_wrapper (fun param ->
+                  stdlib (fun { add_bignat; uncurry; _ } ->
+                      uncurry add_bignat param)))
+           ~parameter:
+             (Ast.Pair
+                ( numlist_to_bignat_ast_value [-1L],
+                  numlist_to_bignat_ast_value [-1L] ))
+           ~expectation:(numlist_to_bignat_ir_value [-1L; 1L]));
+      make_test "add_bignat maxint64^3  maxint64^3"
+        (expect_script_output
+           ~script:
+             (pair_wrapper (fun param ->
+                  stdlib (fun { add_bignat; uncurry; _ } ->
+                      uncurry add_bignat param)))
+           ~parameter:
+             (Ast.Pair
+                ( numlist_to_bignat_ast_value [-2L; -1L; -1L; -1L],
+                  numlist_to_bignat_ast_value [-1L; -1L; -1L] ))
+           ~expectation:(numlist_to_bignat_ir_value [-1L; 1L]))*);
   ]
 
 let add_with_carry =
-  let expect_int64_list =
+  let expect_bignum_list =
     Alcotest.(
       check
         (testable
            (fun ppf value ->
              Fmt.pf ppf "%s" ([%derive.show: Int64.t list] value))
            [%derive.eq: Int64.t list])) in
+  let expect_bignum_list_with_size =
+    Alcotest.(
+      check
+        (testable
+           (fun ppf value ->
+             Fmt.pf ppf "%s" ([%derive.show: int * Int64.t list] value))
+           [%derive.eq: int * Int64.t list])) in
   [
     make_test "Math.add_with_carry 33 11 (no overflow)" (fun description ->
         expect_int64_pair description (44L, 0L) (Math.add_with_carry 33L 11L));
     (* Recall that -1L is treated as max_int64 *)
-    make_test "Math.add_with_carry maxint 0 (no overflow)" (fun description ->
+    make_test "Math.add_with_carry maxint64 0 (no overflow)" (fun description ->
         expect_int64_pair description (-1L, 0L) (Math.add_with_carry (-1L) 0L));
-    make_test "Math.add_with_carry maxint 1 (yes overflow)" (fun description ->
+    make_test "Math.add_with_carry maxint64 1 (yes overflow)"
+      (fun description ->
         expect_int64_pair description (0L, 1L) (Math.add_with_carry (-1L) 1L));
-    make_test "Math.add_with_carry maxint maxint (yes overflow)"
+    make_test "Math.add_with_carry maxint64 maxint64 (yes overflow)"
       (fun description ->
         expect_int64_pair description (-2L, 1L)
           (Math.add_with_carry (-1L) (-1L)));
-    make_test "Math.triple_add_with_carry maxint maxint maxint (yes overflow)"
+    make_test
+      "Math.triple_add_with_carry maxint64 maxint64 maxint64 (yes overflow)"
       (fun description ->
         expect_int64_pair description (-3L, 2L)
           (Math.triple_add_with_carry (-1L) (-1L) (-1L)));
     (* Should never happen, but might as well test it anyway *)
-    make_test "Math.add_bignum empty" (fun description ->
-        expect_int64_list description [] (Math.add_bignum [] []));
-    make_test "Math.add_bignum empty zeros" (fun description ->
-        expect_int64_list description [0L] (Math.add_bignum [0L] [0L]));
-    make_test "Math.add_bignum maxint maxint" (fun description ->
-        expect_int64_list description [-2L; 1L] (Math.add_bignum [-1L] [-1L]));
-    make_test "Math.add_bignum maxint^2 1" (fun description ->
-        expect_int64_list description [0L; 0L; 1L]
-          (Math.add_bignum [1L] [-1L; -1L]));
+    make_test "Math.add_bignat empty" (fun description ->
+        expect_bignum_list description [] (Math.add_bignat [] []));
+    make_test "Math.add_bignat zero zero" (fun description ->
+        expect_bignum_list description [0L] (Math.add_bignat [0L] [0L]));
+    make_test "Math.add_bignat maxint64 maxint64" (fun description ->
+        expect_bignum_list description [-2L; 1L] (Math.add_bignat [-1L] [-1L]));
+    make_test "Math.add_bignat maxint64^2 1" (fun description ->
+        expect_bignum_list description [0L; 0L; 1L]
+          (Math.add_bignat [1L] [-1L; -1L]));
     (* result of this one is:
        0111111111111111111111111111111111111111111111111111111111111111  = -2L
        1111111111111111111111111111111111111111111111111111111111111111  = -1L
@@ -399,9 +545,21 @@ let add_with_carry =
        1000000000000000000000000000000000000000000000000000000000000000  = 1L
        (little endian)
     *)
-    make_test "Math.add_bignum maxint^3  maxint^3" (fun description ->
-        expect_int64_list description [-2L; -1L; -1L; 1L]
-          (Math.add_bignum [-1L; -1L; -1L] [-1L; -1L; -1L]));
+    make_test "Math.add_bignat maxint64^3  maxint64^3" (fun description ->
+        expect_bignum_list description [-2L; -1L; -1L; 1L]
+          (Math.add_bignat [-1L; -1L; -1L] [-1L; -1L; -1L]));
+    make_test "Math.add_bignat' zero zero" (fun description ->
+        expect_bignum_list_with_size description
+          (1, [0L])
+          (Math.add_bignat' (1, [0L]) (1, [0L])));
+    make_test "Math.add_bignat' 1 maxint64^3" (fun description ->
+        expect_bignum_list_with_size description
+          (4, [0L; 0L; 0L; 1L])
+          (Math.add_bignat' (1, [1L]) (3, [-1L; -1L; -1L])));
+    make_test "Math.add_bignat' maxint64^3  maxint64^3" (fun description ->
+        expect_bignum_list_with_size description
+          (4, [-2L; -1L; -1L; 1L])
+          (Math.add_bignat' (3, [-1L; -1L; -1L]) (3, [-1L; -1L; -1L])));
   ]
 
 let () =
