8dafdf5a0488ddc5ee893a491276f4f9359681a0
EduardoRFS <theeduardorfs@gmail.com>
2022-03-18 21:23:57 -0300

    tezos_interop: use new run_entrypoint.js
    - was also refactored

--- src/tezos_interop/run_entrypoint.mli
+++ src/tezos_interop/run_entrypoint.mli
@@ -1,19 +1,26 @@
 open Crypto
 open Tezos
 
-type output =
-  | Applied     of { hash : string }
-  | Failed      of { hash : string }
-  | Skipped     of { hash : string }
-  | Backtracked of { hash : string }
-  | Unknown     of { hash : string }
-  | Error       of string
+module Process : sig
+  type t
+  val spawn : unit -> t
+end
+module Response : sig
+  type transaction =
+    | Applied     of { hash : string }
+    | Failed      of { hash : string }
+    | Skipped     of { hash : string }
+    | Backtracked of { hash : string }
+    | Unknown     of { hash : string }
+    | Error       of string
+end
 
 val run :
+  Process.t ->
   rpc_node:Uri.t ->
   secret:Secret.t ->
   required_confirmations:int ->
   destination:Address.t ->
   entrypoint:string ->
   payload:Yojson.Safe.t ->
-  output Lwt.t
+  Response.transaction Lwt.t
