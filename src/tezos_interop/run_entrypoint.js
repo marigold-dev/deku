c3da00a1aceb27c9179118b80556e9503e687321
EduardoRFS <theeduardorfs@gmail.com>
2022-03-18 21:23:16 -0300

    tezos_interop: make run_entrypoint long lived
    - now you can make many transactions from a single process

--- src/tezos_interop/run_entrypoint.js
+++ src/tezos_interop/run_entrypoint.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const fs = require("fs");
 const { TezosToolkit } = require("@taquito/taquito");
 const { InMemorySigner } = require("@taquito/signer");
+const { pipeline, Transform } = require("stream");
 
 /**
- * @typedef Input
+ * @typedef TransactionRequest
  * @type {object}
  * @property {string} rpc_node
  * @property {string} secret
@@ -13,51 +13,132 @@ const { InMemorySigner } = require("@taquito/signer");
  * @property {string} destination
  * @property {string} entrypoint
  * @property {object} payload
- */
-/** @returns {Input} */
-const input = () => JSON.parse(fs.readFileSync(process.stdin.fd));
 
-/**
- * @typedef OutputFinished
+ * @typedef RequestHeader
+ * @type {object}
+ * @property {number} id
+
+ * @typedef Request
+ * @type {RequestHeader & TransactionRequest}
+
+ * @typedef ResponseHeader
+ * @type {object}
+ * @property {number} id
+
+ * @typedef TransactionResponseSuccess
  * @type {object}
  * @property {"applied" | "failed" | "skipped" | "backtracked" | "unknown"} status
  * @property {string} hash
- */
-/**
- * @typedef OutputError
+
+ * @typedef TransactionResponseError
  * @type {object}
  * @property {"error"} status
  * @property {string} error
+
+ * @typedef TransactionResponse
+ * @type {TransactionResponseSuccess | TransactionResponseError}
+
+ * @typedef Response
+ * @type {ResponseHeader & TransactionResponse}
  */
-/** @param {OutputFinished | OutputError} data */
-const output = (data) =>
-  fs.writeFileSync(process.stdout.fd, JSON.stringify(data, null, 2));
 
-const finished = (status, hash) => output({ status, hash });
-const error = (error) =>
-  output({ status: "error", error: JSON.stringify(error) });
+const failure = (err) => {
+  console.error(err);
+  process.exit(1);
+};
 
-(async () => {
-  const { rpc_node, secret, confirmation, destination, entrypoint, payload } =
-    input();
+/** @param {Response} message */
+const write = (message) => {
+  const callback = (err) => {
+    if (err) {
+      failure(err);
+    }
+  };
+  const messageString = JSON.stringify(message);
+  process.stdout.write(messageString + "\n", callback);
+};
+
+const read = (callback) => {
+  let buf = "";
+  /* WARNING: inputs are separated by new lines,
+    that means each input must be contained in a single line
+
+    similar to http://ndjson.org/
+  */
+  const parseStream = Transform({
+    objectMode: true,
+    transform(chunk, encoding, done) {
+      if (encoding !== "utf8") {
+        throw TypeError("encoding expected to be utf8");
+      }
+      buf = buf + chunk;
+
+      const newline = buf.indexOf("\n");
+      if (newline === -1) {
+        done();
+        return;
+      }
+
+      const message = buf.slice(0, newline);
+      // skips the newline
+      buf = buf.slice(newline + 1);
+
+      try {
+        const json = JSON.parse(message);
+        this.push(json);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    },
+  });
+  const errorHandler = (err = "close") => failure(err);
+
+  pipeline(process.stdin, parseStream, errorHandler).on("data", callback);
+};
+
+// This is also defined on the other JS scripts
+const config = {
+  shouldObservableSubscriptionRetry: true,
+  streamerPollingIntervalMilliseconds: 1000,
+  confirmationPollingIntervalSecond: 1,
+};
+
+/** @param {Request} request */
+const onRequest = async (request) => {
+  const {
+    id,
+    rpc_node,
+    secret,
+    confirmation,
+    destination,
+    entrypoint,
+    payload,
+  } = request;
+
   const args = Object.entries(payload)
     .sort(([a], [b]) => a.localeCompare(b))
     .map(([_, value]) => value);
   const Tezos = new TezosToolkit(rpc_node);
   const signer = await InMemorySigner.fromSecretKey(secret);
-  // This is also defined on the other JS scripts
-  Tezos.setProvider({
-    signer,
-    config: {
-      shouldObservableSubscriptionRetry: true,
-      streamerPollingIntervalMilliseconds: 1000,
-      confirmationPollingIntervalSecond: 1,
-    },
-  });
+  Tezos.setProvider({ signer, config });
 
   const contract = await Tezos.contract.at(destination);
   const operation = await contract.methods[entrypoint](...args).send();
   await operation.confirmation(confirmation);
 
-  finished(operation.status, operation.hash);
-})().catch(error);
+  const status = operation.status;
+  const hash = operation.hash;
+  write({ id, status, hash });
+};
+
+read((request) =>
+  onRequest(request)
+    .catch((err) => {
+      const id = request.id;
+      const status = "error";
+      const error = JSON.stringify(err);
+      write({ id, status, error });
+    })
+    .catch(failure)
+);
