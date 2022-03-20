d68581e830fb40537be1febdf4f52cb6c306dbd5
EduardoRFS <theeduardorfs@gmail.com>
2022-03-20 20:27:27 -0300

    tezois_interop: remove old listen_transactions.js

--- src/tezos_interop/scripts.ml
+++ src/tezos_interop/scripts.ml
@@ -1,17 +1,10 @@
 open Helpers
 
 let tezos_js_bridge = [%blob "tezos_js_bridge.bundle.js"]
-let listen_transactions = [%blob "listen_transactions.bundle.js"]
 
-let make_file ~prefix content =
-  let%await file, oc = Lwt_io.open_temp_file ~prefix ~suffix:".js" () in
-  let%await () = Lwt_io.write oc content in
-  await file
-
-let file_tezos_js_bridge, file_listen_transactions =
+let file_tezos_js_bridge =
   Lwt_main.run
-    (let%await file_tezos_js_bridge =
-       make_file ~prefix:"tezos_js_bridge" tezos_js_bridge in
-     let%await file_listen_transactions =
-       make_file ~prefix:"listen_transactions" listen_transactions in
-     Lwt.return (file_tezos_js_bridge, file_listen_transactions))
+    (let%await file, oc =
+       Lwt_io.open_temp_file ~prefix:"tezos_js_bridge" ~suffix:".js" () in
+     let%await () = Lwt_io.write oc tezos_js_bridge in
+     await file)
