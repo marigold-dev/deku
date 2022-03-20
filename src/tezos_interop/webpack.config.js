9c7ca4815b3d94f0b3a7437d3ee202dd711e97ec
EduardoRFS <theeduardorfs@gmail.com>
2022-03-20 20:27:27 -0300

    tezois_interop: remove old listen_transactions.js

--- src/tezos_interop/webpack.config.js
+++ src/tezos_interop/webpack.config.js
@@ -1,7 +1,6 @@
 module.exports = (env) => ({
   entry: {
     tezos_js_bridge: "./tezos_js_bridge.js",
-    listen_transactions: "./listen_transactions.js",
   },
   output: {
     filename: "[name].bundle.js",
