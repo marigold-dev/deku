9065ab4e8aa246dc2f9550bcfe5938459b6318b6
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2022-03-22 14:20:55 +0100

    bench simple expressions

--- benchmarks/bench_simple_expr.ml
+++ benchmarks/bench_simple_expr.ml
@@ -9,6 +9,11 @@ let test_compile_value_incr = bench_compile_value "incr" ~initial_gas:101 43L
 let test_compile_incr =
   bench_compile_script "incr" ~initial_gas:1901 ~script:script_incr
 
+let test_execute_incr =
+  bench_execute_exn 42L "incr" ~gas_value:101 ~gas_compile:1901 ~gas_exe:1901
+    ~script:script_incr
+
+(* TODO: do I need to check the result storage ? *)
 (* decrement script *)
 let script_decr = [%lambda_vm.script fun x -> (x - 1L, (0L, 0L))]
 
@@ -17,14 +22,25 @@ let test_compile_value_decr = bench_compile_value "decr" ~initial_gas:101 41L
 let test_compile_decr =
   bench_compile_script "decr" ~initial_gas:1901 ~script:script_decr
 
+let test_execute_decr =
+  bench_execute_exn 41L "decr" ~gas_value:101 ~gas_compile:1901 ~gas_exe:1901
+    ~script:script_decr
+
 (* pair *)
 let script_pair =
   [%lambda_vm.script fun pair -> (fst pair + snd pair, (0L, 0L))]
 
 let test_compile_value_pair = bench_compile_value "pair" ~initial_gas:101 51L
 
+let test_compile_value_pair_pair =
+  bench_compile_value_pair "pair" ~initial_gas:2901 (23L, 28L)
+
 let test_compile_pair =
   bench_compile_script "pair" ~initial_gas:2901 ~script:script_pair
+
+let test_execute_pair =
+  bench_execute_exn_pair "pair" (23L, 28L) ~gas_value:2901 ~gas_compile:2901
+    ~gas_exe:2901 ~script:script_pair
 
 (* if *)
 let script_if =
@@ -38,8 +54,22 @@ let test_compile_value_if_true =
 let test_compile_value_if_false =
   bench_compile_value "if_false" ~initial_gas:101 32L
 
+let test_compile_value_pair_if_true =
+  bench_compile_value_pair "if_true" ~initial_gas:4001 (1L, 51L)
+
+let test_compile_value_pair_if_false =
+  bench_compile_value_pair "if_false" ~initial_gas:4001 (0L, 33L)
+
 let test_compile_if =
   bench_compile_script "if" ~initial_gas:4001 ~script:script_if
+
+let test_execute_if_true =
+  bench_execute_exn_pair "if_true" (1L, 51L) ~gas_value:4001 ~gas_compile:4001
+    ~gas_exe:4001 ~script:script_if
+
+let test_execute_if_false =
+  bench_execute_exn_pair "if_false" (0L, 33L) ~gas_value:4001 ~gas_compile:4001
+    ~gas_exe:4001 ~script:script_if
 
 (* lambda *)
 let script_lambda =
@@ -53,11 +83,25 @@ let script_lambda =
 let test_compile_value_incr_lam =
   bench_compile_value "incr_lambda" ~initial_gas:101 100L
 
+let test_compile_value_pair_incr_lam =
+  bench_compile_value_pair "incr_lambda" ~initial_gas:6501 (1L, 99L)
+
 let test_compile_value_decr_lam =
   bench_compile_value "decr_lambda" ~initial_gas:101 32L
 
+let test_compile_value_pair_decr_lam =
+  bench_compile_value_pair "decr_lambda" ~initial_gas:6501 (0L, 33L)
+
 let test_compile_lambda =
   bench_compile_script "lambda" ~initial_gas:6501 ~script:script_lambda
+
+let test_execute_pair_incr_lam =
+  bench_execute_exn_pair "incr_lambda" (1L, 99L) ~gas_value:6501
+    ~gas_compile:6501 ~gas_exe:6501 ~script:script_lambda
+
+let test_execute_pair_decr_lam =
+  bench_execute_exn_pair "decr_lambda" (0L, 33L) ~gas_value:6501
+    ~gas_compile:6501 ~gas_exe:6501 ~script:script_lambda
 
 (* lambda application *)
 let script_lambda_app = [%lambda_vm.script fun y -> (fun x -> (x, (0L, 0L))) y]
@@ -67,21 +111,43 @@ let test_compile_value_lam_app =
 let test_compile_lambda_app =
   bench_compile_script "lambda_app" ~initial_gas:2000 ~script:script_lambda_app
 
+let test_execute_lambda_app =
+  bench_execute_exn 45L "lambda_app" ~gas_value:101 ~gas_compile:2000
+    ~gas_exe:2000 ~script:script_lambda_app
+
 let tests =
   [
     test_compile_value_incr;
-    test_compile_value_decr;
-    test_compile_value_pair;
-    test_compile_value_if_true;
-    test_compile_value_if_false;
-    test_compile_value_incr_lam;
-    test_compile_value_decr_lam;
-    test_compile_value_lam_app;
     test_compile_incr;
+    test_execute_incr;
+    (* descrement *)
+    test_compile_value_decr;
     test_compile_decr;
+    test_execute_decr;
+    (* pair *)
+    test_compile_value_pair;
+    test_compile_value_pair_pair;
     test_compile_pair;
+    test_execute_pair;
+    (* if *)
+    test_compile_value_if_true;
+    test_compile_value_pair_if_true;
+    test_compile_value_if_false;
+    test_compile_value_pair_if_false;
     test_compile_if;
+    test_execute_if_true;
+    test_execute_if_false;
+    (* lambda *)
+    test_compile_value_incr_lam;
+    test_compile_value_pair_incr_lam;
+    test_compile_value_decr_lam;
+    test_compile_value_pair_decr_lam;
     test_compile_lambda;
+    test_execute_pair_incr_lam;
+    test_execute_pair_decr_lam;
+    (* lambda application *)
+    test_compile_value_lam_app;
     test_compile_lambda_app;
+    test_execute_lambda_app;
   ]
 let command = Bench.make_command tests
--- benchmarks/vm_utils.ml
+++ benchmarks/vm_utils.ml
@@ -48,6 +48,33 @@ let bench_execute_exn n s ~gas_value ~gas_compile ~gas_exe ~script =
       let _ = execute_exn gas arg ir in
       ())
 
+(* Pair *)
+let compile_value_pair (n1, n2) ~initial_gas =
+  let gas_value = Gas.make ~initial_gas in
+  compile_value_exn gas_value (Pair (Int64 n1, Int64 n2))
+
+let bench_compile_value_pair s (n1, n2) ~initial_gas =
+  let name =
+    "compile value pair ("
+    ^ Int64.to_string n1
+    ^ ", "
+    ^ Int64.to_string n2
+    ^ ") "
+    ^ s in
+  Bench.Test.create ~name (fun () ->
+      let _ = compile_value_pair (n1, n2) ~initial_gas in
+      ())
+let bench_execute_exn_pair s (n1, n2) ~gas_value ~gas_compile ~gas_exe ~script =
+  let name =
+    "execute pair (" ^ Int64.to_string n1 ^ ", " ^ Int64.to_string n2 ^ ") " ^ s
+  in
+  Bench.Test.create ~name (fun () ->
+      let arg = compile_value_pair (n1, n2) ~initial_gas:gas_value in
+      let gas_ir = Gas.make ~initial_gas:gas_compile in
+      let ir = compile_exn gas_ir script in
+      let gas = Gas.make ~initial_gas:gas_exe in
+      let _ = execute_exn gas arg ir in
+      ())
 let counter_script =
   [%lambda_vm.script
     fun x ->
