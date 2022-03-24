aad2eb810023511bce01aa62c85eb69b6bfdb610
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2022-03-24 12:31:39 +0100

    add bench for different gas_value for counter

--- benchmarks/bench_gas.ml
+++ benchmarks/bench_gas.ml
@@ -18,13 +18,39 @@ let test_compile_counter =
 (* execute scripts *)
 
 (* TODO execute ast script *)
-let test_execute_counter_0 =
-  bench_execute_exn 0L "counter_0" ~gas_value:101 ~gas_compile:14_747_900
-    ~gas_exe:15_000_000 ~script:counter_script
 
 let test_execute_counter_4096 =
   bench_execute_exn 4096L "counter_4096" ~gas_value:101 ~gas_compile:14_747_900
     ~gas_exe:15_000_000 ~script:counter_script
+
+(* Bench execute for different initial gas value (gas execute, gas compile)
+   - start: counter_script value 0L initial gas value:101
+*)
+
+let bench_exe_counter_gas s ~gas_value =
+  let s = "counter_gas_" ^ s in
+  bench_execute_exn 0L s ~gas_value ~gas_compile:14_747_900 ~gas_exe:15_000_000
+    ~script:counter_script
+
+let test_execute_counter_0 = bench_exe_counter_gas "0" ~gas_value:101
+
+let test_execute_counter_1 = bench_exe_counter_gas "1" ~gas_value:2_001
+
+let test_execute_counter_2 = bench_exe_counter_gas "2" ~gas_value:3_001
+
+let test_execute_counter_3 = bench_exe_counter_gas "3" ~gas_value:4_001
+
+let test_execute_counter_4 = bench_exe_counter_gas "4" ~gas_value:5_001
+
+let test_execute_counter_5 = bench_exe_counter_gas "5" ~gas_value:6_001
+
+let test_execute_counter_6 = bench_exe_counter_gas "6" ~gas_value:7_001
+
+let test_execute_counter_7 = bench_exe_counter_gas "7" ~gas_value:8_001
+
+let test_execute_counter_8 = bench_exe_counter_gas "8" ~gas_value:9_001
+
+let test_execute_counter_9 = bench_exe_counter_gas "9" ~gas_value:1001
 
 let tests =
   [
@@ -32,8 +58,17 @@ let tests =
     test_compile_value_4096;
     test_compile_ast;
     test_compile_counter;
-    test_execute_counter_0;
     test_execute_counter_4096;
+    test_execute_counter_0;
+    test_execute_counter_1;
+    test_execute_counter_2;
+    test_execute_counter_3;
+    test_execute_counter_4;
+    test_execute_counter_5;
+    test_execute_counter_6;
+    test_execute_counter_7;
+    test_execute_counter_8;
+    test_execute_counter_9;
   ]
 
 let command = Bench.make_command tests
--- benchmarks/benchmarks.ml
+++ benchmarks/benchmarks.ml
@@ -3,6 +3,9 @@ open Core
 (* TODO: script for running benchmark
    dune build @benchmarks/bench
    dune exec -- ./benchmarks/benchmarks.exe gas/recursive/expr/etc.
+   or using esy:
+   esy b dune build
+   esy b dune exec ./benchmarks/benchmarks.exe gas
 *)
 
 let main () =
