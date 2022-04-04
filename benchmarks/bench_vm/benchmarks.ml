e82c16baa1b6013c8c8c08bee41e6860010d1c0d
InfiniteSwerve <jackofthestrand@gmail.com>
2022-04-04 12:42:37 -0700

    init dune, utils, and bench_gas.ml

---
+++ benchmarks/bench_vm/benchmarks.ml
@@ -0,0 +1,15 @@
+open Core
+
+(* TODO: script for running benchmark
+   dune build @benchmarks/bench
+   dune exec -- ./benchmarks/benchmarks.exe gas/recursive/expr/etc.
+   or using esy:
+   esy b dune build
+   esy b dune exec ./benchmarks/benchmarks.exe gas
+*)
+
+let main () =
+  Command_unix.run
+    (Command.group ~summary:"Several benchmarks" [("gas", Bench_gas.command)])
+
+let () = main ()
