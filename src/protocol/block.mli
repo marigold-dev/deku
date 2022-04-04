70dfd57c9f10ccf410be5121e8c60700850b2750
aguillon <arthur@marigold.dev>
2022-04-04 11:36:02 +0200

    Consensus rounds in blocks and hashs

--- src/protocol/block.mli
+++ src/protocol/block.mli
@@ -8,12 +8,18 @@ type t = private {
   previous_hash : BLAKE2B.t;
   author : Key_hash.t;
   block_height : int64;
+  consensus_round : int;
   operations : Protocol_operation.t list;
 }
 [@@deriving yojson, ord]
 val sign : key:Secret.t -> t -> Protocol_signature.t
 val verify : signature:Protocol_signature.t -> t -> bool
 val genesis : t
+
+val update_round : t -> consensus_round:int -> t
+(** [update_round block round] changes the value of [block.consensus_round] and recomputes the hash
+    *)
+
 val produce :
   state:Protocol_state.t ->
   next_state_root_hash:BLAKE2B.t option ->
