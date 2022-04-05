c43b991b654a2788cdd600933b1d6c85548881ab
InfiniteSwerve <jackofthestrand@gmail.com>
2022-04-04 17:55:46 -0700

    changed core to deku_core

--- src/node/state.mli
+++ src/node/state.mli
@@ -27,7 +27,7 @@ type t = {
   snapshots : Snapshots.t;
   uri_state : string Uri_map.t;
   validators_uri : Uri.t Address_map.t;
-  recent_operation_receipts : Core.State.receipt BLAKE2B.Map.t;
+  recent_operation_receipts : Deku_core.State.receipt BLAKE2B.Map.t;
   persist_trusted_membership_change :
     Trusted_validators_membership_change.t list -> unit Lwt.t;
 }
