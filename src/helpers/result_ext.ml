b84fa891e1522b9c8d80149ed33e22e8feac5f9e
aguillon <arthur@marigold.dev>
2022-04-05 10:18:18 +0200

    Helpers for result and lwt

--- src/helpers/result_ext.ml
+++ src/helpers/result_ext.ml
@@ -8,4 +8,17 @@ module Let_syntax = struct
       match bool with
       | true -> f ()
       | false -> Error message]
+
+  (* When let%assert is not available *)
+  let ensure (message, bool) =
+    match bool with
+    | true -> Lwt.return @@ Ok ()
+    | false -> Lwt.return @@ Error message
+
+  let ( let*? ) lwtx f =
+    let open Lwt.Syntax in
+    let* x = lwtx in
+    match x with
+    | Ok a -> f a
+    | Error e -> Lwt.return @@ Error e
 end
