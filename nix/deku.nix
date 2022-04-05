e04e552c5ae7988ca09a65d466243752d3d3fe36
Ulrik Strid <ulrik.strid@outlook.com>
2022-04-05 09:16:11 +0200

    Override dreams inputs

--- nix/deku.nix
+++ nix/deku.nix
@@ -1,5 +1,12 @@
 { pkgs, stdenv, lib, doCheck ? true, npmPackages, nodejs ? pkgs.nodejs }:
 
+let
+  dream' = pkgs.ocamlPackages.dream.overrideAttrs (o: {
+    propagatedBuildInputs = o.propagatedBuildInputs
+      ++ [ pkgs.ocamlPackages.ppx_inline_test ];
+  });
+in
+
 pkgs.ocamlPackages.buildDunePackage {
   pname = "sidechain";
   version = "0.0.0-dev";
@@ -35,7 +42,7 @@ pkgs.ocamlPackages.buildDunePackage {
     mirage-crypto-rng
     mirage-crypto-ec
     piaf-dream-compat
-    dream
+    dream'
     mrmime
     hex
     tezos-micheline
