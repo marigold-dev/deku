46de07d7d7d34fdf35dd4bd2d782d22d6acbc941
lykimq <lykimq@gmail.com>
2022-04-11 11:39:49 +0200

    change folder path for script

--- benchmarks/micro_bench/bench_deku/scripts/benchmark.sh
+++ benchmarks/micro_bench/bench_deku/scripts/benchmark.sh
@@ -1,13 +1,13 @@
 # build benchmarks
 esy b dune build @benchmarks/bench &> /dev/null
 
-RESULTPATH=./benchmarks/bench_deku/results
+RESULTPATH=./benchmarks/micro_bench/bench_deku/results
 
 # TODO: figure out how to substitute this in the for loop for more modularity
 #BENCHMARKS = "expr" "gas" "prim" "recursive"
 
 for benchmark in "interop" "ledger" "patricia" "rpc" "validators"
   do
-    ./_esy/default/build/default/benchmarks/bench_deku/bench_deku.exe $benchmark > \
+    ./_esy/default/build/default/benchmarks/micro_bench/bench_deku/bench_deku.exe $benchmark > \
     $RESULTPATH/$benchmark.txt
   done
--- benchmarks/micro_bench/bench_vm/scripts/benchmark.sh
+++ benchmarks/micro_bench/bench_vm/scripts/benchmark.sh
@@ -1,13 +1,13 @@
 # build benchmarks
 esy b dune build @benchmarks/bench &> /dev/null
 
-RESULTPATH=./benchmarks/bench_vm/results
+RESULTPATH=./benchmarks/micro_bench/bench_vm/results
 
 # TODO: figure out how to substitute this in the for loop for more modularity
 #BENCHMARKS = "expr" "gas" "prim" "recursive"
 
 for benchmark in "simple-expr" "gas" "prim" "recursive"
   do
-    ./_esy/default/build/default/benchmarks/bench_vm/benchmarks.exe $benchmark > \
+    ./_esy/default/build/default/benchmarks/micro_bench/bench_vm/benchmarks.exe $benchmark > \
     $RESULTPATH/$benchmark.txt
   done
