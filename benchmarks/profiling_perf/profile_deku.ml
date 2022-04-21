2e751c899277d793cd2cbb2a286dbee7bb0bc778
lykimq <lykimq@gmail.com>
2022-04-21 14:45:46 +0200

    add guide for flamegraph

--- benchmarks/profiling_perf/profile_deku.ml
+++ benchmarks/profiling_perf/profile_deku.ml
@@ -7,6 +7,16 @@
   Note: each run esy x dune build, remove the perf.data and old perf.data  
 
   sudo perf report -n
+
+  For Flamegraph need to download the Flamegraph from git:
+
+  # git clone https://github.com/brendangregg/FlameGraph 
+  # cd FlameGraph
+  
+  # perf record --callgraph dwarf -- program-to-run program-arguments
+
+  $ sudo perf record --call-graph dwarf -- ~/deku/_build/default/benchmarks/profiling_perf/profile_deku.exe
+  # sudo perf script | ./stackcollapse-perf.pl | ./flamegraph.pl > perf-deku-flamegraph.svg
 *)
 let main () =
   Profile_ledger.profile_ledger ();
