46bb55c1574cf83e8cbaf2230202a3223248ca6d
lykimq <lykimq@gmail.com>
2022-04-22 14:24:12 +0200

    add todo work for profiling using memtrace to track the memory leaks

--- benchmarks/profiling_perf/profile_deku.ml
+++ benchmarks/profiling_perf/profile_deku.ml
@@ -25,7 +25,49 @@
   https://github.com/KDAB/hotspot
   hotspot /path/to/perf.data
 
+
+  -- Memory profiling the C heap
+  - install `valgrind` and `massif-visualizer`
+
+  Then run:
+  `valgrind --tool=massif ~/deku/_build/default/benchmarks/profiling_perf/profile_deku.exe`
+
+  where:
+  --tool=<name>: use the Valgrind tool named <name> [memcheck]
+
+  - Using Ctrl-C then seeing the result with:
+  `massif-visualizer massif.out.pid`
+
+  in this example it is: 
+  `massif-visualizer massif.out.45147`
+
+-- find memory leak 
+https://blog.janestreet.com/finding-memory-leaks-with-memtrace/
+spacetime is another profiling memory, it would gather data on all the allocations performed
+by a program. It was very useful for finding memory leaks but had a high overhead and required using
+a special configuration of the OCaml compiler. Its profiles could become very large and require
+huge amounts of memory to process.
+
+The memtrace support:
++ profiles a sample of the allocations, not all of them
++ works with the ordinary OCaml compiler
++ it is supported by all platforms
++ it can run with low enough overhead to be used in production systems
+
+install: opam switch create memtrace
+eval $(opam env --switch=memtrace)
+
+--another lib for memory leak
+https://github.com/jhjourdan/statmemprof-emacs/
+http://doc.tzalpha.net/developer/profiling.html
+
+
+
+
 *)
+
 let main () =
+  let _ = Memtrace.trace_if_requested ~context:"Profile_deku with memtrace" () in
   Profile_ledger.profile_ledger ();
-  Profile_tezos_interop.profile_tezos_interop ()
+  Profile_tezos_interop.profile_tezos_interop ();
+  ()
