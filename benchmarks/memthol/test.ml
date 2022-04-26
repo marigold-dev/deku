7afeed6ce7d89368f668f2d7e49df9e1a6838927
lykimq <lykimq@gmail.com>
2022-04-26 15:16:53 +0200

    add viewer memthol a viewer for memtrace

---
+++ benchmarks/memthol/test.ml
@@ -0,0 +1,14 @@
+(*
+   https://github.com/OCamlPro/memthol
+
+   It is a visualizer and analyzer for program profiling.
+   It works on memory "dumps" containing information about 
+   the size and (de)allocation date of (some of) the allocations
+   performed by some execution of a program
+
+   Currently, memthol accepts memory dumps produced by Memtrace. 
+   A memtrace dump for a program execution is a single Common Trace Format (ctf) file
+
+   It is kind of a viewer for Memtrace
+
+*)
