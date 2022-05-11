a13a668bcac663a450887a4a3458238cac263d14
EduardoRFS <theeduardorfs@gmail.com>
2022-05-11 20:14:25 +0000

    deps: add lwt_domain

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,6 +5,17 @@ in {
     ocamlVersion.overrideScope' (oself: osuper: {
       lwt_react = osuper.lwt_react.overrideAttrs
         (o: { nativeBuildInputs = o.nativeBuildInputs ++ [ oself.cppo ]; });
+      lwt_domain = osuper.buildDunePackage {
+        pname = "lwt_domain";
+        src = prev.fetchFromGitHub {
+          owner = "ocsigen";
+          repo = "lwt";
+          rev = "654952b62dccea63f186747be79916dbfdccf48d";
+          sha256 = "sha256-j+ufbnovlcJnKM0DwU3Z/DRLeyJ9+AmhKSrJCpMVW7w=";
+        };
+        version = "dev";
+        propagatedBuildInputs = with osuper; [ domainslib lwt ];
+      };
       utop = osuper.utop.overrideAttrs (o: {
         propagatedBuildInputs = o.propagatedBuildInputs ++ [ oself.findlib ];
       });
