8135792eecaf27c32deeeb26cb6cd681bfd8777d
Daniel Hines <d4hines@gmail.com>
2022-05-11 11:48:08 -0400

    add comment about unnecessary gossip messages

--- src/node/flows.ml
+++ src/node/flows.ml
@@ -371,6 +371,7 @@ let received_user_operation state update_state user_operation =
     Node.Operation_map.mem operation state.Node.pending_operations in
 
   if not operation_exists then (
+    (* TODO: this broadcast is unnecessary in many case. We should optimize the messages sent. *)
     Lwt.async (fun () ->
         broadcast_user_operation_gossip state { user_operation });
     append_operation state update_state operation);
