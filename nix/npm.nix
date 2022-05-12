41ab1407b1729742f284cae324debd9183feb280
Ulrik Strid <ulrik.strid@outlook.com>
2022-05-12 22:58:35 +0200

    chore: use dream2nix for a better npm experience
    This is heavily inspired by the work in #579 so I'm adding co-author
    
    Co-authored-by: r-muhairi

---
+++ nix/npm.nix
@@ -0,0 +1,38 @@
+{ system, dream2nix-lib, nix-filter, nodejs }:
+
+let
+  outputs = (dream2nix-lib.makeFlakeOutputs {
+    source = nix-filter.lib.filter {
+      root = ../.;
+      include = [ ../package-lock.json ../package.json ];
+    };
+
+    packageOverrides = {
+      webpack-cli = {
+        remove-webpack-check = {
+          patches = [ ./patches/remove-webpack-check.patch ];
+        };
+      };
+    };
+
+    inject = { acorn-import-assertions."1.8.0" = [[ "acorn" "8.7.1" ]]; };
+
+    settings =
+      [{ subsystemInfo.nodejs = (builtins.substring 0 2 nodejs.version); }];
+  });
+  npmPackages = outputs.packages."${system}".sidechain;
+
+  node_modules' = builtins.attrValues
+    (builtins.removeAttrs npmPackages.dependencies [ "webpack" ]);
+  webpack = npmPackages.dependencies.webpack.overrideAttrs (_: {
+    postFixup = ''
+      wrapProgram $out/bin/webpack \
+      --set NODE_PATH=${npmPackages}/lib/node_modules/sidechain/node_modules:${
+        builtins.foldl' (prev: curr: prev + ":" + curr) "$NODE_PATH"
+        node_modules'
+      }
+    '';
+  });
+  node_modules = node_modules' ++ [ webpack ];
+
+in node_modules
