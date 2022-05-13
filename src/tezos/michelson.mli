23b8f2b859e3d1fc58a7fcf035895013215bb038
Andre Popovitch <andre@popovit.ch>
2022-05-13 13:09:10 -0500

    Fetch validators from discovery.mligo instead of validators.json (#450)
    * Consensus: Fetch validators from discovery.mligo
    
    * tezos_interop: refactor js bridge
    
    * tezos_interop: refactor tezos_interop
    
    * tezos_interop: fix my mistakes
    
    * tezos_interop: fix my mistakes 2
    
    Co-authored-by: EduardoRFS <theeduardorfs@gmail.com>

--- src/tezos/michelson.mli
+++ src/tezos/michelson.mli
@@ -6,3 +6,6 @@ val expr_encoding : t Data_encoding.t
 
 val unit : t
 val is_unit : t -> bool
+
+(* TODO: this seems like it should be in a tezos library somewhere already *)
+type big_map_key = Key_hash of Crypto.Key_hash.t
