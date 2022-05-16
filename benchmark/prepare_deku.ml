7fa7de5cf9ec7c0ecf31774ada09bf29f970f6e1
lykimq <lykimq@gmail.com>
2022-05-16 15:39:53 +0200

    dune does not find prepare deku

--- benchmark/prepare_deku.ml
+++ benchmark/prepare_deku.ml
@@ -1,13 +1,3 @@
-open Node
-open Helpers
-open Bin_common
-
-open (
-  struct
-    include Server
-  end :
-    sig end)
-
 (*************************************************************************)
 (* making Deku accounts - wallet *)
 
@@ -90,53 +80,3 @@ let make_ticket ticketer =
   let contract = Tezos.Contract_hash.of_string ticketer |> Option.get in
   let ticketer = Tezos.Address.Originated { contract; entrypoint = None } in
   Core.Ticket_id.{ ticketer; data = Bytes.empty }
-
-(*************************************************************************)
-(* Transactions *)
-
-let nonce = ref 0l
-
-let make_transaction ~block_level ~ticket ~sender ~recipient ~amount =
-  nonce := Int32.add 1l !nonce;
-  let amount = Core.Amount.of_int amount in
-  let transaction =
-    Core.User_operation.Transaction
-      { destination = recipient.key_hash; amount; ticket } in
-  let data = Core.User_operation.make ~source:sender.key_hash transaction in
-  Protocol.Operation.Core_user.sign ~secret:sender.secret ~nonce:!nonce
-    ~block_height:block_level ~data
-
-let spam_transactions ~ticketer ~n () =
-  let validator_uri = get_random_validator_uri () in
-  let%await block_level = get_current_block_level () in
-  let ticket = make_ticket ticketer in
-  let transactions =
-    List.init n (fun _ ->
-        make_transaction ~block_level ~ticket ~sender:alice_wallet
-          ~recipient:bob_wallet ~amount:1) in
-  Format.eprintf "Number of transactions - packed: %d\n%!"
-    (List.length transactions);
-  let%await _ =
-    Network.request_user_operations_gossip
-      { user_operations = transactions }
-      validator_uri in
-  Lwt.return transactions
-
-let rec spam ~ticketer =
-  let n = 2000 in
-  let%await _ =
-    Lwt_list.iter_p Fun.id
-    @@ (* REMARK: list 4 *)
-    List.init 4 (fun _ ->
-        let%await _ = spam_transactions ~ticketer ~n () in
-        await ()) in
-  let%await () = Lwt_unix.sleep 1.0 in
-  spam ~ticketer
-
-let load_test_transactions ticketer =
-  let%await starting_block_level = get_current_block_level () in
-  Format.printf "Starting block level: %Li\n%!" starting_block_level;
-  spam ~ticketer
-
-let load_test_transactions ticketer =
-  load_test_transactions ticketer |> Lwt_main.run
