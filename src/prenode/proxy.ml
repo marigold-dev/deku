a6544bf957545307a250fba1875712d46a24ce8c
Maria Climent-Pommeret <maria@marigold.dev>
2022-05-17 16:15:36 +0200

    Renaming Message

--- src/prenode/proxy.ml
+++ src/prenode/proxy.ml
@@ -1,10 +1,10 @@
 module type PROXY_POLLINATE = sig
   type t
-  val of_deku : Prenodemessage.t -> Pollinate.Node.Message.t
-  val of_pollinate : Pollinate.Node.Message.t -> Prenodemessage.t
+  val of_deku : Message.t -> Pollinate.Node.Message.t
+  val of_pollinate : Pollinate.Node.Message.t -> Message.t
 
   val handle_outgoing_messages :
-    Pollinate.Node.Inbox.t -> Prenodemessage.t list -> Pollinate.Node.Inbox.t
+    Pollinate.Node.Inbox.t -> Message.t list -> Pollinate.Node.Inbox.t
   val handle_ingoing_messages :
-    Pollinate.Node.Inbox.t -> Prenodemessage.t * Pollinate.Node.Inbox.t
+    Pollinate.Node.Inbox.t -> Message.t * Pollinate.Node.Inbox.t
 end
