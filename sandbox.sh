179b68eed04d1cbd19f9c0c17ffc03d4a7c48dd3
aguillon <aguillon@users.noreply.github.com>
2022-05-18 16:13:52 +0200

    Doc/sandbox script rebased (#617)
    Co-authored-by: lykimq, gsebil08, Daniel Hines, Ulrik Strid, Eduardo Rafael

--- sandbox.sh
+++ sandbox.sh
@@ -2,16 +2,49 @@
 
 set -e
 
+# =======================
+# Parameters declarations
+
+# Starting from 0
+NUMBER_OF_NODES=${3:-"3"}
+
+# This secret key never changes.
+# We need this secret key for sigining Tezos operations.
+# We have to give the secret key to the Deku node as part of configuration.
+SECRET_KEY="edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq"
+
+# Folder containing the data of each node:
+# - identity.json
+# - pre_epoch_state.json
+# - state.bin
+# - tezos.json
+# - trusted-validator-membership-change.json
+# - validators.json
+DATA_DIRECTORY="data"
+
+# https://github.com/koalaman/shellcheck/wiki/SC2207
+# shellcheck disable=SC2207
+VALIDATORS=($(seq 0 "$NUMBER_OF_NODES"))
+
+# =======================
+# Running environments
+# Docker mode is to run Deku nodes from docker-compose file
 if [ "${2:-local}" = "docker" ]; then
   mode="docker"
 else
   mode="local"
 fi
 
-NUMBER_OF_NODES=${3:-"3"}
+# =======================
+# We need the tezos-client to deploy contracts.
+# We will use the binary included in the Tezos testnet deployment
+# See https://tezos.gitlab.io/introduction/howtouse.html#client
+tezos-client() {
+  docker exec -t deku_flextesa tezos-client "$@"
+}
 
-data_directory="data"
 
+# https://github.com/koalaman/shellcheck/wiki/SC2016
 # shellcheck disable=SC2016
 [ "$USE_NIX" ] || LD_LIBRARY_PATH=$(esy x sh -c 'echo $LD_LIBRARY_PATH')
 export LD_LIBRARY_PATH
@@ -27,34 +60,38 @@ if [ "${REBUILD:-}" ]; then
   fi
 fi
 
-tezos-client() {
-  docker exec -t deku_flextesa tezos-client "$@"
-}
-
 if ! [ "$USE_NIX" ]; then
+  # Necessary to write tezos smart contracts
   ligo() {
     docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.37.0 "$@"
   }
 fi
 
 if [ $mode = "docker" ]; then
+  # Flextesa: Flexible Tezos Sandboxes
+  # https://gitlab.com/tezos/flextesa
   RPC_NODE=http://flextesa:20000
 else
+  # Using a Tezos node on localhost:20000 that is provided by the docker-compose file
   RPC_NODE=http://localhost:20000
 fi
 
-# This secret key never changes.
-SECRET_KEY="edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq"
-
-DATA_DIRECTORY="data"
-
-# shellcheck disable=SC2207
-VALIDATORS=($(seq 0 "$NUMBER_OF_NODES"))
+# =======================
+# Helper to print message
 
 message() {
   echo -e "\e[35m\e[1m**************************    $*    ********************************\e[0m"
 }
 
+# =======================
+# Generate Json files
+
+# validators_json:
+# We configure the nodes to be aware of each other.
+# The validator addresses in Deku are not Tezos addresses.
+# Their addresses begin with `tz1` or `tz2`.
+# These addresses are to support Tezos' hashing scheme.
+# They don't correlate to actual Tezos wallets.
 discovery_storage() {
   ## most of the noise in this function is because of indentation
   echo "Big_map.literal ["
@@ -68,6 +105,12 @@ EOF
   echo "]"
 }
 
+# trusted_validator_membership_change_json:
+# Node operators must manually allow for each change to the list
+# of validators, otherwise the node will not sign a requested change.
+# This information will be stored in the trusted_validator_membership_change.json
+# This list is provided at the moment merely as a convenience for
+# more efficient hacking on Deku.
 trusted_validator_membership_change_json() {
   echo "["
   for VALIDATOR in "${VALIDATORS[@]}"; do
@@ -88,11 +131,19 @@ EOF
   echo "]"
 }
 
+# [deploy_contract name source_file initial_storage] compiles the Ligo code in [source_file],
+# the [initial_storage] expression and originates the contract as myWallet on Tezos.
 deploy_contract () {
   message "Deploying new $1 contract"
+
+  # Compiles an initial storage for a given contract to a Michelson expression.
+  # The resulting Michelson expression can be passed as an argument in a transaction which originates a contract.
   storage=$(ligo compile storage "$2" "$3")
+
+  # Compiles a contract to Michelson code.
+  # Expects a source file and an entrypoint function.
   contract=$(ligo compile contract "$2")
-  
+
   echo "Originating $1 contract"
   sleep 2
   tezos-client --endpoint $RPC_NODE originate contract "$1" \
@@ -103,13 +154,24 @@ deploy_contract () {
     --force
 }
 
+# Creates deku validators' data directories in $DATA_DIRECTORY,
+# compiles and deploys the consensus and validators contracts,
+# creates the trusted_validator_membership_change JSON file in $DATA_DIRECTORY subfolders,
+# writes Tezos identity files in $DATA_DIRECTORY subfolders
 create_new_deku_environment() {
+
+  # Step 1: Set up validator identities for each node
   message "Creating validator identities"
   for i in "${VALIDATORS[@]}"; do
     FOLDER="$DATA_DIRECTORY/$i"
     mkdir -p "$FOLDER"
 
     if [ $mode = "docker" ]; then
+      # Using the deku-cli to generate identities for each node
+      # It is important to decide the URI of each node with the `--uri` flag
+      # For the current setup, we are using either localhost or deku-node-i and
+      # run on different ports (incremental).
+      # In future, one can configure URI to be whatever one wish
       deku-cli setup-identity "$FOLDER" --uri "http://deku-node-$i:4440"
     else
       deku-cli setup-identity "$FOLDER" --uri "http://localhost:444$i"
@@ -120,13 +182,11 @@ create_new_deku_environment() {
     VALIDATORS[$i]="$i;$KEY;$URI;$ADDRESS"
   done
 
-
-  # To register the validators, run consensus.mligo with the list of
-  # validators. To do this quickly, open the LIGO IDE with the url
-  # provided and paste the following storage as inputs to the contract.
   consensus_storage=$(
+  # Step 3: After having the Deku identities, we will configure and deploy
+  # a Deku consensus contract to the Tezos testnet.
     cat <<EOF
-{
+  {
   root_hash = {
     current_block_hash = 0x;
     current_block_height = 0;
@@ -149,23 +209,29 @@ EOF
   }
 }
 EOF
-  )
 
+)
+
+  # Step 4: deploying the consensus and validators discovery contracts
   consensus="./src/tezos_interop/consensus.mligo"
   discovery="./src/tezos_interop/discovery.mligo"
   deploy_contract "consensus" "$consensus" "$consensus_storage"
   deploy_contract "discovery" "$discovery" "$(discovery_storage)"
 
+  # call validators_json() and trusted_validator_membership_change_json()
   for VALIDATOR in "${VALIDATORS[@]}"; do
     i=$(echo "$VALIDATOR" | awk -F';' '{ print $1 }')
     FOLDER="$DATA_DIRECTORY/$i"
     trusted_validator_membership_change_json >"$FOLDER/trusted-validator-membership-change.json"
   done
 
+  # Step 5: Look up the address of the contract we just deployed.
   message "Getting contract address"
   TEZOS_CONSENSUS_ADDRESS="$(tezos-client --endpoint $RPC_NODE show known contract consensus | grep KT1 | tr -d '\r')"
   TEZOS_DISCOVERY_ADDRESS="$(tezos-client --endpoint $RPC_NODE show known contract discovery | grep KT1 | tr -d '\r')"
 
+  # Step 6: Finally we need to configure each Deku node to communicate with the Tezos testnet.
+  # This configuration is stored in a file named `tezos.json`, and is created with the `deku-cli setup-tezos``
   message "Configuring Deku nodes"
   for VALIDATOR in "${VALIDATORS[@]}"; do
     i=$(echo "$VALIDATOR" | awk -F';' '{ print $1 }')
@@ -181,44 +247,44 @@ EOF
   echo -e "\e[32m\e[1m### Tezos Contract address: $TEZOS_CONSENSUS_ADDRESS ###\e[0m"
 }
 
-tear-down() {
-  for i in "${VALIDATORS[@]}"; do
-    FOLDER="$DATA_DIRECTORY/$i"
-    if [ -d "$FOLDER" ]; then
-      rm -r "$FOLDER"
-    fi
-  done
-}
-
-start_tezos_node() {
-  tear-down
-  message "Configuring Tezos client"
-  tezos-client --endpoint $RPC_NODE bootstrapped
-  tezos-client --endpoint $RPC_NODE config update
-  tezos-client --endpoint $RPC_NODE import secret key myWallet "unencrypted:$SECRET_KEY" --force
-}
+# =======================
+# Steps for the command: ./sandbox start
+# - start_deku_cluster()
+# - wait_for_servers()
 
 SERVERS=()
 start_deku_cluster() {
+  # The steps are:
+  # - Step 1: Lauch a Tezos testnet
+  # - Step 2: Configure the Deku nodes that will run in the cluster such that they
+  #           are aware of each other
+  # (Step 1 and 2 are ./sandbox.sh setup)
+  # - Step 3: Start each node
   echo "Starting nodes."
   for i in "${VALIDATORS[@]}"; do
     if [ "$mode" = "local" ]; then
-      deku-node "$data_directory/$i" --listen-prometheus="900$i" &
+      deku-node "$DATA_DIRECTORY/$i" --listen-prometheus="900$i" &
       SERVERS+=($!)
     fi
   done
 
   sleep 1
 
+  # Step 4: Manually produce the block
+  # Produce a block using `deku-cli produce-block`
+  # See deku-cli produce-block --help
   echo "Producing a block"
   if [ "$mode" = "docker" ]; then
-    HASH=$(docker exec -t deku-node-0 deku-cli produce-block /app/data | awk '{ print $2 }' | tail -n1 | tr -d " \t\n\r")
+    HASH=$(docker exec -t deku-node-0 /app/deku_cli.exe produce-block /app/data | awk '{ print $2 }' | tail -n1 | tr -d " \t\n\r")
   else
-    HASH=$(deku-cli produce-block "$data_directory/0" | awk '{ print $2 }')
+    HASH=$(deku-cli produce-block "$DATA_DIRECTORY/0" | awk '{ print $2 }')
   fi
 
   sleep 0.1
 
+  # Step 5: Manually sign the block with 2/3rd of the nodes
+  # Sign the previously produced block using `deku-cli sign-block`
+  # See ./src/bin/deku_cli.ml:sign_block
   echo "Signing"
   for i in "${VALIDATORS[@]}"; do
     if [ "$mode" = "docker" ]; then
@@ -226,7 +292,7 @@ start_deku_cluster() {
       echo "deku-node-$i"
       docker exec -t "deku-node-$i" deku-cli sign-block /app/data "$HASH"
     else
-      deku-cli sign-block "$data_directory/$i" "$HASH"
+      deku-cli sign-block "$DATA_DIRECTORY/$i" "$HASH"
     fi
   done
 
@@ -238,9 +304,15 @@ wait_for_servers() {
   done
 }
 
+# =======================
+# Steps for the command: ./sandbox.sh smoke-test
+# - deku_height()
+# - start_deku_cluster()
+# - killall deku-node
+# - assert_deku_state()
 deku_storage() {
   local contract
-  contract=$(jq <"$data_directory/0/tezos.json" '.consensus_contract' | xargs)
+  contract=$(jq <"$DATA_DIRECTORY/0/tezos.json" '.consensus_contract' | xargs)
   local storage
   storage=$(curl --silent "$RPC_NODE/chains/main/blocks/head/context/contracts/$contract/storage")
   echo "$storage"
@@ -280,15 +352,49 @@ assert_deku_state() {
   fi
 
   for i in "${VALIDATORS[@]}"; do
-    asserter "$data_directory/$i" "$current_state_hash" "$minimum_expected_height"
+    asserter "$DATA_DIRECTORY/$i" "$current_state_hash" "$minimum_expected_height"
   done
 }
+
+# =======================
+# ./sandbox.sh tear-down
+# Removes the DATA_DIRECTORY subfolders
+# This avoids having wrong state when starting again
+# tear-down will be called first when start_tezos_node()
+tear-down() {
+  for i in "${VALIDATORS[@]}"; do
+    FOLDER="$DATA_DIRECTORY/$i"
+    if [ -d "$FOLDER" ]; then
+      rm -r "$FOLDER"
+    fi
+  done
+}
+
+# =======================
+# Steps for the command: ./sandbox.sh setup
+# - start_tezos_node ()
+# - create_new_deku_environment()
+
+# We need a Tezos wallet to deploy the contract with.
+# For convenience, we are using a hard-code secret key to create this wallet
+
+start_tezos_node() {
+  tear-down
+  message "Configuring Tezos client"
+  tezos-client --endpoint $RPC_NODE bootstrapped
+  tezos-client --endpoint $RPC_NODE config update
+  tezos-client --endpoint $RPC_NODE import secret key myWallet "unencrypted:$SECRET_KEY" --force
+}
+
+
+
+# =======================
 
 help() {
   # FIXME: fix these docs
   echo "$0 automates deployment of a Tezos testnet node and setup of a Deku cluster."
   echo ""
-  echo "Usage: $0 setup|tear-down"
+  echo "Usage: $0 setup|start|tear-down|smoke-test"
   echo "Commands:"
   echo "setup"
   echo "  Does the following:"
