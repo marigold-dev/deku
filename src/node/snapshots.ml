5e93dfb64d07f93f2808de8317c611fe3ca456de
Daniel Hines <d4hines@gmail.com>
2022-05-20 11:49:54 -0400

    fix: add snapshot logging back (#631)

--- src/node/snapshots.ml
+++ src/node/snapshots.ml
@@ -52,7 +52,7 @@ let add_snapshot_ref ~block_height t =
       additional_blocks = t.additional_blocks;
     } )
 let set_snapshot_ref ref_ snapshot =
-  Logs.info (fun m ->
+  Logs.app (fun m ->
       m "New protocol snapshot hash: %s" (snapshot.hash |> BLAKE2B.to_string));
   Atomic.set ref_ (Some snapshot)
 let start_new_epoch t =
