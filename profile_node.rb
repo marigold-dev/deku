f830a27034359f74d3986dfc26e63adc8faf93eb
lykimq <lykimq@gmail.com>
2022-05-23 12:18:35 +0200

    add profile deku node using perf and read perf.data using flamegraph

---
+++ profile_node.rb
@@ -0,0 +1,31 @@
+#
+# Profile deku node with perf
+# - Tear down the deku node
+# - Setup deku environment
+# - Start deku clusters and use perf to get the profiling data
+#  + -F 99: Sampling CPU stacks at 99 Hertz*
+#  + --callgraph dwarf: last branch record
+#  + -a: samples across all CPUs
+# - Sleep for 10 seconds
+#
+# Note:
+# The rate for perf is 99 Hertz, the generated flame graph from a 1000 Hertz profile.
+# Choosing 99 Hertz instead of 100 Hertz, is to avoid accidentally sampling in 
+# lockstep with some periodic activity, which would produce skewed results.
+# You can also increase to higher rates (eg, up to 997 Hertz) for finer resolution.
+# Bear in mind that higher frequencies means higher overhead
+#
+# Note: Setting `perf` to run without super-user permission (allow non-root users)
+# sudo sh -c 'echo kernel.perf_event_paranoid=0 > /etc/sysctl.d/local.conf'
+
+profile_node=
+command ="./sandbox.sh tear-down \
+          && ./sandbox.sh setup \
+          && perf record -F 99 -a --call-graph dwarf ./sandbox.sh start \
+          && sleep 10"
+system (command)
+
+
+
+#perf record -g -p "$node_pid" sh -c "./sandbox start && sleep 10"
+#perf script -i perf.data > profile.linux-perf.txt
