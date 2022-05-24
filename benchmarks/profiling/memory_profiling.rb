070f63b951d13eca410bf3c50a418fb027756cc4
lykimq <lykimq@gmail.com>
2022-05-24 18:04:14 +0200

    remove ruby file

--- benchmarks/profiling/memory_profiling.rb
+++
@@ -1,8 +0,0 @@
-#
-# Memory profiling the C heap using `valgrind` and `massif-visualizer`
-
-memory_profile=
-command="cd .. && cd .. && ./sandbox.sh tear-down \
-         && ./sandbox.sh setup \
-         && valgrind --tool=massif ./sandbox.sh start"
-system(command)
