ce4297e0301660dabda9304df6e2fded4bec06d7
lykimq <lykimq@gmail.com>
2022-05-25 10:41:11 +0200

    update readme with more info about massif

--- benchmarks/profiling/profile_deku.sh
+++ benchmarks/profiling/profile_deku.sh
@@ -2,6 +2,20 @@
 set -e 
 
 # Profiling deku node
+#
+# - `-F 99`: Sampling CPU stacks at 99 Hertz*
+# - `-g`: enable call-graph (stack chain/backtrace) recording for both kernel space 
+#        and user space
+# - `--call-graph dwarf`: set up and enable call-graph mode, get backtraces by using `dwarf` option
+# - `-a`: samples across all CPUs
+#
+# **Note:**
+# The rate for perf is 99 Hertz, the generated FlameGraph from a 1000 Hertz profile.
+# Choosing 99 Hertz instead of 100 Hertz, is to avoid accidentally sampling in 
+# lockstep with some periodic activity, which would produce skewed results.
+# You can also increase to higher rates (eg, up to 997 Hertz) for finer resolution.
+# Bear in mind that higher frequencies means higher overhead
+
 run(){
   cd .. && cd .. \
         && ./sandbox.sh tear-down \
@@ -26,32 +40,28 @@ call_graph(){
 }
 
 # Memory profiling using valgrind
-# Massif:
-# - Heap blocks;
-# - Heap administration blocks;
-# - Stack sizes. 
+# - Massif:
+#   + Heap blocks;
+#   + Heap administration blocks;
+#   + Stack sizes. 
 # 
 # --smc-check=all: tells Valgrind that the program may use self-modifying code
-# --massif-out-file=./massif.out.%p 
+# --massif-out-file=./massif.out.%p: define the name output by massif
 # --detailed-freq=1000000 tells Massif to do detailed snapshots
-# (which are more informative but more costly) only every 1,000,000th snapshot,
-# which is less often than the default of every 10th snapshot.  
-# This makes it run a bit faster.  This is fine because Massif always 
-# takes a detailed snapshot at the peak memory consumption point
+#   (which are more informative but more costly) only every 1,000,000th snapshot,
+#   which is less often than the default of every 10th snapshot.  
+#   This makes it run a bit faster. This is fine because Massif always 
+#   takes a detailed snapshot at the peak memory consumption point.
 #
-# Generate a call tree:
-#  --xtree-memory=full:  which can then be inspected using kcachegrind. 
-# The call tree filename is xtmemory.kcg.[pid] 
+# - Generate a call tree:
+#  --xtree-memory=full: which can then be inspected using kcachegrind. 
+#   The call tree filename is xtmemory.kcg.[pid] 
 #
-# Install kachegrind:
-# sudo apt update
-# sudo apt-get install kcachegrind
-#
-# Investigate memory while running:(?)
-# --vgdb=yes 
-# --vgdb-error=0:
-# To investigate the memory usage or call tree while an executable is running,
-# valgrind needs to be used through a gdb server. 
+# - Investigate memory while running:(?)
+#   --vgdb=yes 
+#   --vgdb-error=0:
+#   To investigate the memory usage or call tree while an executable is running,
+#   valgrind needs to be used through a gdb server. 
 
 memory_profile(){
    cd .. && cd .. && ./sandbox.sh tear-down \
