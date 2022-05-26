36950d73580a3f6b6f9cdfccd15b7152cd1917c5
Daniel Hines <d4hines@gmail.com>
2022-05-26 17:23:24 -0400

    feat: add fa2 implementation in typescript

---
+++ examples/js-fa2/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  "globals": {
+    "ts-jest": {
+      "diagnostics": {
+        "ignoreCodes": [151001]
+      }
+    }
+  }
+};
