83f32265471c4ffdb42465ee7cc0c89abc5daf76
lykimq <lykimq@gmail.com>
2022-05-30 16:40:48 +0200

    build state

---
+++ benchmarks/state_hash/build_state.ml
@@ -0,0 +1,13 @@
+(*
+  A Deku state is of:
+  type t =
+  {
+    ledger : Ledger.t;
+    contract_storage: Contract_storage.t
+  }
+*)
+
+let build_state () =
+  let ledger = Ledger.empty in
+  let contract_storage = Build_contract_storage.build_contract () in
+  { ledger; contract_storage }
