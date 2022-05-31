538571a4efdac0e20f750d8e1cb7ec0ab20246ff
Renato Alencar <renatoalencar.73@gmail.com>
2022-05-31 17:58:41 -0300

    Fix contract originated address (#652)
    When originating the contract, we need to use the protocol operation
    hash and not the core user operation hash, which would be the same for
    other contracts from the same implicit account with
    the same code and initial storge.

--- src/core_deku/state.mli
+++ src/core_deku/state.mli
@@ -13,6 +13,7 @@ val contract_storage : t -> Contract_storage.t
 
 val hash : t -> BLAKE2B.t
 
-val apply_user_operation : t -> User_operation.t -> t * receipt option
+val apply_user_operation :
+  t -> BLAKE2B.t -> User_operation.t -> t * receipt option
 
 val apply_tezos_operation : t -> Tezos_operation.t -> t
