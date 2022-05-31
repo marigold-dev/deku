869ecff7f485182050ecac62f52e251b55aacbfd
lykimq <lykimq@gmail.com>
2022-05-31 12:04:54 +0200

    add more deposit

--- benchmarks/state_hash/build_state.ml
+++ benchmarks/state_hash/build_state.ml
@@ -26,17 +26,33 @@ let make_ticket ?ticketer ?data () =
   let open Ticket_id in
   { ticketer; data }
 
+(*******************************************************************************)
+(* Tezos operation *)
+
 let init_tezos_operation_hash =
   "opCAkifFMh1Ya2J4WhRHskaXc297ELtx32wnc2WzeNtdQHp7DW4"
 
-let init_state ?(init_amount = 10_000) () =
-  let destination = make_tezos_address () in
-  let ticket = make_ticket () in
-  (* TODO: build a list of deposits *)
-  let internal_operation =
+let init_state () =
+  let destination_1 = make_tezos_address () in
+  let ticket_1 = make_ticket () in
+  let destination_2 = make_tezos_address () in
+  let ticket_2 = make_ticket () in
+  (* build a list of deposits *)
+  let op1 =
     Core_deku.Tezos_operation.Tezos_deposit
-      { destination; ticket; amount = Core_deku.Amount.of_int init_amount }
-  in
+      {
+        destination = destination_1;
+        ticket = ticket_1;
+        amount = Core_deku.Amount.of_int 10_000;
+      } in
+  let op2 =
+    Core_deku.Tezos_operation.Tezos_deposit
+      {
+        destination = destination_2;
+        ticket = ticket_2;
+        amount = Core_deku.Amount.of_int 20_000;
+      } in
+  (* init state *)
   let state = Core_deku.State.empty in
   let tezos_operation_hash =
     init_tezos_operation_hash |> Tezos.Operation_hash.of_string |> Option.get
@@ -44,23 +60,30 @@ let init_state ?(init_amount = 10_000) () =
   let payload =
     {
       Core_deku.Tezos_operation.tezos_operation_hash;
-      internal_operations = [internal_operation];
+      internal_operations = [op1; op2];
     } in
   let tezos_operation = Core_deku.Tezos_operation.make payload in
-  (* make tezos address as a source address for this operation *)
-  let make_address =
-    destination
+  (* make tezos address as a source addresses for this operation *)
+  let source_1 =
+    destination_1
+    |> Tezos.Address.to_string
+    |> Core_deku.Address.of_string
+    |> Option.map Core_deku.Address.to_key_hash
+    |> Option.join
+    |> Option.get in
+  let source_2 =
+    destination_2
     |> Tezos.Address.to_string
     |> Core_deku.Address.of_string
     |> Option.map Core_deku.Address.to_key_hash
     |> Option.join
     |> Option.get in
   let state = Core_deku.State.apply_tezos_operation state tezos_operation in
-  (state, make_address)
+  (state, (source_1, source_2), (ticket_1, ticket_2))
 
-(* Build user init operation as a contract origination where
-    we choose the Origination_payload as Lambda type
-*)
+(*******************************************************************************)
+(* User operations *)
+
 let user_op_contract_origination () : Core_deku.User_operation.initial_operation
     =
   let script =
@@ -76,10 +99,7 @@ let user_op_contract_origination () : Core_deku.User_operation.initial_operation
     |> Result.get_ok in
   Core_deku.User_operation.Contract_origination payload
 
-(* TODO other user operation like transfer, withdraw these will call
-   the ledger.transfer, ledger.withdraw; etc. *)
-
-(* The contract_address is a hash of the previous operation  if any*)
+(* The contract_address is a hash of the previous operation if any*)
 let user_op_contract_invocation user_operation =
   let contract_address =
     user_operation.Core_deku.User_operation.hash
@@ -91,22 +111,32 @@ let user_op_contract_invocation user_operation =
   Core_deku.User_operation.Contract_invocation
     { to_invoke = contract_address; argument = invocation_payload }
 
+(* Transaction *)
+let make_address () =
+  let _secret, _key, key_hash = Crypto.Key_hash.make_ed25519 () in
+  key_hash
+
+let user_op_transaction amount ticket () =
+  let destination = make_address () in
+  Core_deku.User_operation.Transaction { destination; amount; ticket }
+
+(*******************************************************************************)
 (* Build state:
    - Add a lot of user_operation(s) and then apply each of them to build the
    size of state
 *)
 let build_state () : Core_deku.State.t =
-  let init_state, source = init_state () in
+  let init_state, (source_1, _), (_ticket_1, _) = init_state () in
   let initial_operation = user_op_contract_origination () in
   (* first user operation as contract origination,
      source is the destination address of tezos_operation
   *)
-  let op1 = Core_deku.User_operation.make ~source initial_operation in
+  let op1 = Core_deku.User_operation.make ~source:source_1 initial_operation in
   let state, _receipt_option =
     Core_deku.State.apply_user_operation init_state op1 in
   (* second user operation as contract invocation payload same source *)
   let op2 =
-    Core_deku.User_operation.make ~source (user_op_contract_invocation op1)
-  in
+    Core_deku.User_operation.make ~source:source_1
+      (user_op_contract_invocation op1) in
   let state, _receipt_option = Core_deku.State.apply_user_operation state op2 in
   state
