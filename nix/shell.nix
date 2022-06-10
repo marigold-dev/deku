f93bce9a17848616b47a790792680c58fc11158d
Ulrik Strid <ulrik.strid@outlook.com>
2022-06-10 08:36:06 +0200

    We don't need 2 ocamlformat

--- nix/shell.nix
+++ nix/shell.nix
@@ -24,7 +24,6 @@ pkgs.mkShell {
       # formatters
       nixfmt
       nodePackages.prettier
-      ocamlformat
     ] ++ (pkgs.lib.optional (system != "x86_64-darwin") tilt)
     ++ (with pkgs.ocaml-ng.ocamlPackages_5_00; [
       # OCaml developer tooling
