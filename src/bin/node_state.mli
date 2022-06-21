9db7c439b79de363f6f6f2e881e06a736bac55b1
aguillon <arthur@marigold.dev>
2022-06-21 15:04:19 +0200

    pollinate: squashing everything and rebasing on main

--- src/bin/node_state.mli
+++ src/bin/node_state.mli
@@ -1,1 +1,4 @@
-val get_initial_state : folder:string -> Node.State.t Lwt.t
+val get_initial_state :
+  folder:string ->
+  pollinate_node_opt:Pollinate.PNode.t ref Lwt.t option ->
+  Node.State.t Lwt.t
