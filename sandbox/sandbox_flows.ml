0211e458334427fc4d039eff3abc2d146a894416
Daniel Hines <d4hines@gmail.com>
2022-06-29 17:01:43 -0400

    refactor: extract a setup module

--- sandbox/sandbox_flows.ml
+++ sandbox/sandbox_flows.ml
@@ -3,7 +3,6 @@ open Sandbox_helpers
 open Feather
 open Crypto
 open Tezos
-open Protocol
 
 let produce_block mode =
   (match mode with
@@ -45,8 +44,6 @@ let sign_block mode hash i =
     process "deku-node"
       ["sign-block"; Format.sprintf "data/%i" i; BLAKE2B.to_string hash])
   |> run_res ~error:"Error in sign block"
-
-let make_validators nodes = List.init nodes (fun i -> i)
 
 let get_balance address ticketer =
   let ticket = Format.sprintf "(Pair \"%s\" 0x)" (Address.to_string ticketer) in
@@ -128,123 +125,6 @@ let create_wallet address priv_key path =
   |> run_res ~error:"error in create wallet"
   |> Result.map (fun _ -> ())
 
-let is_node_bootstrapped rpc_url =
-  tezos_client ["--endpoint"; rpc_url; "bootstrapped"]
-  |> Result.map (String.split_on_char '\n')
-  |> Result.map List.rev
-  |> Result.map (fun list -> List.nth_opt list 0)
-  |> Result.map
-       (Option.to_result ~none:"No output from tezos-client bootstrapped")
-  |> Result.join
-  |> Result.map (String.starts_with ~prefix:"Node is bootstrapped")
-  |> Result.fold ~ok:(fun _ -> true) ~error:(fun _ -> false)
-
-let tezos_client_update_config rpc_url =
-  tezos_client ["--endpoint"; rpc_url; "config"; "update"]
-
-let import_secret rpc_url alias secret =
-  tezos_client
-    ["--endpoint"; rpc_url; "import"; "secret"; "key"; alias; secret; "--force"]
-
-let setup_identity mode i =
-  let folder = Format.sprintf "data/%d" i in
-  process "mkdir" ["-p"; folder] |> run;
-  (match mode with
-  | Docker ->
-    deku_cli
-      [
-        "setup-identity";
-        folder;
-        "--uri";
-        Format.sprintf "http://deku-node-%d:4440" i;
-      ]
-  | Local ->
-    deku_node
-      [
-        "setup-identity";
-        folder;
-        "--uri";
-        Format.sprintf "http://localhost:444%d" i;
-      ])
-  |> run;
-  let%ok key =
-    deku_node ["self"; folder]
-    |. grep "key:"
-    |. process "awk" ["{ print $2 }"]
-    |> collect stdout
-    |> Wallet.of_string
-    |> Option.to_result ~none:"error in key parsing" in
-  let%ok address =
-    deku_node ["self"; folder]
-    |. grep "address"
-    |. process "awk" ["{ print $2 }"]
-    |> collect stdout
-    |> Key_hash.of_string
-    |> Option.to_result ~none:"error in address parsing" in
-  let uri =
-    deku_node ["self"; folder]
-    |. grep "uri:"
-    |. process "awk" ["{ print $2 }"]
-    |> collect stdout in
-  Ok (i, key, uri, address)
-
-let make_consensus_storage identities =
-  identities
-  |> List.map (fun (_, _, _, address) ->
-         Format.sprintf "(\"%s\" : key_hash)" (Key_hash.to_string address))
-  |> String.concat ";"
-  |> Format.sprintf
-       "{root_hash = { current_block_hash = 0x;current_block_height = \
-        0;current_state_hash = 0x;current_handles_hash = 0x;current_validators \
-        = [%s]; }; \n\
-       \          vault = {\n\
-       \            known_handles_hash = (Big_map.empty : \
-        vault_known_handles_hash_set); used_handles = (Big_map.empty : \
-        vault_used_handle_set); vault = (Big_map.empty : vault); } }"
-
-let make_discovery_storage identities =
-  identities
-  |> List.map (fun (_, _, uri, address) ->
-         Format.sprintf "((\"%s\" : key_hash), (0, \"%s\"))"
-           (Key_hash.to_string address)
-           uri)
-  |> String.concat ";"
-  |> Format.sprintf "Big_map.literal [%s]"
-
-let make_trusted_validator_membership_change_json identities =
-  let open Consensus.Trusted_validators_membership_change in
-  let json =
-    List.map (fun (_, _, _, address) -> { action = Add; address }) identities
-    |> [%to_yojson: t list] in
-  List.iter
-    (fun (i, _, _, _) ->
-      let path_to_file =
-        Format.sprintf "data/%d/trusted-validator-membership-change.json" i
-      in
-      Yojson.Safe.to_file path_to_file json)
-    identities
-
-let setup_tezos rpc_node tezos_secret consensus_address discovery_address
-    identity =
-  let i, _, _, _ = identity in
-  let folder = Format.sprintf "data/%i" i in
-  deku_node
-    [
-      "setup-tezos";
-      folder;
-      "--tezos_consensus_contract";
-      Address.to_string consensus_address;
-      "--tezos_discovery_contract";
-      Address.to_string discovery_address;
-      "--tezos_rpc_node";
-      rpc_node;
-      "--tezos_secret";
-      tezos_secret;
-      "--unsafe_tezos_required_confirmations";
-      "4";
-    ]
-  |> run_res ~error:"error in deku-cli setup-tezos"
-
 (* start *)
 let start_deku_cluster mode validators =
   (* Step 1: Starts all the nodes only if mode is set to local *)
@@ -292,47 +172,6 @@ let tear_down nodes =
   make_validators nodes
   |> List.map (fun i -> Format.sprintf "data/%i" i)
   |> List.iter rm_dir;
-  Ok ()
-
-let setup mode validators rpc_url =
-  (* FIXME: this relative path seems suspicious - does it work if you move directories? *)
-  let consensus = "./src/tezos_interop/consensus.mligo" in
-  let discovery = "./src/tezos_interop/discovery.mligo" in
-  let secret = "edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq" in
-  validators
-  |> List.map (fun i -> Format.sprintf "data/%i" i)
-  |> List.iter rm_dir;
-  (* setup tezos-client *)
-  let%assert () =
-    ("the tezos node is not bootstrapped", is_node_bootstrapped rpc_url) in
-  let%ok _ = tezos_client_update_config rpc_url in
-  let%ok _ =
-    import_secret rpc_url "myWallet" (Format.sprintf "unencrypted:%s" secret)
-  in
-
-  (* setup write indentity.json to file system *)
-  let%ok identities = validators |> List.map_ok (setup_identity mode) in
-
-  (* deploy smart contracts *)
-  let consensus_storage = make_consensus_storage identities in
-  let discovery_storage = make_discovery_storage identities in
-  let%ok consensus_address =
-    deploy_contract rpc_url "consensus" consensus consensus_storage "myWallet"
-  in
-  let%ok discovery_address =
-    deploy_contract rpc_url "discovery" discovery discovery_storage "myWallet"
-  in
-
-  (* setup tezos informations *)
-  make_trusted_validator_membership_change_json identities;
-  identities
-  |> List.map_ok
-       (setup_tezos rpc_url secret consensus_address discovery_address)
-
-let setup mode nodes =
-  let validators = make_validators nodes in
-  let rpc_url = rpc_url mode in
-  let%ok _validators = setup mode validators rpc_url in
   Ok ()
 
 let deploy_dummy_ticket mode =
