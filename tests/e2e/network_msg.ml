7f134e4f4515082869562795c4a1971d2d4557f1
Quyen <quyen@marigold.dev>
2022-06-30 13:00:35 +0200

    add result data of network msg

--- tests/e2e/network_msg.ml
+++ tests/e2e/network_msg.ml
@@ -14,20 +14,18 @@ open Cmdliner
 *)
 let spam_messages ~batch_count ~batch samples =
   let rec go samples =
-    if samples <= 0 then await ()
+    if samples <= 0 then
+      await ()
     else
       let%await () =
         Lwt_list.iter_p
           (fun _ ->
             let%await _ =
               Network.raw_request Network.User_operations_noop.path batch
-                (get_random_validator_uri ())
-            in
-            await () )
-          (List.init batch_count Fun.id)
-      in
-      go (samples - 1)
-  in
+                (get_random_validator_uri ()) in
+            await ())
+          (List.init batch_count Fun.id) in
+      go (samples - 1) in
   go samples
 
 (* Chosen ad hoc as a number that finished reasonably quickly *)
@@ -36,40 +34,36 @@ let ad_hoc_params ticketer =
     let n = 13 in
     List.init n (fun i ->
         let i = i |> Float.of_int in
-        (2. ** i, 2. ** (Float.of_int n -. i)) )
-  in
+        (2. ** i, 2. ** (Float.of_int n -. i))) in
   params
   |> List.map (fun (batch_count, batch_size) ->
          let user_operations =
            List.init (batch_size |> Float.to_int) (fun _ ->
                make_transaction ~block_level:0L ~ticket:(make_ticket ticketer)
-                 ~sender:alice_wallet ~recipient:bob_wallet ~amount:0 )
-         in
+                 ~sender:alice_wallet ~recipient:bob_wallet ~amount:0) in
          let batch =
-           {user_operations} |> Network.User_operations_noop.request_to_yojson
-           |> Yojson.Safe.to_string
-         in
-         (batch_count |> Float.to_int, batch_size |> Float.to_int, batch) )
+           { user_operations }
+           |> Network.User_operations_noop.request_to_yojson
+           |> Yojson.Safe.to_string in
+         (batch_count |> Float.to_int, batch_size |> Float.to_int, batch))
 
 let spam_noop_transactions ticketer =
-  Format.eprintf "Batches ready, starting test\n%!" ;
-  print_endline "batch_count, batch_size, messages_per_second" ;
+  Format.eprintf "Batches ready, starting test\n%!";
+  print_endline "batch_count, batch_size, messages_per_second";
   let samples = 5 in
   Lwt_list.iter_s
     (fun (batch_count, batch_size, batch) ->
-      Format.eprintf "Testing batch count: %d, batch size: %d\n" batch_count
-        batch_size ;
+      (*Format.eprintf "batch count: %d, batch size: %d\n" batch_count batch_size ;*)
       let start_time = Unix.gettimeofday () in
       let%await () = spam_messages ~batch_count ~batch samples in
       let end_time = Unix.gettimeofday () in
       let duration = end_time -. start_time in
       let message_per_second =
-        Float.of_int (samples * batch_count * batch_size) /. duration
-      in
-      Format.eprintf "Duration: %.3f, mps: %.3f\n%!" duration message_per_second ;
+        Float.of_int (samples * batch_count * batch_size) /. duration in
+      (*Format.eprintf "Duration: %.3f, mps: %.3f\n%!" duration message_per_second ;*)
       Format.eprintf "%d, %d, %.3f\n%!" batch_count batch_size
-        message_per_second ;
-      await () )
+        message_per_second;
+      await ())
     (ad_hoc_params ticketer)
 
 let spam_noop_transactions ticketer =
@@ -81,10 +75,8 @@ let args =
     let docv = "ticketer" in
     let doc =
       "Tezos address of the contract issuing the ticket (e.g \
-       KT1Ec5eb7WZNuqWDUdcFM1c2XcmwjWsJrrxb)"
-    in
-    required & pos 0 (some string) None & info [] ~doc ~docv
-  in
+       KT1Ec5eb7WZNuqWDUdcFM1c2XcmwjWsJrrxb)" in
+    required & pos 0 (some string) None & info [] ~doc ~docv in
   let open Term in
   const spam_noop_transactions $ ticketer
 
