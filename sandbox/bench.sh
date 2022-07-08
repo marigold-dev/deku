14c3fd4912f5a8e86b52d06daad50ff19eb9b253
Quyen <quyen@marigold.dev>
2022-07-08 14:38:28 +0200

    add results of benchmarks

---
+++ sandbox/bench.sh
@@ -0,0 +1,25 @@
+set -e
+
+# add deku tezos secret key in the tezos chain as bob
+
+export DEKU_TEZOS_SECRET=edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq
+
+# add account for bob to have tez
+
+nix run github:marigold-dev/tezos-nix#tezos-client -- import secret key bob unencrypted:edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq --force
+
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe setup
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe start
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe deploy-dummy-ticket
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe load-test-tps
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe load-test-tps-offline
+
+dune exec -- _build/default/sandbox/deku_sandbox.exe network-msg >& ~/deku/tests/e2e/bench_tps/network_msg.csv
+
+# benchmark for state hash core bench
+dune exec -- _build/default/benchmarks/state_hash/bench_state_hash.exe state-hash >& ~/deku/benchmarks/state_hash/bench_state_hash.csv
--- sandbox/network_msg.ml
+++ sandbox/network_msg.ml
@@ -9,7 +9,8 @@ let network_msg rpc_address =
   let%ok dummy_ticket_address =
     get_contract_address rpc_address "dummy_ticket" in
   let dummy_ticket_address = Address.to_string dummy_ticket_address in
-  let%ok _result = process "network-msg" [dummy_ticket_address] |> run_res in
+  let%ok _result =
+    process "deku-network-msg" [dummy_ticket_address] |> run_res in
   Ok ()
 
 let term =
