bea291dd8452fe9dac1202a278fc93f0b684db8f
kienle371999 <kienle371999@gmail.com>
2022-07-12 09:40:19 +0700

    format codebase

--- tests/e2e/asserter_uri_update.ml
+++ tests/e2e/asserter_uri_update.ml
@@ -2,24 +2,24 @@
 
 open Cmdliner
 
-
 let assert_uri_update uri storage_uri =
   assert (uri = storage_uri);
-  Format.printf "The uri is updated properly 👍 \n" 
- 
-  
+  Format.printf "The uri is updated properly 👍 \n"
+
 let args =
   let uri =
     let docv = "uri" in
     let doc = "The updated uri of a particular validator" in
     let open Arg in
     required & pos 1 (some string) None & info [] ~doc ~docv in
-  let storage_uri = 
+  let storage_uri =
     let docv = "storage_uri" in
-    let doc = "The uri of a particular validator is got in the storage of discovery contract" in
+    let doc =
+      "The uri of a particular validator is got in the storage of discovery \
+       contract" in
     let open Arg in
     required & pos 1 (some string) None & info [] ~doc ~docv in
   let open Term in
-  const assert_uri_update $ uri $ storage_uri 
-  
-  let _ = Cmd.eval @@ Cmd.v (Cmd.info "asserter") args
+  const assert_uri_update $ uri $ storage_uri
+
+let _ = Cmd.eval @@ Cmd.v (Cmd.info "asserter") args
