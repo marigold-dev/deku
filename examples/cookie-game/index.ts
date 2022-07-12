2829bded2e3ca69013854f5c656c4cc50c57b163
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-07-12 15:41:49 +0200

    mint cookies, grandmas and factories

--- examples/cookie-game/index.ts
+++ examples/cookie-game/index.ts
@@ -13,27 +13,82 @@ import {main, get, set} from "deku_js_interop"
 
 // fct({param1: "salut", param2:3});
 
+interface cookie_baker {
+    number_of_cookies: number,
+    number_of_grandmas: number,
+    number_of_factories: number,
+}
+
+interface grandma {
+    cookie_per_second : 2
+}
+interface factory {
+    cookie_per_second : 10
+}
+
+enum actions {
+    increment_cookie = "increment_cookie",
+    increment_grandma = "increment_grandma",
+    increment_factory = "increment_factory"
+}
+
 const transition = ( {source, op_hash, tx_hash, operation}) => {
     // source -> tz1 address
     // op_hash / tx_hash => BLAKE2B => resolved as string
     // operation => any
 
-    //always need to do `JSON.parse(get(source));`
-    const source_value = JSON.parse(get(source));
-    console.log("Successfully parsed source: " + source_value);
-    console.log(source_value);
-    console.log("adding cookie");
-    //adding one cookie
-    source_value.cookies += 1;
-    console.log("new cookies amount: " + source_value.cookies);
-    //update state
-    set(source, JSON.stringify(source_value));
-    console.log("successfully minted cookie for:" + source)
+    switch (operation) {
+        case actions.increment_cookie : {
+            const source_value = JSON.parse(get(source));
+            console.log("Successfully parsed source: ");
+            console.log(source_value);
+            console.log("adding cookie");
+            //adding one cookie
+            source_value.cookie_baker.number_of_cookies += 1;
+            console.log("new cookies amount: " + source_value.number_of_cookies);
+            //update state
+            set(source, JSON.stringify(source_value));
+            console.log("successfully minted cookie for:")
+            console.log(source_value);
+            break;
+        }
+        case actions.increment_grandma : {
+            const source_value = JSON.parse(get(source));
+            console.log("Successfully parsed source: ");
+            console.log(source_value);
+            console.log("adding grandma");
+            //adding one cookie
+            source_value.cookie_baker.number_of_grandmas += 1;
+            console.log("new grandmas amount: " + source_value.number_of_grandmas);
+            //update state
+            set(source, JSON.stringify(source_value));
+            console.log("successfully minted grandma for:")
+            console.log(source_value);
+            break;
+        }
+        case actions.increment_factory : {
+            const source_value = JSON.parse(get(source));
+            console.log("Successfully parsed source: ");
+            console.log(source_value);
+            console.log("adding factory");
+            //adding one factory
+            source_value.factory_baker.number_of_factories += 1;
+            console.log("new factories amount: " + source_value.number_of_factories);
+            //update state
+            set(source, JSON.stringify(source_value));
+            console.log("successfully minted factory for:")
+            console.log(source_value);
+            break;
+        }
+    }
+    //always need to do `JSON.parse(get(source));
+    
 }
 
 main(
     //tz address must be replaced by a correct one obtained with 
     //deku-cli create-wallet
     {"tz1LRHhTuoPn79STLaKbPpZUvB8QQk4WMeS1": 
-        {cookies:0}
+        {cookie_baker:
+            { number_of_cookies: 0, number_of_grandmas:0, number_of_factories:0}}
     }, transition)
