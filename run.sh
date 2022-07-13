af7d2a2ab997922e8e575247c6f1b3df86e35a1b
Quyen <quyen@marigold.dev>
2022-07-13 09:49:38 +0200

    fix mistake in build_sdks, add build dune for Deku project to remove node_modules first

--- run.sh
+++ run.sh
@@ -4,55 +4,63 @@ node=$2
 wallet="$3"
 cmd="$4"
 
+# Build dune project
+build_dune(){
+    rm -rf node_modules
+    dune clean
+    dune build
+}
+
+# Build cookie project
 build_cookie(){
     cd examples/cookie-game && npm run build
 }
 
 # Build sdks
 build_sdks(){
-    cd sdks/deku_js_interop && npm install && npm run build
+    cd sdks/deku_js_interop && npm install && npm run build && npm i
 }
 
 # Run mock create
 mock(){
-    # build the ts project
+    # build cookie project project
     cd examples/cookie-game && npm run build
 
     cd ../../
 
-    # run the deku-cli
+    # run the deku-cli in mock mode
      deku-cli create-mock-transaction $node $wallet $cmd node examples/cookie-game/index.js
 
-    #deku-cli create-mock-transaction wallet.json '"$payload"' node \
-    #_build/default/examples/cookie-game/index.js
 }
 
 
-# run tilt
-
+# run Deku-cluster in tilt mode
 deku_cluster(){
-    # build ts project
+    # build cookie project
     cd examples/cookie-game && npm run build
 
     cd ../../
+
     # tear-down the cluster
     tilt down
+    
     # setup and start the cluster with vm cookie
     tilt up -- --mode=Local --vm="node examples/cookie-game/index.js"
 }
 
 # Run custom create
-
 custom(){
     deku-cli create-custom-transaction $node $wallet $cmd
 }
 
-
 case "$1" in 
+build_dune) 
+ build_dune
+ ;;
 build_cookie)
  build_cookie
 ;;
-build_skds)
+build_sdks)
  build_sdks
 ;;
 mock)
