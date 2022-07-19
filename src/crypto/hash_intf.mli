c47c75c80e193d4746565372a998a776b18f9d8a
EduardoRFS <theeduardorfs@gmail.com>
2022-07-19 18:22:06 -0300

    crypto: introduce Hash module specification
    Describes the specification that any hashing module should implement

---
+++ src/crypto/hash_intf.mli
@@ -0,0 +1,37 @@
+module type Alg = sig
+  type secret
+  type key
+  type signature
+
+  val sign : secret -> string -> signature
+  val verify : key -> signature -> string -> bool
+end
+
+module type S = sig
+  type hash
+  type t = hash [@@deriving show, eq, ord]
+
+  val hash : string -> hash
+
+  (* repr *)
+  val of_hex : string -> hash option
+  val to_hex : hash -> string
+
+  (* utils *)
+  val both : hash -> hash -> hash
+
+  (* TODO: better name for this*)
+  module With_alg (Alg : Alg) : sig
+    open Alg
+
+    val sign : secret -> hash -> signature
+    val verify : key -> signature -> hash -> bool
+  end
+
+  module With_b58 (_ : sig
+    val prefix : string
+  end) : sig
+    val of_b58 : string -> hash option
+    val to_b58 : hash -> string
+  end
+end
