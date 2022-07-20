9479e63228bde729a5d135980285e39b651622b2
EduardoRFS <theeduardorfs@gmail.com>
2022-07-20 09:41:56 -0300

    crypto: introduce Random module
    All the randomness primitives should be exposed here.

---
+++ src/crypto/random.ml
@@ -0,0 +1,6 @@
+let self_init () =
+  Stdlib.Random.self_init ();
+  Mirage_crypto_rng_unix.initialize ()
+
+let generate bits = Mirage_crypto_rng.generate bits
+let int32 = Stdlib.Random.int32
---
+++ src/crypto/random.mli
@@ -0,0 +1,3 @@
+val self_init : unit -> unit
+val generate : int -> Cstruct.t
+val int32 : int32 -> int32
