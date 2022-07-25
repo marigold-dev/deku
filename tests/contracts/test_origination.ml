84932dac23eaf926696a3e5f75a66d891a11c0a3
Renato Alencar <renatoalencar.73@gmail.com>
2022-07-25 07:00:44 -0300

    Wasm ticket mint testing (#743)
    * Improve WASM contract testing
    
    * Test for ticket minting

--- tests/contracts/test_origination.ml
+++ tests/contracts/test_origination.ml
@@ -2,106 +2,107 @@ open Core_deku
 open Contracts
 open Helpers_contracts
 
-let test_wasm msg =
-  let initial_state, address, ticket = setup () in
+let test_wasm () =
   let payload =
     let code =
-      {|
-      (module
-      (import "env" "syscall" (func $syscall (param i64) (result i32)))
-      (memory (export "memory") 1)
-      (func (export "main")  (param i32) (result i64 i64 i64)
-        i32.const 21
-        i32.const 5
-        i32.store
-        i32.const 22
-        i32.const 0
-        i32.store
-        i64.const 21 
-        call $syscall
-        i64.extend_i32_s
-        (i64.const 20)
-        (i64.const 45)
-        ))
-      |}
-      |> Bytes.of_string in
-    let storage = Int32.zero |> Context.Ticket_handle.to_bytes in
-
-    Contract_vm.Origination_payload.wasm_of_yojson ~code ~storage
-    |> Result.get_ok in
-
-  let operation =
-    User_operation.Contract_origination
-      {
-        payload;
-        tickets =
-          [((ticket, Amount.of_int 10000), (Int32.zero, Some Int64.zero))];
-      } in
-  let user_op = User_operation.make ~source:address operation in
-
-  let state, _ = State.apply_user_operation initial_state user_op.hash user_op in
-  [
-    Alcotest.test_case msg `Quick (fun () ->
-        Alcotest.(check' bool)
-          ~msg ~expected:false
-          ~actual:
-            (Contract_storage.equal
-               (State.contract_storage state)
-               Contract_storage.empty));
-  ]
-
-let test_wasm_fail msg =
-  let _, _, t1 = setup () in
-  let initial_state, address, _ticket = setup () in
-  let payload =
-    let code =
-      {|
+      Bytes.of_string
+        {|
         (module
-        (import "env" "syscall" (func $syscall (param i64) (result i32)))
-        (memory (export "memory") 1)
-        (func (export "main")  (param i32) (result i64 i64 i64)
-          i32.const 21
-          i32.const 5
-          i32.store
-          i32.const 22
-          i32.const 0
-          i32.store
-          i64.const 21 
-          call $syscall
-          i64.extend_i32_s
-          (i64.const 20)
-          (i64.const 45)
-          ))
-        |}
-      |> Bytes.of_string in
-    let storage = Int32.one |> Context.Ticket_handle.to_bytes in
+          (import "env" "syscall" (func $syscall (param i64) (result i32)))
+          (memory (export "memory") 1)
+          (func (export "main")  (param i32) (result i64 i64 i64)
+            i32.const 21
+            i32.const 5
+            i32.store
+            i32.const 22
+            i32.const 0
+            i32.store
+            i64.const 21 
+            call $syscall
+            i64.extend_i32_s
+            (i64.const 20)
+            (i64.const 45)
+            ))
+      |}
+    in
+    let storage = Context.Ticket_handle.to_bytes Int32.zero in
 
     Contract_vm.Origination_payload.wasm_of_yojson ~code ~storage
     |> Result.get_ok in
 
-  let operation =
-    User_operation.Contract_origination
-      {
-        payload;
-        tickets = [((t1, Amount.of_int 10000), (Int32.one, Some Int64.zero))];
-      } in
-  let user_op = User_operation.make ~source:address operation in
+  let initial_state, address, ticket = setup () in
+  let tickets =
+    [((ticket, Amount.of_int 10000), (Int32.zero, Some Int64.zero))] in
 
-  let state, _ = State.apply_user_operation initial_state user_op.hash user_op in
-  [
-    Alcotest.test_case msg `Quick (fun () ->
-        Alcotest.(check' bool)
-          ~msg ~expected:true
-          ~actual:
-            (Contract_storage.equal
-               (State.contract_storage state)
-               Contract_storage.empty));
-  ]
+  let actual =
+    let operation = User_operation.Contract_origination { payload; tickets } in
+    let user_op = User_operation.make ~source:address operation in
+
+    let state, _ =
+      State.apply_user_operation initial_state user_op.hash user_op in
+    let address = Contract_address.of_user_operation_hash user_op.hash in
+    Contract_storage.get_contract (State.contract_storage state) ~address
+    |> Option.get in
+
+  let expected =
+    compile payload tickets ~sender:(Address.of_key_hash address)
+    |> Result.get_ok in
+  Alcotest.check' contract_value
+    ~msg:"Contract should have the same value as its counterpart in storage"
+    ~actual ~expected
+
+let test_wasm_fail_on_wrong_ticket () =
+  let payload =
+    let code =
+      Bytes.of_string
+        {|
+        (module
+          (import "env" "syscall" (func $syscall (param i64) (result i32)))
+          (memory (export "memory") 1)
+          (func (export "main")  (param i32) (result i64 i64 i64)
+            i32.const 21
+            i32.const 5
+            i32.store
+            i32.const 22
+            i32.const 0
+            i32.store
+            i64.const 21 
+            call $syscall
+            i64.extend_i32_s
+            (i64.const 20)
+            (i64.const 45)
+            ))
+          |}
+    in
+    let storage = Context.Ticket_handle.to_bytes Int32.one in
+
+    Contract_vm.Origination_payload.wasm_of_yojson ~code ~storage
+    |> Result.get_ok in
+
+  let _, _, not_owned_ticket = setup () in
+  let initial_state, address, _ticket = setup () in
+  let tickets =
+    [((not_owned_ticket, Amount.of_int 10000), (Int32.one, Some Int64.zero))]
+  in
+
+  let actual =
+    let operation = User_operation.Contract_origination { payload; tickets } in
+    let user_op = User_operation.make ~source:address operation in
+
+    let state, _ =
+      State.apply_user_operation initial_state user_op.hash user_op in
+    let address = Contract_address.of_user_operation_hash user_op.hash in
+    Contract_storage.get_contract (State.contract_storage state) ~address in
+
+  Alcotest.(check' (option contract_value))
+    ~msg:"Failed origination should add no contract to storage" ~actual
+    ~expected:None
 
 let test_origination =
-  ( "Origination",
-    [
-      test_wasm "Wasm origination should succeed";
-      test_wasm_fail "Wasm origination should fail";
-    ]
-    |> List.concat )
+  Alcotest.
+    ( "Origination",
+      [
+        test_case "Wasm origination should succeed" `Quick test_wasm;
+        test_case "Wasm origination should fail due to wrong ticket handle"
+          `Quick test_wasm_fail_on_wrong_ticket;
+      ] )
