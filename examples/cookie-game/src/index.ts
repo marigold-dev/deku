8a8ef615885eb89ae0b42be88d046a8164e1cee3
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-07-27 14:10:04 +0200

    get rid of class, to use type

--- examples/cookie-game/src/index.ts
+++ examples/cookie-game/src/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import { main, get, set } from "deku_js_interop"
-import { Cookie_baker } from "./state"
+import { main, get, set, transaction } from "deku_js_interop"
+import { cookie_baker_type, create_cookie_baker, add_cookie, add_cursor, add_grandma, add_farm } from "./state"
 import { action_type } from "./actions"
 
 const print_message_with_source = (message: string, source: transaction) => {
@@ -8,7 +8,7 @@ const print_message_with_source = (message: string, source: transaction) => {
     console.log(source);
 }
 
-const save_state = (source: transaction, source_value: Cookie_baker) => {
+const save_state = (source: transaction, source_value: cookie_baker_type) => {
     print_message_with_source("Saving state", source_value);
     set(source, source_value);
     console.log("Successfully saved state");
@@ -22,50 +22,49 @@ const transition = (tx: transaction) => {
     const operation = tx.operation;
     console.log("Getting source");
     const source_value = JSON.parse(get(source));
-    const cookie_baker =
-        new Cookie_baker(
-            source_value.cookie_baker.number_of_cookie,
-            source_value.cookie_baker.number_of_cursor,
-            source_value.cookie_baker.number_of_grandma,
-            source_value.cookie_baker.number_of_farm,
-            source_value.cookie_baker.number_of_free_cursor,
-            source_value.cookie_baker.number_of_free_grandma,
-            source_value.cookie_baker.number_of_free_farm
-        );
+    const cookie_baker: cookie_baker_type = create_cookie_baker(
+        source_value.cookie_baker.number_of_cookie,
+        source_value.cookie_baker.number_of_cursor,
+        source_value.cookie_baker.number_of_grandma,
+        source_value.cookie_baker.number_of_farm,
+        source_value.cookie_baker.number_of_free_cursor,
+        source_value.cookie_baker.number_of_free_grandma,
+        source_value.cookie_baker.number_of_free_farm
+    );
 
 
     switch (operation) {
         case action_type.increment_cookie: {
-            cookie_baker.add_cookie();
+            const updated_cookie_baker = add_cookie(cookie_baker);
             //update state
-            source_value.cookie_baker = cookie_baker;
+            source_value.cookie_baker = updated_cookie_baker;
             console.log("Successfully minted cookie");
             save_state(source, source_value);
             break;
         }
         case action_type.increment_cursor: {
-            cookie_baker.add_cursor();
+            const updated_cookie_baker = add_cursor(cookie_baker);
 
             //action successful, update state
-            source_value.cookie_baker = cookie_baker;
+            source_value.cookie_baker = updated_cookie_baker;
             console.log("Successfully minted cursor");
             save_state(source, source_value);
             break;
         }
         case action_type.increment_grandma: {
-            cookie_baker.add_grandma();
+            const updated_cookie_baker = add_grandma(cookie_baker);
 
             //action successful, update state
-            source_value.cookie_baker = cookie_baker;
+            source_value.cookie_baker = updated_cookie_baker;
             console.log("Successfully minted grandma");
             save_state(source, source_value);
             break;
         }
         case action_type.increment_farm: {
-            cookie_baker.add_farm();
+            const updated_cookie_baker = add_farm(cookie_baker);
 
             //action successful, update state
-            source_value.cookie_baker = cookie_baker;
+            source_value.cookie_baker = updated_cookie_baker;
             console.log("Successfully minted farm");
             save_state(source, source_value);
             break;
