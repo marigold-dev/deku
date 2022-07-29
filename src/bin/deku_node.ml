d8775c506059d99c444af48bee1b7f59a3f4646a
Daniel Hines <d4hines@gmail.com>
2022-07-29 09:44:19 -0400

    feat: /identity endpoint

--- src/bin/deku_node.ml
+++ src/bin/deku_node.ml
@@ -397,6 +397,13 @@ let handle_bootstrap_signal =
       let%ok payload = Consensus.Bootstrapper_signal.make ~producer ~signature in
       Flows.handle_bootstrap_signal ~payload |> ok)
 
+let handle_identity =
+  handle_request
+    (module Network.Identity)
+    (fun () ->
+      let state = Server.get_state () in
+      Ok state.config.identity.t)
+
 let node folder port minimum_block_delay prometheus_port =
   let node =
     Node_state.get_initial_state ~folder ~minimum_block_delay |> Lwt_main.run
@@ -431,6 +438,7 @@ let node folder port minimum_block_delay prometheus_port =
              handle_trusted_validators_membership;
              handle_in_sync;
              handle_bootstrap_signal;
+             handle_identity;
            ];
       Prometheus_dream.serve prometheus_port;
     ]
