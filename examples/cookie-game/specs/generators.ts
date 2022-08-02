77453be88780bdb97bdf75dbbd1d38b3018724a7
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-08-02 15:43:42 +0200

    snake_case to camelCase

--- examples/cookie-game/specs/generators.ts
+++ examples/cookie-game/specs/generators.ts
@@ -1,35 +1,29 @@
 import * as fc from 'fast-check';
-import { create_cookie_baker } from '../src/state';
+import { createCookieBaker } from '../src/state';
 
-export const cookie_baker_arbitrary = () =>
+export const cookieBakerArbitrary = () =>
     fc.tuple(fc.integer({ min: 0, max: 2000000000 }),
         fc.integer({ min: 0, max: 200 }),
         fc.integer({ min: 0, max: 200 }),
         fc.integer({ min: 0, max: 200 }),
         fc.integer({ min: 0, max: 10 }),
         fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
-        fc.integer({ min: 0, max: 10 }),
         fc.integer({ min: 0, max: 10 })).map(
-            ([number_of_cookie,
-                number_of_cursor,
-                number_of_grandma,
-                number_of_farm,
-                number_of_free_cursor,
-                number_of_free_grandma,
-                number_of_free_farm,
+            ([numberOfCookie,
+                numberOfCursor,
+                numberOfGrandma,
+                numberOfFarm,
+                numberOfFreeCursor,
+                numberOfFreeGrandma,
+                numberOfFreeFarm,
             ]) => {
-                const cookie_baker = create_cookie_baker(
-                    number_of_cookie,
-                    number_of_cursor,
-                    number_of_grandma,
-                    number_of_farm,
-                    number_of_free_cursor,
-                    number_of_free_grandma,
-                    number_of_free_farm)
+                const cookie_baker = createCookieBaker(
+                    numberOfCookie,
+                    numberOfCursor,
+                    numberOfGrandma,
+                    numberOfFarm,
+                    numberOfFreeCursor,
+                    numberOfFreeGrandma,
+                    numberOfFreeFarm)
                 return cookie_baker;
             });
