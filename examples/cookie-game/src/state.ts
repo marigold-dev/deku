e7b9e0e065a1b54e353bd9fc57aa034bbff25ed0
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-08-02 15:43:42 +0200

    snake_case to camelCase

--- examples/cookie-game/src/state.ts
+++ examples/cookie-game/src/state.ts
@@ -1,154 +1,154 @@
-import { action_type } from "./actions"
+import { actions } from "./actions"
 
 export const initial_cursor_cps: number = 0.1;
-export const initial_grandma_cps: number = 1;
-export const initial_farm_cps: number = 8;
+export const initial_grandmaCps: number = 1;
+export const initial_farmCps: number = 8;
 
-export const initial_cursor_cost: number = 15;
-export const initial_grandma_cost: number = 100;
-export const initial_farm_cost: number = 1100;
+export const initial_cursorCost: number = 15;
+export const initial_grandmaCost: number = 100;
+export const initial_farmCost: number = 1100;
 
 
-export type cookie_baker_type = {
-    number_of_cookie: number;
-    number_of_cursor: number;
-    number_of_grandma: number;
-    number_of_farm: number;
+export type cookieBaker = {
+    numberOfCookie: number;
+    numberOfCursor: number;
+    numberOfGrandma: number;
+    numberOfFarm: number;
 
     /* Gift from application */
     /* TODO: add the rule to generate them! */
-    number_of_free_cursor: number;
-    number_of_free_grandma: number;
-    number_of_free_farm: number;
+    numberOfFreeCursor: number;
+    numberOfFreeGrandma: number;
+    numberOfFreeFarm: number;
 
-    cursor_cost: number;
-    grandma_cost: number;
-    farm_cost: number;
+    cursorCost: number;
+    grandmaCost: number;
+    farmCost: number;
 
     /* Cookie per second*/
-    cursor_cps: number;
-    grandma_cps: number;
-    farm_cps: number;
+    cursorCps: number;
+    grandmaCps: number;
+    farmCps: number;
 }
 
-export const create_cookie_baker = (number_of_cookie: number,
-    number_of_cursor: number,
-    number_of_grandma: number,
-    number_of_farm: number,
-    number_of_free_cursor: number,
-    number_of_free_grandma: number,
-    number_of_free_farm: number,): cookie_baker_type => {
+export const createCookieBaker = (numberOfCookie: number,
+    numberOfCursor: number,
+    numberOfGrandma: number,
+    numberOfFarm: number,
+    numberOfFreeCursor: number,
+    numberOfFreeGrandma: number,
+    numberOfFreeFarm: number,): cookieBaker => {
     const cookie_baker = {
-        number_of_cookie,
-        number_of_cursor,
-        number_of_grandma,
-        number_of_farm,
-        number_of_free_cursor,
-        number_of_free_grandma,
-        number_of_free_farm,
-        cursor_cost: 0,
-        grandma_cost: 0,
-        farm_cost: 0,
-        cursor_cps: 0,
-        grandma_cps: 0,
-        farm_cps: 0,
+        numberOfCookie,
+        numberOfCursor,
+        numberOfGrandma,
+        numberOfFarm,
+        numberOfFreeCursor,
+        numberOfFreeGrandma,
+        numberOfFreeFarm,
+        cursorCost: 0,
+        grandmaCost: 0,
+        farmCost: 0,
+        cursorCps: 0,
+        grandmaCps: 0,
+        farmCps: 0,
     }
-    const cursor_cost = calculate_cost(action_type.increment_cursor, cookie_baker);
-    const grandma_cost = calculate_cost(action_type.increment_grandma, cookie_baker);
-    const farm_cost = calculate_cost(action_type.increment_farm, cookie_baker);
-    const cursor_cps = cookie_baker.number_of_cursor * initial_cursor_cps;
-    const grandma_cps = cookie_baker.number_of_grandma * initial_grandma_cps;
-    const farm_cps = cookie_baker.number_of_farm * initial_farm_cps;
-    cookie_baker.cursor_cost = cursor_cost;
-    cookie_baker.grandma_cost = grandma_cost;
-    cookie_baker.farm_cost = farm_cost;
-    cookie_baker.cursor_cps = cursor_cps;
-    cookie_baker.grandma_cps = grandma_cps;
-    cookie_baker.farm_cps = farm_cps;
+    const cursorCost = calculateCost(actions.incrementCursor, cookie_baker);
+    const grandmaCost = calculateCost(actions.incrementGrandma, cookie_baker);
+    const farmCost = calculateCost(actions.incrementFarm, cookie_baker);
+    const cursor_cps = cookie_baker.numberOfCursor * initial_cursor_cps;
+    const grandmaCps = cookie_baker.numberOfGrandma * initial_grandmaCps;
+    const farmCps = cookie_baker.numberOfFarm * initial_farmCps;
+    cookie_baker.cursorCost = cursorCost;
+    cookie_baker.grandmaCost = grandmaCost;
+    cookie_baker.farmCost = farmCost;
+    cookie_baker.cursorCps = cursor_cps;
+    cookie_baker.grandmaCps = grandmaCps;
+    cookie_baker.farmCps = farmCps;
     return cookie_baker;
 }
 
-export const calculate_cost = (action: action_type, cookie_baker: cookie_baker_type): number => {
+export const calculateCost = (action: actions, cookie_baker: cookieBaker): number => {
     switch (action) {
-        case action_type.increment_cookie:
+        case actions.incrementCookie:
             console.log("Cookie does not have cost");
             throw new Error("Cookie does not have cost");
-        case action_type.increment_cursor:
-            console.log("Calculating price for next cursor, actual price is: " + cookie_baker.cursor_cost);
-            const new_cursor_price = Math.floor(initial_cursor_cost * Math.pow(1.15, cookie_baker.number_of_cursor - cookie_baker.number_of_free_cursor));
+        case actions.incrementCursor:
+            console.log("Calculating price for next cursor, actual price is: " + cookie_baker.cursorCost);
+            const new_cursor_price = Math.floor(initial_cursorCost * Math.pow(1.15, cookie_baker.numberOfCursor - cookie_baker.numberOfFreeCursor));
             console.log("New cursor price is: " + new_cursor_price);
             return new_cursor_price;
-        case action_type.increment_grandma:
-            console.log("Calculating price for next grandma, actual price is: " + cookie_baker.grandma_cost);
-            const new_grandma_price = Math.floor(initial_grandma_cost * Math.pow(1.15, cookie_baker.number_of_grandma - cookie_baker.number_of_free_grandma));
+        case actions.incrementGrandma:
+            console.log("Calculating price for next grandma, actual price is: " + cookie_baker.grandmaCost);
+            const new_grandma_price = Math.floor(initial_grandmaCost * Math.pow(1.15, cookie_baker.numberOfGrandma - cookie_baker.numberOfFreeGrandma));
             console.log("New grandma price is: " + new_grandma_price);
             return new_grandma_price;
-        case action_type.increment_farm:
-            console.log("Calculating price for next farm, actual price is: " + cookie_baker.farm_cost);
-            const new_farm_price = Math.floor(initial_farm_cost * Math.pow(1.15, cookie_baker.number_of_farm - cookie_baker.number_of_free_farm));
+        case actions.incrementFarm:
+            console.log("Calculating price for next farm, actual price is: " + cookie_baker.farmCost);
+            const new_farm_price = Math.floor(initial_farmCost * Math.pow(1.15, cookie_baker.numberOfFarm - cookie_baker.numberOfFreeFarm));
             console.log("New farm price is: " + new_farm_price);
             return new_farm_price;
 
     }
 }
 
-export const add_cookie = (cookie_baker: cookie_baker_type): cookie_baker_type => {
-    console.log("Adding cookie: " + cookie_baker.number_of_cookie);
-    cookie_baker.number_of_cookie = cookie_baker.number_of_cookie + 1;
-    console.log("Successfully added cookie: " + cookie_baker.number_of_cookie);
+export const addCookie = (cookie_baker: cookieBaker): cookieBaker => {
+    console.log("Adding cookie: " + cookie_baker.numberOfCookie);
+    cookie_baker.numberOfCookie = cookie_baker.numberOfCookie + 1;
+    console.log("Successfully added cookie: " + cookie_baker.numberOfCookie);
     return cookie_baker;
 }
 
-export const add_cursor = (cookie_baker: cookie_baker_type): cookie_baker_type => {
-    if (cookie_baker.number_of_cookie >= cookie_baker.cursor_cost) {
+export const addCursor = (cookie_baker: cookieBaker): cookieBaker => {
+    if (cookie_baker.numberOfCookie >= cookie_baker.cursorCost) {
         console.log("Enough cookie to buy a cursor");
         // adding cursor
-        cookie_baker.number_of_cursor = cookie_baker.number_of_cursor + 1;
+        cookie_baker.numberOfCursor = cookie_baker.numberOfCursor + 1;
         // removing cursor cost
-        cookie_baker.number_of_cookie = cookie_baker.number_of_cookie - cookie_baker.cursor_cost;
+        cookie_baker.numberOfCookie = cookie_baker.numberOfCookie - cookie_baker.cursorCost;
         // calculating next cursor price
-        cookie_baker.cursor_cost = calculate_cost(action_type.increment_cursor, cookie_baker);
+        cookie_baker.cursorCost = calculateCost(actions.incrementCursor, cookie_baker);
         // calculate new cps
-        cookie_baker.cursor_cps = cookie_baker.number_of_cursor * initial_cursor_cps;
+        cookie_baker.cursorCps = cookie_baker.numberOfCursor * initial_cursor_cps;
         return cookie_baker;
     } else {
-        console.log("Not enough cookie to buy a cursor, needed: " + cookie_baker.cursor_cost + " actual amount: " + cookie_baker.number_of_cookie);
+        console.log("Not enough cookie to buy a cursor, needed: " + cookie_baker.cursorCps + " actual amount: " + cookie_baker.numberOfCookie);
         return cookie_baker;
     }
 }
 
-export const add_grandma = (cookie_baker: cookie_baker_type): cookie_baker_type => {
-    if (cookie_baker.number_of_cookie >= cookie_baker.grandma_cost) {
+export const addGrandma = (cookie_baker: cookieBaker): cookieBaker => {
+    if (cookie_baker.numberOfCookie >= cookie_baker.grandmaCost) {
         console.log("Enough cookie to buy a grandma");
         // adding grandma
-        cookie_baker.number_of_grandma = cookie_baker.number_of_grandma + 1;
+        cookie_baker.numberOfGrandma = cookie_baker.numberOfGrandma + 1;
         // removing grandma cost
-        cookie_baker.number_of_cookie = cookie_baker.number_of_cookie - cookie_baker.grandma_cost;
+        cookie_baker.numberOfCookie = cookie_baker.numberOfCookie - cookie_baker.grandmaCost;
         // calculating next grandma price
-        cookie_baker.grandma_cost = calculate_cost(action_type.increment_grandma, cookie_baker);
+        cookie_baker.grandmaCost = calculateCost(actions.incrementGrandma, cookie_baker);
         // calculate new cps
-        cookie_baker.grandma_cps = cookie_baker.number_of_grandma * initial_grandma_cps;
+        cookie_baker.grandmaCps = cookie_baker.numberOfGrandma * initial_grandmaCps;
         return cookie_baker;
     } else {
-        console.log("Not enough cookie to buy a grandma, needed: " + cookie_baker.grandma_cost + " actual amount: " + cookie_baker.number_of_cookie);
+        console.log("Not enough cookie to buy a grandma, needed: " + cookie_baker.grandmaCost + " actual amount: " + cookie_baker.numberOfCookie);
         return cookie_baker;
     }
 }
 
-export const add_farm = (cookie_baker: cookie_baker_type): cookie_baker_type => {
-    if (cookie_baker.number_of_cookie >= cookie_baker.farm_cost) {
+export const addFarm = (cookie_baker: cookieBaker): cookieBaker => {
+    if (cookie_baker.numberOfCookie >= cookie_baker.farmCost) {
         console.log("Enough cookie to buy a farm");
         // adding farm
-        cookie_baker.number_of_farm = cookie_baker.number_of_farm + 1;
+        cookie_baker.numberOfFarm = cookie_baker.numberOfFarm + 1;
         // removing farm cost
-        cookie_baker.number_of_cookie = cookie_baker.number_of_cookie - cookie_baker.farm_cost;
+        cookie_baker.numberOfCookie = cookie_baker.numberOfCookie - cookie_baker.farmCost;
         // calculating next farm price
-        cookie_baker.farm_cost = calculate_cost(action_type.increment_farm, cookie_baker);
+        cookie_baker.farmCost = calculateCost(actions.incrementFarm, cookie_baker);
         // calculate new cps
-        cookie_baker.farm_cps = cookie_baker.number_of_farm * initial_farm_cps;
+        cookie_baker.farmCps = cookie_baker.numberOfFarm * initial_farmCps;
         return cookie_baker;
     } else {
-        console.log("Not enough cookie to buy a farm, needed: " + cookie_baker.farm_cost + " actual amount: " + cookie_baker.number_of_cookie);
+        console.log("Not enough cookie to buy a farm, needed: " + cookie_baker.farmCost + " actual amount: " + cookie_baker.numberOfCookie);
         return cookie_baker;
     }
 }
