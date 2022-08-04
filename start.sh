c02c5d54060050f72604afd0d07a2058246343bf
Daniel Hines <d4hines@gmail.com>
2022-08-04 13:37:45 -0400

    add bootstrapper

--- start.sh
+++ start.sh
@@ -13,6 +13,10 @@ do
     sleep 0.1
 done
 
-(cd chain && dune exec deku-bootstrap -- bootstrap)
+
+export DEKU_BOOTSTRAP_SECRET="edsk2jS9cPs3Tgp6T34NQvPPPUPGYtGysCHS569FgDAKmdkKutQHUw"
+export DEKU_VALIDATOR_URIS="http://localhost:4440,http://localhost:4441,http://localhost:4442,http://localhost:4443"
+export DEKU_VALIDATORS="tz1fpf9DffkGAnzT6UKMDoS4hZjNmoEKhGsK,tz1PYdVbnLwiqKo3fLFXTKxw6K7BhpddQPh8,tz1Pv4viWq7ye4R6cr9SKR3tXiZGvpK34SKi,tz1cXKCCxLwYCHDSrx9hfD5Qmbs4W8w2UKDw"
+(cd chain && dune exec deku-bootstrap)
 
 wait
