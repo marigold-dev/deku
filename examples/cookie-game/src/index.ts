233b7df0c9fe38a435c1bc4f7e2bc2c95d614296
lykimq <lykimq@gmail.com>
2022-08-04 14:39:34 +0200

    add the rest of levels, todo: fix error

--- examples/cookie-game/src/index.ts
+++ examples/cookie-game/src/index.ts
@@ -30,12 +30,39 @@ const transition = (tx: transaction) => {
         sourceValue.cookieBaker.numberOfMine,
         sourceValue.cookieBaker.numberOfFactory,
         sourceValue.cookieBaker.numberOfBank,
+        sourceValue.cookieBaker.numberOfTemple,
+        sourceValue.cookieBaker.numberOfWizard,
+        sourceValue.cookieBaker.numberOfShipment,
+        sourceValue.cookieBaker.numberOfAlchemy,
+        sourceValue.cookieBaker.numberOfPortal,
+        sourceValue.cookieBaker.numberOfTimeMachine,
+        sourceValue.cookieBaker.numberOfAntimatter,
+        sourceValue.cookieBaker.numberOfPrism,
+        sourceValue.cookieBaker.numberOfChanceMaker,
+        sourceValue.cookieBaker.numberOfFractal,
+        sourceValue.cookieBaker.numberOfJavaScript,
+        sourceValue.cookieBaker.numberOfIdleverse,
+        sourceValue.cookieBaker.numberOfCordex,
+
         sourceValue.cookieBaker.numberOfFreeCursor,
         sourceValue.cookieBaker.numberOfFreeGrandma,
         sourceValue.cookieBaker.numberOfFreeFarm,
         sourceValue.cookieBaker.numberOfFreeMine,
         sourceValue.cookieBaker.numberOfFreeFactory,
         sourceValue.cookieBaker.numberOfFreeBank,
+        sourceValue.cookieBaker.numberOfFreeTemple,
+        sourceValue.cookieBaker.numberOfFreeWizard,
+        sourceValue.cookieBaker.numberOfFreeShipment,
+        sourceValue.cookieBaker.numberOfFreeAlchemy,
+        sourceValue.cookieBaker.numberOfFreePortal,
+        sourceValue.cookieBaker.numberOfFreeTimeMachine,
+        sourceValue.cookieBaker.numberOfFreeAntimatter,
+        sourceValue.cookieBaker.numberOfFreePrism,
+        sourceValue.cookieBaker.numberOfFreeChanceMaker,
+        sourceValue.cookieBaker.numberOfFreeFractal,
+        sourceValue.cookieBaker.numberOfFreeJavaScript,
+        sourceValue.cookieBaker.numberOfFreeIdleverse,
+        sourceValue.cookieBaker.numberOfFreeCordex,
     );
 
 
@@ -99,6 +126,15 @@ const transition = (tx: transaction) => {
             //action successful, update state
             sourceValue.cookieBaker = updatedCookieBaker;
             console.log("Successfully minted bank");
+            saveState(source, sourceValue);
+            break;
+        }
+        case actions.incrementTemple: {
+            const updatedCookieBaker = addTemple(cookieBaker);
+
+            //action successful, update state
+            sourceValue.cookieBaker = updatedCookieBaker;
+            console.log("Successfully minted temple");
             saveState(source, sourceValue);
             break;
         }
