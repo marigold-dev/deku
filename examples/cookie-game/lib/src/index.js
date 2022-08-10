03a6a113c74f648f23237aff11a24013bcad7466
Gauthier SEBILLE <gauthier.sebille@gmail.com>
2022-08-10 10:35:51 +0200

    remove logs

--- examples/cookie-game/lib/src/index.js
+++ examples/cookie-game/lib/src/index.js
@@ -6,7 +6,7 @@ var state_1 = require("./state");
 var actions_1 = require("./actions");
 var saveState = function (source, sourceValue) {
     (0, deku_js_interop_1.set)(source, sourceValue);
-    console.log("Successfully saved state: " + JSON.stringify(sourceValue));
+    console.log("Successfully saved state");
 };
 var transition = function (tx) {
     // source -> tz1 address
@@ -14,20 +14,16 @@ var transition = function (tx) {
     // operation => any
     var source = tx.source;
     var operation = tx.operation;
-    console.log("source: " + source);
     var sourceValue = JSON.parse((0, deku_js_interop_1.get)(source));
-    console.log("sourceValue: " + sourceValue);
     var cookieBaker;
     if (sourceValue === undefined || sourceValue === null) {
         sourceValue = { source: source };
-        console.log("sourceValue: " + sourceValue);
         cookieBaker = (0, state_1.createCookieBaker)(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
     else {
         cookieBaker = (0, state_1.createCookieBaker)(sourceValue.cookieBaker.cookies, sourceValue.cookieBaker.cursors, sourceValue.cookieBaker.grandmas, sourceValue.cookieBaker.farms, sourceValue.cookieBaker.mines, sourceValue.cookieBaker.factories, sourceValue.cookieBaker.banks, sourceValue.cookieBaker.temples, sourceValue.cookieBaker.wizards, sourceValue.cookieBaker.shipments, sourceValue.cookieBaker.alchemies, sourceValue.cookieBaker.portals, sourceValue.cookieBaker.timeMachines, sourceValue.cookieBaker.antimatters, sourceValue.cookieBaker.prisms, sourceValue.cookieBaker.chanceMakers, sourceValue.cookieBaker.fractals, sourceValue.cookieBaker.javaScripts, sourceValue.cookieBaker.idleverses, sourceValue.cookieBaker.cordexs, sourceValue.cookieBaker.freeCursor, sourceValue.cookieBaker.freeGrandma, sourceValue.cookieBaker.freeFarm, sourceValue.cookieBaker.freeMine, sourceValue.cookieBaker.freeFactory, sourceValue.cookieBaker.freeBank, sourceValue.cookieBaker.freeTemple, sourceValue.cookieBaker.freeWizard, sourceValue.cookieBaker.freeShipment, sourceValue.cookieBaker.freeAlchemy, sourceValue.cookieBaker.freePortal, sourceValue.cookieBaker.freeTimeMachine, sourceValue.cookieBaker.freeAntimatter, sourceValue.cookieBaker.freePrism, sourceValue.cookieBaker.freeChanceMaker, sourceValue.cookieBaker.freeFractal, sourceValue.cookieBaker.FreeJavaScript, sourceValue.cookieBaker.freeIdleverse, sourceValue.cookieBaker.freeCordex);
     }
     sourceValue.cookieBaker = cookieBaker;
-    console.log("sourceValue.cookieBaker: " + JSON.stringify(sourceValue.cookieBaker));
     switch (operation) {
         case actions_1.actions.incr_Cookie: {
             //update state
