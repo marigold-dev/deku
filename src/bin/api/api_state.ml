36d3b39792f2b35298f7ad4732f9f9edbade9d89
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-06 16:44:28 +0200

    feat/api: get chain info endpoint

--- src/bin/api/api_state.ml
+++ src/bin/api/api_state.ml
@@ -1,14 +1,15 @@
 open Deku_indexer
 open Deku_stdlib
+open Deku_tezos
 
-type t = { indexer : Indexer.t }
+type t = { indexer : Indexer.t; consensus : Address.t; discovery : Address.t }
 
 let state : t option ref = ref None
 
-let make ~database_uri =
-  let config = Indexer.{ save_blocks = true; save_messages = true } in
+let make ~database_uri ~consensus ~discovery =
+  let config = Indexer.{ save_messages = true; save_blocks = true } in
   let%await indexer = Indexer.make ~uri:database_uri ~config in
-  state := Some { indexer };
+  state := Some { indexer; discovery; consensus };
   Lwt.return ()
 
 let get_state () =
