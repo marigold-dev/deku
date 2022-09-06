df3769746df172e2eb5e0976c5c911af1445aa92
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-06 14:26:43 +0200

    nix: add dream to dependencies

--- nix/default.nix
+++ nix/default.nix
@@ -33,6 +33,7 @@ with ocamlPackages; buildDunePackage rec {
     caqti-lwt
     caqti-driver-sqlite3
     ppx_deriving_cmdliner
+    dream
   ]
   # checkInputs are here because when cross compiling dune needs test dependencies
   # but they are not available for the build phase. The issue can be seen by adding strictDeps = true;.
