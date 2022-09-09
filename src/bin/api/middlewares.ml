e1b227fba6f94c59b9b63ce7af5401753bc6178b
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-09 17:09:50 +0200

    feat/api: handle cache with http headers

--- src/bin/api/middlewares.ml
+++ src/bin/api/middlewares.ml
@@ -7,3 +7,10 @@ let cors handler req =
   Dream.add_header response "Access-Control-Allow-Headers" "*";
   Dream.add_header response "Allow" "*";
   Lwt.return response
+
+(* Adds header to prevent cache on some get routes *)
+let no_cache handler req =
+  let%await response = handler req in
+  print_endline "add Cache-Control";
+  Dream.add_header response "Cache-Control" "max-age=0, no-cache, no-store";
+  Lwt.return response
