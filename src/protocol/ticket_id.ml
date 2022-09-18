a54bda8eaa90c4cb9ea2ab23bd1f4f5881efc909
aguillon <arthur@marigold.dev>
2022-09-18 17:38:33 +0200

    String conversion for ticket_ids

--- src/protocol/ticket_id.ml
+++ src/protocol/ticket_id.ml
@@ -12,4 +12,36 @@ let from_tezos_ticket tezos_ticket =
   | Deku_tezos.Address.Originated { contract; _ } ->
       Ok (Ticket_id { ticketer = contract; data })
 
+let of_string string =
+  (* TODO: support Deku tickets *)
+  let tezos_ticket =
+    Deku_tezos.Ticket_id.of_string string
+    |> Option.to_result ~none:`Cannot_parse
+  in
+  Result.bind tezos_ticket from_tezos_ticket
+
+let to_string t =
+  let open Tezos_micheline in
+  match t with
+  | Ticket_id { ticketer; data } ->
+      let loc =
+        let open Micheline_printer in
+        { comment = None }
+      in
+      let address =
+        Deku_tezos.Address.(
+          Originated { contract = ticketer; entrypoint = None })
+      in
+      let micheline =
+        Micheline.Prim
+          ( loc,
+            "Pair",
+            [
+              String (loc, Deku_tezos.Address.to_string address);
+              Bytes (loc, data);
+            ],
+            [] )
+      in
+      Format.asprintf "%a" Micheline_printer.print_expr micheline
+
 (* TODO: add Deku tickets back from v0 *)
