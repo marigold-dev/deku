b11018e42ac3d9d6c97c84bcd9d805dcc78fbc17
EduardoRFS <theeduardorfs@gmail.com>
2022-09-20 19:11:53 -0300

    fix writing to disk

--- src/storage/storage.mli
+++ src/storage/storage.mli
@@ -2,6 +2,6 @@ open Deku_stdlib
 open Deku_chain
 
 module Chain : sig
-  val read : file:string -> Chain.t option Lwt.t
-  val write : pool:Parallel.Pool.t -> file:string -> Chain.t -> unit Lwt.t
+  val read : folder:string -> Chain.t option Lwt.t
+  val write : pool:Parallel.Pool.t -> folder:string -> Chain.t -> unit Lwt.t
 end
