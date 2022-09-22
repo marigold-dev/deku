0b69233cc498334b8003a511730b748bfecef494
Daniel Hines <d4hines@gmail.com>
2022-09-21 22:16:07 -0400

    fix: start decookies vm in start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,22 @@ tezos-client() {
   docker exec -t deku_flextesa tezos-client "$@"
 }
 
+# Rebuild the sdk
+cd sdks/typescript-sdk
+echo $PWD
+npm i > /dev/null
+npm run build > /dev/null
+cd ../../
+echo $PWD
+
+# Rebuild the library
+cd examples/cookie-game/
+echo $PWD
+npm i > /dev/null
+npm run build > /dev/null
+cd ../../
+echo $PWD
+
 export DEKU_VALIDATORS="tz1fpf9DffkGAnzT6UKMDoS4hZjNmoEKhGsK,tz1PYdVbnLwiqKo3fLFXTKxw6K7BhpddQPh8,tz1Pv4viWq7ye4R6cr9SKR3tXiZGvpK34SKi,tz1cXKCCxLwYCHDSrx9hfD5Qmbs4W8w2UKDw"
 export DEKU_VALIDATOR_URIS="http://localhost:4440,http://localhost:4441,http://localhost:4442,http://localhost:4443"
 export DEKU_TEZOS_SECRET="edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq"
@@ -18,11 +34,25 @@ export DEKU_API_PORT=8080
 
 for N in 0 1 2 3; do
   source "./chain/data/$N/env"
+
+  # Creates the FIFO
+  test -p "./chain/data/$N/pipe_write" || mkfifo "./chain/data/$N/pipe_write"
+  test -p "./chain/data/$N/pipe_read" || mkfifo "./chain/data/$N/pipe_read"
+
+  # Starts the VM
+  node examples/cookie-game/lib/src/index.js "./chain/data/$N/pipe" |&
+  awk -v n=$N '{ print "vm " n ": " $0}' &
+
+  sleep 2
+
+  # Starts the Node
   _build/install/default/bin/deku-node \
+    --default-block-size=100 \
     --port "444$N" \
     --database-uri "sqlite3:./chain/data/$N/database.db" \
+    --named-pipe-path "./chain/data/$N/pipe" \
     --data-folder "./chain/data/$N" |&
-      awk -v n=$N '{ print "node " n ": " $0}' &
+    awk -v n=$N '{ print "node " n ": " $0}' &
   sleep 0.1
 done
 
