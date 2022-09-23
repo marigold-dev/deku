65c61eaf727b2dc275164d35fcebbc5ae2707676
Daniel Hines <d4hines@gmail.com>
2022-09-23 19:45:16 -0400

    infra: package cookie game with nix

--- nix/service.nix
+++ nix/service.nix
@@ -28,41 +28,95 @@ in
   };
 
   config = mkIf cfg.enable {
-    systemd.services.deku-node = {
-      description = "Deku node";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.nodejs-16_x ];
-      environment = cfg.environment;
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${deku-packages.${config.nixpkgs.system}.default}/bin/deku-node";
-        Restart = "on-failure";
-        StateDirectory = "deku";
-        MemoryMax = "55G";
+    systemd = {
+      services = {
+        deku-node = {
+          description = "Deku node";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          path = [ pkgs.nodejs-16_x ];
+          environment = cfg.environment;
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${deku-packages.${config.nixpkgs.system}.default}/bin/deku-node --named-pipe-path /run/deku/pipe";
+            Restart = "on-failure";
+            StateDirectory = "deku";
+            RuntimeDirectory = "deku";
+            RuntimeDirectoryPreserve = "yes";
+            MemoryMax = "55G";
 
-        # Basic Hardening
-        NoNewPrivileges = "yes";
-        PrivateTmp = "yes";
-        PrivateDevices = "yes";
-        DevicePolicy = "closed";
-        DynamicUser = "true";
-        ProtectSystem = "strict";
-        ProtectHome = "read-only";
-        ProtectControlGroups = "yes";
-        ProtectKernelModules = "yes";
-        ProtectKernelTunables = "yes";
-        RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
-        RestrictNamespaces = "yes";
-        RestrictRealtime = "yes";
-        RestrictSUIDSGID = "yes";
-        MemoryDenyWriteExecute = "no";
-        LockPersonality = "yes";
+            # Basic Hardening
+            # NoNewPrivileges = "yes";
+            # PrivateTmp = "yes";
+            # PrivateDevices = "yes";
+            # DevicePolicy = "closed";
+            # DynamicUser = "true";
+            # ProtectSystem = "strict";
+            # ProtectHome = "read-only";
+            # ProtectControlGroups = "yes";
+            # ProtectKernelModules = "yes";
+            # ProtectKernelTunables = "yes";
+            # RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+            # RestrictNamespaces = "yes";
+            # RestrictRealtime = "yes";
+            # RestrictSUIDSGID = "yes";
+            # MemoryDenyWriteExecute = "no";
+            # LockPersonality = "yes";
+          };
+        };
+
+        deku-cookie-vm = {
+          description = "Deku cookie VM";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          before = [ "deku-node.service" ];
+          path = [ pkgs.nodejs-16_x ];
+          environment = cfg.environment;
+          serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.nodejs-16_x}/bin/node ${deku-packages.${config.nixpkgs.system}.cookie-game}/lib/node_modules/cookie-game/lib/src/index.js /run/deku/pipe";
+            Restart = "on-failure";
+            StateDirectory = "deku";
+            RuntimeDirectory = "deku";
+            RuntimeDirectoryPreserve = "yes";
+
+            # Basic Hardening
+            # NoNewPrivileges = "yes";
+            # PrivateTmp = "yes";
+            # PrivateDevices = "yes";
+            # DevicePolicy = "closed";
+            # DynamicUser = "true";
+            # ProtectSystem = "strict";
+            # ProtectHome = "read-only";
+            # ProtectControlGroups = "yes";
+            # ProtectKernelModules = "yes";
+            # ProtectKernelTunables = "yes";
+            # RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+            # RestrictNamespaces = "yes";
+            # RestrictRealtime = "yes";
+            # RestrictSUIDSGID = "yes";
+            # MemoryDenyWriteExecute = "no";
+            # LockPersonality = "yes";
+          };
+        };
+      };
+
+      sockets = {
+        deku-vm = {
+          description = "Sockets to communicate between Deku and VM";
+          unitConfig = { RequiresMountsFor = "/run/deku"; };
+          socketConfig = { ListenFIFO = [ "/run/deku/pipe_read" "/run/deku/pipe_write" ]; };
+          before = [ "deku-node.service" "deku-cookie-vm.service" ];
+        };
       };
     };
 
     networking.firewall = mkIf cfg.openFirewall {
-      allowedTCPPorts = [ cfg.port ];
+      allowedTCPPorts = [
+        cfg.port
+        # FIXME: stop hard coding this
+        8080
+      ];
     };
   };
 }
