db0a5a730d17711f296355e1cd2d71e73560234f
aguillon <arthur@marigold.dev>
2022-09-24 16:12:36 +0200

    Using two wallets to prevent "one manager operation per block" error

--- scripts/deploy_contracts.sh
+++ scripts/deploy_contracts.sh
@@ -7,6 +7,9 @@ source $dir/common.sh
 # We need this secret key for sigining Tezos operations.
 SECRET_KEY="edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq"
 
+# tz1SZtpiT7KAdwZ23PDWDwdrkbe1Ci6Zzb48
+SECRET_KEY2="edsk3Vjga1rxDHC9Bns7EN4CWkpaYoHwnHBPJMdERPtytxcx7A78PG"
+
 # We need the tezos-client to deploy contracts.
 # We will use the binary included in the Tezos testnet deployment
 # See https://tezos.gitlab.io/introduction/howtouse.html#client
@@ -22,12 +25,15 @@ message "Using Tezos RPC Node: $DEKU_TEZOS_RPC_NODE"
 message "Configuring Tezos client"
 tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" bootstrapped
 tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key myWallet "unencrypted:$SECRET_KEY" --force
+tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key myWallet2 "unencrypted:$SECRET_KEY2" --force
 
 # [deploy_contract name source_file initial_storage] compiles the Ligo code in [source_file],
 # the [initial_storage] expression and originates the contract as myWallet on Tezos.
 deploy_contract() {
     message "Deploying new $1 contract"
 
+
+    wallet=${4:-myWallet}
     # Compiles an initial storage for a given contract to a Michelson expression.
     # The resulting Michelson expression can be passed as an argument in a transaction which originates a contract.
     storage=$(ligo compile storage "$2" "$3")
@@ -41,7 +47,7 @@ deploy_contract() {
     tezos-client \
         --wait 1 \
         --endpoint "$DEKU_TEZOS_RPC_NODE" originate contract "$1" \
-        transferring 0 from myWallet \
+        transferring 0 from $wallet \
         running "$contract" \
         --init "$storage" \
         --burn-cap 2 \
@@ -56,7 +62,8 @@ deploy_contract "consensus" \
 
 deploy_contract "discovery" \
     "./src/tezos_interop/discovery.mligo" \
-    "$(cat "$storage_path/discovery_storage.mligo")"
+    "$(cat "$storage_path/discovery_storage.mligo")" \
+    myWallet2 # switch wallet to avoid the "Only one manager operation per block" error
 
 deploy_contract "dummy_ticket" \
     "./dummy_ticket.mligo" \
