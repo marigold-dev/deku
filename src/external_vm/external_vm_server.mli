12e7e88d4b58779172c3d1cd49fee4695554ddf4
Daniel Hines <d4hines@gmail.com>
2022-09-24 16:18:03 -0400

    feat: handle errors in the external vm

--- src/external_vm/external_vm_server.mli
+++ src/external_vm/external_vm_server.mli
@@ -5,9 +5,10 @@ type storage = { set : string -> string -> unit; get : string -> string option }
 val main :
   named_pipe_path:string ->
   External_vm_protocol.set list ->
-  (storage ->
-  Deku_crypto.Key_hash.t ->
-  (Ticket_id.t * int64) list ->
-  string ->
+  (storage:storage ->
+  source:Deku_crypto.Key_hash.t ->
+  tickets:(Ticket_id.t * int64) list ->
+  operation:string ->
+  operation_hash_raw:string ->
   (unit, string) result) ->
   unit
