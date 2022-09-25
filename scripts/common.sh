60bcfd99805d5715ee7b5c84604d1b48d1a8b3fd
aguillon <arthur@marigold.dev>
2022-09-25 15:49:50 +0200

    Testing on flextesa and kathmandunet

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,12 +1,30 @@
 #/!bin/bash
-# Various functions used by other scripts
+# bash utilities for other scripts or to be sourced for manual testing
 
 message() {
   echo -e "\e[35m\e[1m**************************    $*    ********************************\e[0m"
 }
 
+using_flextesa() {
+  if [ -z $DEKU_TEST_MODE ]
+  then
+    >&2 echo "!!! DEKU_TEST_MODE not set, using flextesa"
+    return 0
+  elif [ $DEKU_TEST_MODE = "local" ] || [ $DEKU_TEST_MODE = "flextesa" ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
 tezos_client() {
-  docker exec -t deku_flextesa tezos-client "$@"
+  if using_flextesa
+  then
+    docker exec -t deku_flextesa tezos-client "$@"
+  else
+    nix run github:marigold-dev/tezos-nix#tezos-client -- "$@"
+  fi
 }
 
 proof_id() {
