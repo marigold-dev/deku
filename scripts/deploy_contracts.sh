01b5e1fb204ac7f1d54a2f63624f30f71aa35412
aguillon <arthur@marigold.dev>
2022-09-25 15:49:50 +0200

    Testing on flextesa and kathmandunet

--- scripts/deploy_contracts.sh
+++ scripts/deploy_contracts.sh
@@ -7,15 +7,8 @@ source $dir/common.sh
 # We need this secret key for sigining Tezos operations.
 SECRET_KEY="edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq"
 
-# tz1SZtpiT7KAdwZ23PDWDwdrkbe1Ci6Zzb48
-SECRET_KEY2="edsk3Vjga1rxDHC9Bns7EN4CWkpaYoHwnHBPJMdERPtytxcx7A78PG"
-
-# We need the tezos-client to deploy contracts.
-# We will use the binary included in the Tezos testnet deployment
-# See https://tezos.gitlab.io/introduction/howtouse.html#client
-tezos-client() {
-    nix run github:marigold-dev/tezos-nix#tezos-client -- "$@"
-}
+# tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6 (or "bob" on flextesa defaults)
+SECRET_KEY2="edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt"
 
 # Using a Tezos node on localhost:20000 that is provided by the docker-compose file
 DEKU_TEZOS_RPC_NODE=${DEKU_TEZOS_RPC_NODE:-http://localhost:20000}
@@ -23,9 +16,10 @@ DEKU_TEZOS_RPC_NODE=${DEKU_TEZOS_RPC_NODE:-http://localhost:20000}
 message "Using Tezos RPC Node: $DEKU_TEZOS_RPC_NODE"
 
 message "Configuring Tezos client"
-tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" bootstrapped
-tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key myWallet "unencrypted:$SECRET_KEY" --force
-tezos-client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key myWallet2 "unencrypted:$SECRET_KEY2" --force
+tezos_client --endpoint "$DEKU_TEZOS_RPC_NODE" bootstrapped
+tezos_client --endpoint "$DEKU_TEZOS_RPC_NODE" config update
+tezos_client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key myWallet "unencrypted:$SECRET_KEY" --force
+tezos_client --endpoint "$DEKU_TEZOS_RPC_NODE" import secret key bob "unencrypted:$SECRET_KEY2" --force
 
 # [deploy_contract name source_file initial_storage] compiles the Ligo code in [source_file],
 # the [initial_storage] expression and originates the contract as myWallet on Tezos.
@@ -44,7 +38,7 @@ deploy_contract() {
 
     echo "Originating $1 contract"
     sleep 2
-    tezos-client \
+    tezos_client \
         --wait 1 \
         --endpoint "$DEKU_TEZOS_RPC_NODE" originate contract "$1" \
         transferring 0 from $wallet \
@@ -63,7 +57,7 @@ deploy_contract "consensus" \
 deploy_contract "discovery" \
     "./src/tezos_interop/discovery.mligo" \
     "$(cat "$storage_path/discovery_storage.mligo")" \
-    myWallet2 # switch wallet to avoid the "Only one manager operation per block" error
+    bob # switch wallet to avoid the "Only one manager operation per block" error
 
 deploy_contract "dummy_ticket" \
     "./dummy_ticket.mligo" \
