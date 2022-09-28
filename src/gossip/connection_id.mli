4782eabf7cbe771680983b8b3af70369df888f82
EduardoRFS <theeduardorfs@gmail.com>
2022-09-28 14:24:59 -0300

    beta grade networking

---
+++ src/gossip/connection_id.mli
@@ -0,0 +1,7 @@
+type connection_id
+type t = connection_id [@@deriving eq, ord]
+
+val initial : connection_id
+val next : connection_id -> connection_id
+
+module Map : Map.S with type key = connection_id
--- src/network/network_manager.ml
+++ src/network/network_manager.ml
@@ -1,136 +1,138 @@
 open Deku_gossip
 
 type network = {
-  mutable nodes :
-    ((raw_expected_hash:string -> raw_content:string -> unit)
-    * (raw_expected_hash:string -> raw_content:string -> unit))
-    list;
-  mutable next_request_id : Request_id.t;
-  mutable requests :
-    (raw_expected_hash:string -> raw_content:string -> unit) Request_id.Map.t;
+  mutable connection_id : Connection_id.t;
+  (* TODO: Hashtbl would do a great job here *)
+  mutable connections : (Network_message.message -> unit) Connection_id.Map.t;
 }
 
 type t = network
 
-(* TODO: move to Deku_constants *)
-let retry_timeout = 3.0
-let drop ~raw_expected_hash:_ ~raw_content:_ = ()
-let drop = (drop, drop)
-
-let rec connect_loop ~sw ~clock ~net ~host ~port ~ref ~on_request ~on_message =
-  let on_error = Deku_constants.async_on_error in
-  try
-    Network_protocol.connect ~sw ~net ~host ~port ~on_request ~on_message
-    @@ fun ~request ~send -> Atomic.set ref (request, send)
-  with exn ->
-    Atomic.set ref drop;
-    on_error exn;
-    Eio.Time.sleep clock retry_timeout;
-    connect_loop ~sw ~clock ~net ~host ~port ~ref ~on_request ~on_message
-
-let rec listen_loop ~sw ~clock ~net ~port ~on_request ~on_message =
-  let on_error = Deku_constants.async_on_error in
-  try Network_protocol.listen ~sw ~net ~port ~on_error ~on_request ~on_message
-  with exn ->
-    on_error exn;
-    Eio.Time.sleep clock retry_timeout;
-    listen_loop ~sw ~clock ~net ~port ~on_request ~on_message
-
 let make () =
   {
-    nodes = [];
-    next_request_id = Request_id.initial;
-    requests = Request_id.Map.empty;
+    connection_id = Connection_id.initial;
+    connections = Connection_id.Map.empty;
   }
 
-let make_on_request ~on_request network ~send ~raw_expected_hash ~raw_content =
-  let id = network.next_request_id in
-  network.next_request_id <- Request_id.next id;
-  network.requests <- Request_id.Map.add id send network.requests;
-  on_request ~id ~raw_expected_hash ~raw_content
+let create_connection network =
+  let connection_id = network.connection_id in
+  network.connection_id <- Connection_id.next connection_id;
+  connection_id
 
-let listen ~sw ~env ~port ~on_request ~on_message network =
-  let clock = Eio.Stdenv.clock env in
-  let net = Eio.Stdenv.net env in
-  let on_request = make_on_request ~on_request network in
-  listen_loop ~sw ~clock ~net ~port ~on_request ~on_message
+let set_connection ~connection ~write network =
+  network.connections <-
+    Connection_id.Map.add connection write network.connections
 
-let connect ~sw ~env ~nodes ~on_request ~on_message network =
-  let clock = Eio.Stdenv.clock env in
-  let net = Eio.Stdenv.net env in
-  let nodes_with_host =
-    List.map
-      (fun (host, port) ->
-        let ref = Atomic.make drop in
-        (host, port, ref))
-      nodes
+let close_connection ~connection network =
+  network.connections <- Connection_id.Map.remove connection network.connections
+
+let with_connection ~on_request ~on_message network k =
+  let connection = create_connection network in
+  let handler ~sw ~read ~write =
+    let write message = Eio.Fiber.fork ~sw @@ fun () -> write message in
+    set_connection ~connection ~write network;
+
+    let on_message message =
+      Eio.Fiber.fork ~sw @@ fun () ->
+      match message with
+      | Network_message.Message { raw_expected_hash; raw_content } ->
+          on_message ~raw_expected_hash ~raw_content
+      | Network_message.Request { raw_expected_hash; raw_content } ->
+          on_request ~connection ~raw_expected_hash ~raw_content
+    in
+    let rec loop () =
+      let message = read () in
+      on_message message;
+      loop ()
+    in
+    loop ()
   in
-  let nodes =
-    List.map
-      (fun (_host, _port, ref) ->
-        let request ~raw_expected_hash ~raw_content =
-          let request, _send = Atomic.get ref in
-          request ~raw_expected_hash ~raw_content
-        in
-        let send ~raw_expected_hash ~raw_content =
-          let _request, send = Atomic.get ref in
-          send ~raw_expected_hash ~raw_content
-        in
-        (request, send))
-      nodes_with_host
+  let handler ~read ~write =
+    try Eio.Switch.run @@ fun sw -> handler ~sw ~read ~write
+    with exn ->
+      close_connection ~connection network;
+      (* TODO: reraise *)
+      raise exn
   in
-  network.nodes <- nodes;
-  let on_request = make_on_request ~on_request network in
-  List.iter
-    (fun (host, port, ref) ->
-      Eio.Fiber.fork_sub ~sw ~on_error:Deku_constants.async_on_error
-      @@ fun sw ->
-      connect_loop ~sw ~clock ~net ~host ~port ~ref ~on_request ~on_message)
-    nodes_with_host
+  k ~handler
 
-let broadcast ~sw ~raw_expected_hash ~raw_content network =
-  List.iter
-    (fun (_request, send) ->
-      Eio.Fiber.fork ~sw (fun () -> send ~raw_expected_hash ~raw_content))
-    network.nodes
+let connect ~clock ~net ~host ~port ~on_request ~on_message network =
+  let rec reconnect_loop ~clock ~net ~host ~port ~handler =
+    try
+      Eio.Switch.run @@ fun sw ->
+      Network_protocol.connect ~sw ~net ~host ~port handler
+    with exn ->
+      Format.eprintf "reconnect(%s:%d): %s\n%!" host port
+        (Printexc.to_string exn);
+      Eio.Time.sleep clock Deku_constants.reconnect_timeout;
+      reconnect_loop ~clock ~net ~host ~port ~handler
+  in
+  with_connection ~on_request ~on_message network @@ fun ~handler ->
+  reconnect_loop ~clock ~net ~host ~port ~handler
 
-let request ~sw ~raw_expected_hash ~raw_content network =
-  List.iter
-    (fun (request, _send) ->
-      Eio.Fiber.fork ~sw (fun () -> request ~raw_expected_hash ~raw_content))
-    network.nodes
+let rec listen ~sw ~net ~clock ~port ~on_request ~on_message network =
+  let handler ~read ~write =
+    with_connection ~on_request ~on_message network @@ fun ~handler ->
+    handler ~read ~write
+  in
+  try
+    let on_error exn =
+      Format.eprintf "listen.connection: %s\n%!" (Printexc.to_string exn)
+    in
+    Network_protocol.listen ~sw ~net ~port ~on_error handler
+  with exn ->
+    Format.eprintf "listen: %s\n%!" (Printexc.to_string exn);
+    Eio.Time.sleep clock Deku_constants.listen_timeout;
+    listen ~sw ~clock ~net ~port ~on_request ~on_message network
 
-let respond ~id ~raw_expected_hash ~raw_content network =
-  match Request_id.Map.find_opt id network.requests with
-  | Some send ->
-      network.requests <- Request_id.Map.remove id network.requests;
-      Format.eprintf "response: %s\n%!" raw_expected_hash;
-      (* TODO: Eio.Fiber.fork*)
-      send ~raw_expected_hash ~raw_content
+let connect ~net ~clock ~nodes ~on_request ~on_message network =
+  Eio.Fiber.List.iter
+    (fun (host, port) ->
+      try connect ~clock ~net ~host ~port ~on_request ~on_message network
+      with exn ->
+        Format.eprintf "connect(%s:%d): %s\n%!" host port
+          (Printexc.to_string exn))
+    nodes
+
+let send ~message ~write =
+  (* write always includes a fork *)
+  try write message
+  with exn -> Format.eprintf "write.error: %s\n%!" (Printexc.to_string exn)
+
+let broadcast message network =
+  Connection_id.Map.iter
+    (fun _connection write -> send ~message ~write)
+    network.connections
+
+let request ~raw_expected_hash ~raw_content network =
+  let request = Network_message.request ~raw_expected_hash ~raw_content in
+  broadcast request network
+
+let broadcast ~raw_expected_hash ~raw_content network =
+  let message = Network_message.message ~raw_expected_hash ~raw_content in
+  broadcast message network
+
+let send ~connection ~raw_expected_hash ~raw_content network =
+  match Connection_id.Map.find_opt connection network.connections with
+  | Some write ->
+      let message = Network_message.message ~raw_expected_hash ~raw_content in
+      send ~message ~write
   | None ->
-      (* TODO: what do I do here? *)
-      Format.eprintf "duplicated respond\n%!";
+      (* dead connection *)
       ()
-
-let not_found ~id network =
-  network.requests <- Request_id.Map.remove id network.requests
 
 let test () =
   Eio_main.run @@ fun env ->
-  let nodes =
-    [
-      ("localhost", 1234);
-      ("localhost", 1235);
-      ("localhost", 1236);
-      ("localhost", 1236);
-    ]
-  in
+  let nodes = [ ("localhost", 1234) ] in
+
+  let net = Eio.Stdenv.net env in
+  let clock = Eio.Stdenv.clock env in
 
   let start ~sw ~port : unit =
     let network = make () in
-    let on_request ~id ~raw_expected_hash ~raw_content =
+    let on_request ~connection ~raw_expected_hash ~raw_content =
       Format.eprintf "request(%s): %s\n%!" raw_expected_hash raw_content;
-      respond ~id ~raw_expected_hash ~raw_content network
+      send ~connection ~raw_expected_hash ~raw_content network
     in
     let on_message ~raw_expected_hash ~raw_content =
       Format.eprintf "message(%s): %s\n%!" raw_expected_hash raw_content
@@ -138,42 +140,27 @@ let test () =
 
     let () =
       Eio.Fiber.fork ~sw (fun () ->
-          listen ~sw ~env ~port ~on_request ~on_message network)
+          listen ~sw ~net ~clock ~port ~on_request ~on_message network)
     in
     let () =
       Eio.Fiber.fork ~sw (fun () ->
-          connect ~sw ~env ~nodes ~on_request ~on_message network)
+          connect ~net ~clock ~nodes ~on_request ~on_message network)
     in
 
-    let rec throughput_loop counter =
+    let rec loop counter =
       Eio.Fiber.yield ();
       Eio.Fiber.both
         (fun () ->
           let raw_expected_hash = Format.sprintf "rh%d" counter in
           let raw_content = Format.sprintf "rc%d" counter in
-          request ~sw ~raw_expected_hash ~raw_content network)
+          request ~raw_expected_hash ~raw_content network)
         (fun () ->
           let raw_expected_hash = Format.sprintf "sh%d" counter in
           let raw_content = Format.sprintf "sc%d" counter in
-          broadcast ~sw ~raw_expected_hash ~raw_content network);
-      throughput_loop (counter + 1)
+          broadcast ~raw_expected_hash ~raw_content network);
+      loop (counter + 1)
     in
-    let _ = throughput_loop in
-    let clock = Eio.Stdenv.clock env in
-    let rec latency_loop counter =
-      Eio.Fiber.yield ();
-      Eio.Time.sleep clock 0.01;
-      let _request, send = List.nth network.nodes 0 in
-      let raw_expected_hash = Format.sprintf "sh%d" counter in
-      let raw_content =
-        Format.sprintf "sc%d:%.3f" counter (Unix.gettimeofday ())
-      in
-      Format.printf "sending(%s): %s\n%!" raw_expected_hash raw_content;
-      send ~raw_expected_hash ~raw_content;
-      latency_loop (counter + 1)
-    in
-
-    latency_loop 0
+    loop 0
   in
 
   let domains = Eio.Stdenv.domain_mgr env in
@@ -181,5 +168,4 @@ let test () =
     Eio.Domain_manager.run domains (fun () ->
         Eio.Switch.run @@ fun sw -> start ~sw ~port)
   in
-  Eio.Fiber.all (List.init 1 (fun n () -> start ~port:(1234 + n)))
-(* let clock = Eio.Stdenv.clock env in *)
+  Eio.Fiber.List.iter (fun (_host, port) -> start ~port) nodes
