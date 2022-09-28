7db66ccc4f01ac528fd46ce73765aa1ea01e89b5
EduardoRFS <theeduardorfs@gmail.com>
2022-09-28 19:34:25 -0300

    tezos interop stuff

--- src/tezos_interop/long_lived_process.ml
+++ src/tezos_interop/long_lived_process.ml
@@ -1,45 +1,52 @@
 open Deku_stdlib
 
-let spawn ~file ~on_error ~on_close input_stream =
-  let node = "node" in
-  let command = (node, [| node; file |]) in
+(* TODO: magic number: 256mb *)
+let max_size = 256 * 1024 * 1024
 
-  let process = Lwt_process.open_process command in
+let spawn ~file k =
+  let prog = "node" in
+  let args = [| prog; file |] in
+  IO.spawn ~prog ~args @@ fun ~stdin ~stdout ->
+  Eio.Buf_write.with_flow stdin @@ fun write_buf ->
+  let read_buf = Eio.Buf_read.of_flow ~max_size stdout in
+  let read () =
+    let line = Eio.Buf_read.line read_buf in
+    Yojson.Safe.from_string line
+  in
+  let write json =
+    let json = Yojson.Safe.to_string json in
+    Eio.Buf_write.string write_buf json;
+    Eio.Buf_write.char write_buf '\n'
+  in
+  k ~read ~write
 
-  let on_error exn =
-    on_error exn;
-    raise exn
+let spawn ~file ~on_error ~on_json k =
+  let dummy_write _json = () in
+  let write_ref = ref dummy_write in
+  let write json =
+    try !write_ref json
+    with exn -> Format.eprintf "spawn.write: %s\n%!" (Printexc.to_string exn)
+  in
+  Eio.Switch.run @@ fun sw ->
+  let handler ~read ~write =
+    let write json = Eio.Fiber.fork ~sw @@ fun () -> write json in
+    let on_json json = Eio.Fiber.fork ~sw @@ fun () -> on_json json in
+
+    write_ref := write;
+    let rec loop () =
+      let json = read () in
+      on_json json;
+      loop ()
+    in
+    loop ()
+  in
+  let rec loop () =
+    try spawn ~file handler
+    with exn ->
+      write_ref := dummy_write;
+      on_error exn;
+      loop ()
   in
 
-  (* handle process failure *)
-  Lwt.async (fun () ->
-      Lwt.catch
-        (fun () ->
-          (* this should never return *)
-          let%await status = process#status in
-          Lwt.return (on_close status))
-        on_error);
-
-  (* TODO: test that exception on input and output kill the node *)
-  let input_stream = Lwt_stream.map Yojson.Safe.to_string input_stream in
-  (* handle input exceptions *)
-  Lwt.async (fun () ->
-      Lwt.catch
-        (fun () -> Lwt_io.write_lines process#stdin input_stream)
-        on_error);
-
-  let output_stream = Lwt_io.read_lines process#stdout in
-  let output_stream =
-    Lwt_stream.map
-      (fun line ->
-        (* TODO: are exceptions on Lwt_stream.map safe? *)
-        Yojson.Safe.from_string line)
-      output_stream
-  in
-  (* handle output exceptions *)
-  let output_stream =
-    Lwt_stream.map
-      (function Ok value -> value | Error exn -> on_error exn)
-      (Lwt_stream.wrap_exn output_stream)
-  in
-  output_stream
+  let () = Eio.Fiber.fork ~sw @@ fun () -> k ~write in
+  loop ()
--- src/tezos_interop/long_lived_process.mli
+++ src/tezos_interop/long_lived_process.mli
@@ -1,6 +1,7 @@
+(* TODO: better API *)
 val spawn :
   file:string ->
   on_error:(exn -> unit) ->
-  on_close:(Unix.process_status -> unit) ->
-  Yojson.Safe.t Lwt_stream.t ->
-  Yojson.Safe.t Lwt_stream.t
+  on_json:(Yojson.Safe.t -> unit) ->
+  (write:(Yojson.Safe.t -> unit) -> unit) ->
+  'a
