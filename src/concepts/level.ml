e316f8e8af3ed538586f6253cf61e2ce008852ac
EduardoRFS <theeduardorfs@gmail.com>
2022-09-27 22:41:02 -0300

    peripheral stuff, not hard

--- src/concepts/level.ml
+++ src/concepts/level.ml
@@ -3,12 +3,12 @@ open N
 
 (* TODO: should we prefix level in b58? *)
 type level = N.t
-and t = level [@@deriving eq, ord, yojson]
+and t = level [@@deriving show, eq, ord, yojson]
 
 let zero = zero
 let next x = x + one
 let of_n n = n
 let to_n n = n
-let ( < ) = ( < )
+let ( > ) = ( > )
 
 module Map = Map
--- src/constants/deku_constants.mli
+++ src/constants/deku_constants.mli
@@ -4,3 +4,5 @@ val includable_operation_window : N.t
 val block_timeout : float
 val clean_block_pool_time : float
 val clean_gossip_time : float
+val async_on_error : exn -> unit
+val genesis_time : float
