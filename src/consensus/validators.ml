fa182f5d92dc448b40a8af141e82f2bf51375593
EduardoRFS <theeduardorfs@gmail.com>
2022-09-29 18:23:44 -0300

    keep validators order

--- src/consensus/validators.ml
+++ src/consensus/validators.ml
@@ -1,20 +1,23 @@
 open Deku_crypto
 
-type validators = Key_hash.Set.t
+type validators = { set : Key_hash.Set.t; list : Key_hash.t list }
 and t = validators [@@deriving yojson]
 
 let of_key_hash_list validators =
   match validators with
   | [] -> raise (Invalid_argument "validators cannot be empty")
-  | validators -> Key_hash.Set.of_list validators
+  | validators ->
+      let set = Key_hash.Set.of_list validators in
+      assert (Key_hash.Set.cardinal set = List.length validators);
+      { set; list = validators }
 
-let to_key_hash_list validators = Key_hash.Set.elements validators
+let to_key_hash_list validators = validators.list
 
 let cardinal validators =
   (* TODO: O(1) cardinality *)
-  Key_hash.Set.cardinal validators
+  List.length validators.list
 
-let mem key_hash validators = Key_hash.Set.mem key_hash validators
+let mem key_hash validators = Key_hash.Set.mem key_hash validators.set
 
 let rec findi_opt n f l =
   match l with
@@ -27,7 +30,7 @@ let find_after_index ~after validators =
   findi_opt (fun validator -> Key_hash.equal validator after) validators
 
 let skip ~after ~skip validators =
-  let validators = to_key_hash_list validators in
+  let validators = validators.list in
   let length = List.length validators in
   let index =
     match find_after_index ~after validators with
