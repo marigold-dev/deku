84a08814bea03bc7eb58384d1e381ea8223dd1fc
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-29 14:03:45 +0200

    fix/indexer: migrate to eio

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,19 @@
 { pkgs, doCheck ? true, nodejs, npmPackages }:
 
 let inherit (pkgs) lib stdenv ocamlPackages; in
-
 with ocamlPackages; buildDunePackage rec {
   pname = "deku";
+
+  caqti-eio = buildDunePackage {
+    pname = "caqti-eio";
+    version = "n/a";
+    src = builtins.fetchurl {
+      url = https://github.com/anmonteiro/caqti-eio/archive/c709dad.tar.gz;
+      sha256 = "0mmjms378akcs7lifpz3s82hw7g6sdxbsyqlb0yrry7as29rccsz";
+    };
+    propagatedBuildInputs = [ eio eio_main caqti ];
+  };
+
   version = "0.0.0-dev";
 
   src = lib.filterSource {
@@ -24,18 +34,16 @@ with ocamlPackages; buildDunePackage rec {
     mirage-crypto-ec
     mirage-crypto-rng
     secp256k1-internal
-    piaf-lwt
-    cohttp
-    cohttp-lwt-unix
+    piaf
     domainslib
     cmdliner
     ppx_blob
     data-encoding
     caqti
-    caqti-lwt
     caqti-driver-sqlite3
     ppx_deriving_cmdliner
     dream
+    caqti-eio
   ]
   # checkInputs are here because when cross compiling dune needs test dependencies
   # but they are not available for the build phase. The issue can be seen by adding strictDeps = true;.
