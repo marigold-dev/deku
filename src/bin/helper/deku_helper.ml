00fc9ce776511c7094e1a5878c254bebb9ee9526
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-09-29 14:47:42 +0200

    fix/deku-helper: migrate to EIO

--- src/bin/helper/deku_helper.ml
+++ src/bin/helper/deku_helper.ml
@@ -2,49 +2,46 @@ open Deku_protocol
 open Deku_stdlib
 open Deku_concepts
 open Deku_gossip
-open Piaf_lwt
 open Deku_crypto
 
-let post_directly_to_node ~operation =
+let post_directly_to_node ~sw ~env ~operation =
   let node = "http://localhost:4440/messages" in
   let content = Message.Content.operation operation in
   let _message, raw_message = Message.encode ~content in
   let (Message.Raw_message { hash; raw_content }) = raw_message in
   let hash, raw_content = (hash |> Message_hash.to_b58, raw_content) in
   let headers =
-    let open Headers in
+    let open Piaf.Headers in
     (* TODO: maybe add json to Well_known in Piaf*)
     let json = Mime_types.map_extension "json" in
     [ ("X-Raw-Expected-Hash", hash); (Well_known.content_type, json) ]
   in
   let target = Uri.of_string node in
   (* Format.eprintf "%a <- %s\n%!" Uri.pp_hum _uri target; *)
-  let body = Body.of_string raw_content in
+  let body = Piaf.Body.of_string raw_content in
 
-  let%await post_result = Piaf_lwt.Client.Oneshot.post ~headers ~body target in
-  (match post_result with
+  let post_result = Piaf.Client.Oneshot.post ~headers ~body ~sw env target in
+  match post_result with
   | Error _error -> print_endline "error"
-  | Ok _ -> print_endline "submitted");
-  Lwt.return_unit
+  | Ok _ -> print_endline "submitted"
 
-let post_to_api ~operation =
+let post_to_api ~sw ~env ~operation =
   let node = "http://localhost:8080/api/v1/operations" |> Uri.of_string in
   let json = Operation.yojson_of_t operation |> Yojson.Safe.to_string in
-  let body = Body.of_string json in
-  let%await post_result = Piaf_lwt.Client.Oneshot.post ~body node in
-  (match post_result with
+  let body = Piaf.Body.of_string json in
+  let post_result = Piaf.Client.Oneshot.post ~body ~sw env node in
+  match post_result with
   | Ok _ -> print_endline "operation submitted"
-  | Error _ -> print_endline "FAIL to submit operation");
-  Lwt.return_unit
+  | Error _ -> print_endline "FAIL to submit operation"
 
 let make_identity secret =
   secret |> Secret.of_b58 |> Option.get |> Identity.make
 
 type level_response = { level : Level.t } [@@deriving of_yojson]
 
-let make_level () =
-  let%await response =
-    Piaf_lwt.Client.Oneshot.get
+let make_level ~sw ~env () =
+  let response =
+    Piaf.Client.Oneshot.get ~sw env
       (Uri.of_string "http://localhost:8080/api/v1/chain/level")
   in
   let body =
@@ -52,7 +49,7 @@ let make_level () =
     | Error _ -> failwith "cannot connect to the API"
     | Ok res -> res.body
   in
-  let%await string = Body.to_string body in
+  let string = Piaf.Body.to_string body in
   let body =
     match string with
     | Error _ -> failwith "cannot parse body"
@@ -60,7 +57,7 @@ let make_level () =
   in
   let yojson = Yojson.Safe.from_string body in
   let { level } = level_response_of_yojson yojson in
-  Lwt.return level
+  level
 
 let make_nonce () =
   let rng = Stdlib.Random.State.make_self_init () in
@@ -70,11 +67,11 @@ let make_nonce () =
 let print_op operation =
   operation |> Operation.yojson_of_t |> Yojson.Safe.to_string |> print_endline
 
-let main () =
+let main ~env ~sw =
   let identity =
     make_identity "edsk4UWkJqpZrAm26qvJE8uY9ZFGFqQiFuBcDyEPASXeHxuD68WvvF"
   in
-  let%await level = make_level () in
+  let level = make_level ~sw ~env () in
   let nonce = make_nonce () in
 
   (* Change this string with your appropriate needs*)
@@ -94,8 +91,9 @@ let main () =
   let transaction = Operation.vm_transaction ~level ~nonce ~content ~identity in
   print_op transaction;
   print_newline ();
+  let _ = post_directly_to_node ~sw ~env ~operation:transaction in
+  ()
 
-  let%await _ = post_directly_to_node ~operation:transaction in
-  Lwt.return_unit
-
-let () = Lwt_main.run (main ())
+let () =
+  Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw -> main ~env ~sw
