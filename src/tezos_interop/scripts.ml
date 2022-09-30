51db777fecf878e241145faa9eaa6d7c59d63b2f
Daniel Hines <d4hines@gmail.com>
2022-09-30 11:03:18 -0400

    fix: add tezos_js_bridge.bundle.js to gitignore

--- src/tezos_interop/scripts.ml
+++ src/tezos_interop/scripts.ml
@@ -1,11 +1,11 @@
 let tezos_js_bridge = [%blob "tezos_js_bridge.bundle.js"]
 
 let file_tezos_js_bridge =
-  let file = "tezos_js_bridge.js" in
+  let file = "tezos_js_bridge.bundle.js" in
   let () =
     Eio_main.run @@ fun env ->
     let cwd = Eio.Stdenv.cwd env in
-    let file = Eio.Path.(cwd / "tezos_js_bridge.js") in
+    let file = Eio.Path.(cwd / file) in
     Eio.Path.save ~create:(`Or_truncate 0o644) file tezos_js_bridge
   in
   let cwd = Unix.getcwd () in
