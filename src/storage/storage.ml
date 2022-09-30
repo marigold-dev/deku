cd31650b443b90e50fa5a47ce15e948a91bad10b
EduardoRFS <theeduardorfs@gmail.com>
2022-09-30 12:34:28 -0300

    allow absolute paths for storage

--- src/storage/storage.ml
+++ src/storage/storage.ml
@@ -6,28 +6,26 @@ module Chain = struct
   let file = "chain.json"
 
   let read ~env ~folder =
-    let cwd = Eio.Stdenv.cwd env in
+    let fs = Eio.Stdenv.fs env in
     match
-      let file =
-        Filename.concat (Filename.concat (Unix.getcwd ()) folder) file
-      in
+      let file = Filename.concat folder file in
       IO.file_exists file
     with
     | true ->
-        let file = Eio.Path.(cwd / folder / file) in
+        let file = Eio.Path.(fs / folder / file) in
         let json = Eio.Path.load file in
         let json = Yojson.Safe.from_string json in
         Some (Chain.t_of_yojson json)
     | false -> None
 
   let write ~env ~folder (chain : Chain.t) =
-    let cwd = Eio.Stdenv.cwd env in
+    let fs = Eio.Stdenv.fs env in
     let bin =
       let json = Chain.yojson_of_t chain in
       Yojson.Safe.to_string json
     in
-    let temp = Eio.Path.(cwd / folder / temp) in
-    let file = Eio.Path.(cwd / folder / file) in
+    let temp = Eio.Path.(fs / folder / temp) in
+    let file = Eio.Path.(fs / folder / file) in
     Eio.Path.save ~create:(`If_missing 0o644) temp bin;
     Eio.Path.rename temp file
 end
