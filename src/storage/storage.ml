fc430b5015e58a00311ce1f3c9470edd8c77f6f8
EduardoRFS <theeduardorfs@gmail.com>
2022-10-01 15:05:49 -0300

    stream chain to and from disk

--- src/storage/storage.ml
+++ src/storage/storage.ml
@@ -5,27 +5,25 @@ module Chain = struct
   let temp = "chain.tmp.json"
   let file = "chain.json"
 
-  let read ~env ~folder =
-    let fs = Eio.Stdenv.fs env in
-    match
-      let file = Filename.concat folder file in
-      IO.file_exists file
-    with
+  let read ~env:_ ~folder =
+    let file = Filename.concat folder file in
+    match IO.file_exists file with
     | true ->
-        let file = Eio.Path.(fs / folder / file) in
-        let json = Eio.Path.load file in
-        let json = Yojson.Safe.from_string json in
-        Some (Chain.t_of_yojson json)
+        let chain =
+          Eio_unix.run_in_systhread (fun () ->
+              let json = Yojson.Safe.from_file file in
+              Chain.t_of_yojson json)
+        in
+        Some chain
     | false -> None
 
-  let write ~env ~folder (chain : Chain.t) =
+  let write ~env ~folder chain =
+    Eio_unix.run_in_systhread (fun () ->
+        let temp = Filename.concat folder temp in
+        let json = Chain.yojson_of_t chain in
+        Yojson.Safe.to_file temp json);
     let fs = Eio.Stdenv.fs env in
-    let bin =
-      let json = Chain.yojson_of_t chain in
-      Yojson.Safe.to_string json
-    in
     let temp = Eio.Path.(fs / folder / temp) in
     let file = Eio.Path.(fs / folder / file) in
-    Eio.Path.save ~create:(`If_missing 0o644) temp bin;
     Eio.Path.rename temp file
 end
