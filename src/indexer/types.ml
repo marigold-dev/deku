2e392afd3c9839a710f2c81f79df373aebecc4ee
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-04 11:22:52 +0200

    refacto/indexer: add a block hash type for sql

--- src/indexer/types.ml
+++ src/indexer/types.ml
@@ -1,5 +1,6 @@
 open Deku_stdlib
 open Deku_concepts
+open Deku_consensus
 
 module Level : Rapper.CUSTOM with type t = Level.t = struct
   type t = Level.t
@@ -15,4 +16,17 @@ module Level : Rapper.CUSTOM with type t = Level.t = struct
       |> Result.map Level.of_n
     in
     Caqti_type.(custom ~encode ~decode int64)
+end
+
+module Block_hash : Rapper.CUSTOM with type t = Block_hash.t = struct
+  type t = Block_hash.t
+
+  let t =
+    let encode block_hash = block_hash |> Block_hash.to_b58 |> Result.ok in
+    let decode b58 =
+      b58 |> Block_hash.of_b58
+      |> Option.to_result
+           ~none:(Format.sprintf "Cannot parse the block hash %s" b58)
+    in
+    Caqti_type.(custom ~encode ~decode string)
 end
