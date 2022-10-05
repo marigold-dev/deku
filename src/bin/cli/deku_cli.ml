1a87a9339ec811a0fa66133ec7057c2c7ea03723
aguillon <arthur@marigold.dev>
2022-10-05 09:17:26 +0200

    generate-identity in deku-cli

--- src/bin/cli/deku_cli.ml
+++ src/bin/cli/deku_cli.ml
@@ -5,6 +5,6 @@ let create_vm_mock_transaction () = print_endline "create a vm operation"
 let () =
   let cmd =
     Cmd.group (Cmd.info "deku-cli")
-      [ Create_custom_transaction.cmd; Create_mock_transaction.cmd ]
+    [ Create_custom_transaction.cmd; Create_mock_transaction.cmd; Generate_identity.cmd ]
   in
   exit (Cmdliner.Cmd.eval ~catch:true cmd)
