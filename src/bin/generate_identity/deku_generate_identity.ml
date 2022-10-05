8762d1f012b76bc510cb679452b80a1d4a2ca30a
Daniel Hines <d4hines@gmail.com>
2022-10-05 19:17:31 -0400

    feat: print key hash in deku-generate-identity

--- src/bin/generate_identity/deku_generate_identity.ml
+++ src/bin/generate_identity/deku_generate_identity.ml
@@ -4,8 +4,10 @@ let main () =
   Mirage_crypto_rng_unix.initialize ();
   let secret = Ed25519.Secret.generate () in
   let key = Ed25519.Key.of_secret secret in
+  let key_hash = Ed25519.Key_hash.of_key key in
   Format.printf "Secret: %s\n%!" (Ed25519.Secret.to_b58 secret);
-  Format.printf "Key: %s\n%!" (Ed25519.Key.to_b58 key)
+  Format.printf "Key: %s\n%!" (Ed25519.Key.to_b58 key);
+  Format.printf "Key Hash: %s\n%!" (Ed25519.Key_hash.to_b58 key_hash)
 
 open Cmdliner
 
