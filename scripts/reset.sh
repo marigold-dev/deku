f7adb63f213994f82ca54fe27fd79abcb4ec51aa
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-10 11:45:08 +0200

    fix/scripts: update start.sh and reset.sh

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -5,4 +5,10 @@ for N in 0 1 2 3; do
 
     rm -rf "./flextesa_chain/data/$N/pipe_read"
     rm -rf "./flextesa_chain/data/$N/pipe_write"
+
+    rm -rf "./chain/data/$N/pipe_read"
+    rm -rf "./chain/data/$N/pipe_write"
+    rm -rf "./chain/data/$N/api_vm_pipe_read"
+    rm -rf "./chain/data/$N/api_vm_pipe_write"
+    rm -rf "./chain/data/$N/deku_api.json"
 done
--- start.sh
+++ start.sh
@@ -28,7 +28,7 @@ export DEKU_API_PORT=8080
 export DEKU_API_DATABASE_URI="sqlite3:/tmp/api_database.db"
 export DEKU_API_DOMAINS=8
 export DEKU_API_VM="./flextesa_chain/data/0/api_vm_pipe"
-export DEKU_API_DATA_FOLDER="./chain/data/0/"
+export DEKU_API_DATA_FOLDER="./flextesa_chain/data/0/"
 
 ## The api needs its own vm
 nix run ".#$vm" -- "$DEKU_API_VM" &
