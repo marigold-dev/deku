085584af9946f7724806b0079adcf88b52f0bb2e
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-17 11:22:22 +0200

    refacto/load-tester: api url

--- deku-p/src/load-tester/deku_load_tester.ml
+++ deku-p/src/load-tester/deku_load_tester.ml
@@ -24,18 +24,18 @@ module Signed_operation = struct
 end
 
 let post_to_api ~sw ~env ~api_url operation =
+  let submit_op_uri = Uri.with_path api_url "/api/v1/operations" in
   let repr = Signed_operation.of_signed operation in
   let json = Signed_operation.yojson_of_t repr |> Yojson.Safe.to_string in
   let body = Piaf.Body.of_string json in
-  let post_result = Piaf.Client.Oneshot.post ~body ~sw env api_url in
+  let post_result = Piaf.Client.Oneshot.post ~body ~sw env submit_op_uri in
   match post_result with
   | Ok _ -> Ok ()
   | Error err -> Error (Piaf.Error.to_string err)
 
 type params = {
   domains : int; [@default 8]
-  api_url : Uri.t;
-      [@default Uri.of_string "http://localhost:8080/api/v1/operations"]
+  api_url : Uri.t; [@default Uri.of_string "http://localhost:8080"]
   secret : Ed25519.Secret.t; [@default Ed25519.Secret.generate ()]
 }
 [@@deriving cmdliner]
