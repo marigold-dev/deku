b9fd44b223ce00d1f54a4fac509699fe6dd203a7
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-17 10:55:00 +0200

    refacto/load-tester: add cmdliner

--- deku-p/src/load-tester/deku_load_tester.ml
+++ deku-p/src/load-tester/deku_load_tester.ml
@@ -32,17 +32,21 @@ let post_to_api ~sw ~env ~api_url operation =
   | Ok _ -> Ok ()
   | Error err -> Error (Piaf.Error.to_string err)
 
-let () =
+type params = {
+  domains : int; [@default 8]
+  api_url : Uri.t;
+      [@default Uri.of_string "http://localhost:8080/api/v1/operations"]
+  secret : Ed25519.Secret.t; [@default Ed25519.Secret.generate ()]
+}
+[@@deriving cmdliner]
+
+let main params =
+  let { domains; api_url; secret } = params in
   Eio_main.run @@ fun env ->
   Eio.Switch.run @@ fun sw ->
   let open Operation in
-  Parallel.Pool.run ~env ~domains:8 @@ fun () ->
-  let api_url = "http://localhost:8080/api/v1/operations" |> Uri.of_string in
-  let identity =
-    let secret = Ed25519.Secret.generate () in
-    let secret = Secret.Ed25519 secret in
-    Identity.make secret
-  in
+  Parallel.Pool.run ~env ~domains @@ fun () ->
+  let identity = Identity.make (Secret.Ed25519 secret) in
   let nonce =
     let rng = Stdlib.Random.State.make_self_init () in
     Stdlib.Random.State.bits64 rng
@@ -58,3 +62,9 @@ let () =
   | Ok _ ->
       print_endline "success";
       exit 0
+
+let () =
+  let info = Cmdliner.Cmd.info "deku-load-tester" in
+  let term = Cmdliner.Term.(const main $ params_cmdliner_term ()) in
+  let cmd = Cmdliner.Cmd.v info term in
+  exit (Cmdliner.Cmd.eval ~catch:true cmd)
