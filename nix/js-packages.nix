0375cb2e39fc7ca741a44d721fb9454000b95c63
Daniel Hines <d4hines@gmail.com>
2022-10-21 08:24:04 -0400

    refactor: s/decookie-vm/decookies-vm

--- nix/js-packages.nix
+++ nix/js-packages.nix
@@ -23,16 +23,16 @@
           [{ subsystemInfo.nodejs = (builtins.substring 0 2 nodejs.version); }];
       }).packages [ "default" ""];
       apps = {
-        decookie-vm =
-          let script = pkgs.writeScriptBin "decookie-vm" ''
-            export NODE_PATH="${self'.packages.decookie-vm}/lib/node_modules/decookie-vm/node_modules"
-            ${nodejs}/bin/node ${self'.packages.decookie-vm}/lib/node_modules/decookie-vm/lib/src/index.js "$@"
+        decookies-vm =
+          let script = pkgs.writeScriptBin "decookies-vm" ''
+            export NODE_PATH="${self'.packages.decookies-vm}/lib/node_modules/decookies-vm/node_modules"
+            ${nodejs}/bin/node ${self'.packages.decookies-vm}/lib/node_modules/decookies-vm/lib/src/index.js "$@"
           '';
           in
           {
             type = "app";
             # FIXME: should we standardize the version of node used with an overlay?
-            program = "${script}/bin/decookie-vm";
+            program = "${script}/bin/decookies-vm";
           };
       };
     };
