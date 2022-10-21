d05c908c455cd29ded8392d3435a5a559b7a2d46
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-21 17:15:30 +0200

    feat/deku-c-toolkit: update the examplle

--- deku-c/deku-c-toolkit/examples/index.ts
+++ deku-c/deku-c-toolkit/examples/index.ts
@@ -2,6 +2,10 @@ import { fromMemorySigner } from "@marigold-dev/deku-toolkit";
 import { DekuCClient } from "../src";
 import { InMemorySigner } from "@taquito/signer"
 
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const test = async () => {
 
     const signer = new InMemorySigner(
@@ -34,9 +38,21 @@ const test = async () => {
             return [list([]), storage]};
     `;
 
-    const contractAddr = await dekuC.originateContract({ kind: "jsligo", storage: 1, code });
+    const contractAddr = await dekuC.originateContract({ kind: "jsligo", initialStorage: 1, code });
+    console.log("contract address:");
     console.log(contractAddr);
     const contract = dekuC.contract(contractAddr)
+
+    // Let's wait 10 seconds
+    const _ = await sleep(10000);
+
+    const state = await contract.getState();
+    console.log("state:");
+    console.log(state);
+
+    const rawState = await contract.getRawState();
+    console.log("raw state:");
+    console.log(rawState);
 }
 
 test().catch(console.error);
