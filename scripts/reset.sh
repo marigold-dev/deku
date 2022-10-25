b08ec2803f9d0591baba76522a947b6db31ac2b0
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-25 13:09:07 +0200

    fix/scripts: reset.sh

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -6,11 +6,11 @@ for N in 0 1 2 3; do
     rm -rf "./flextesa_chain/data/$N/pipe_read"
     rm -rf "./flextesa_chain/data/$N/pipe_write"
 
-    rm -rf "./chain/data/$N/pipe_read"
-    rm -rf "./chain/data/$N/pipe_write"
-    rm -rf "./chain/data/$N/api_vm_pipe_read"
-    rm -rf "./chain/data/$N/api_vm_pipe_write"
-    rm -rf "./chain/data/$N/deku_api.json"
+    rm -rf "./flextesa_chain/data/$N/pipe_read"
+    rm -rf "./flextesa_chain/data/$N/pipe_write"
+    rm -rf "./flextesa_chain/data/$N/api_vm_pipe_read"
+    rm -rf "./flextesa_chain/data/$N/api_vm_pipe_write"
+    rm -rf "./flextesa_chain/data/$N/deku_api.json"
 done
 
 rm -rf /tmp/api_database.db
