b5d1b6ded8696c0b95a37524b24a42867a6d4220
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-10-25 13:15:18 +0200

    doc/api: add a todo for nth block

--- deku-p/src/core/bin/api/handlers.ml
+++ deku-p/src/core/bin/api/handlers.ml
@@ -61,6 +61,7 @@ module Get_block_by_level_or_hash : NO_BODY_HANDLERS = struct
 
   type path = Level_or_hash.t
   type response = Yojson.Safe.t [@@deriving yojson_of]
+  (*TODO: should returns a Repr.Block.t *)
 
   let meth = `GET
 
