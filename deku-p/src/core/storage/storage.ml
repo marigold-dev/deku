9e4400af861816c9173cb32ea75915d600f9ab24
EduardoRFS <theeduardorfs@gmail.com>
2022-10-27 19:23:14 -0300

    storage: use data encoding

--- deku-p/src/core/storage/storage.ml
+++ deku-p/src/core/storage/storage.ml
@@ -5,25 +5,36 @@ module Chain = struct
   let temp = "chain.tmp.json"
   let file = "chain.json"
 
-  let read ~env:_ ~folder =
+  let read ~env ~folder =
     let file = Filename.concat folder file in
+
     match IO.file_exists file with
     | true ->
+        let fs = Eio.Stdenv.fs env in
+        let file = Eio.Path.(fs / file) in
+        Eio.Path.with_open_in file @@ fun source ->
+        let buf = Cstruct.create 4096 in
+        let rec decoding_loop status =
+          let open Data_encoding.Binary in
+          match status with
+          | Success { result; _ } -> result
+          | Await feed ->
+              let size = Eio.Flow.read source buf in
+              let status = feed (Cstruct.to_bytes ~len:size buf) in
+              decoding_loop status
+          | Error err -> raise (Read_error err)
+        in
         let chain =
-          Eio_unix.run_in_systhread (fun () ->
-              let json = Yojson.Safe.from_file file in
-              Chain.t_of_yojson json)
+          decoding_loop (Data_encoding.Binary.read_stream Chain.encoding)
         in
         Some chain
     | false -> None
 
   let write ~env ~folder chain =
-    Eio_unix.run_in_systhread (fun () ->
-        let temp = Filename.concat folder temp in
-        let json = Chain.yojson_of_t chain in
-        Yojson.Safe.to_file temp json);
     let fs = Eio.Stdenv.fs env in
-    let temp = Eio.Path.(fs / folder / temp) in
     let file = Eio.Path.(fs / folder / file) in
+    let temp = Eio.Path.(fs / folder / temp) in
+    let binary = Data_encoding.Binary.to_string_exn Chain.encoding chain in
+    Eio.Path.save ~create:(`Or_truncate 0o644) temp binary;
     Eio.Path.rename temp file
 end
