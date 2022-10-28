6e007bbba5b4eeb4427257e9dee7fff8316c3788
InfiniteSwerve <jackofthestrand@gmail.com>
2022-10-28 13:27:08 -0700

    change data_gen into script using tezos-crypto

--- deku-p/src/core/crypto/tests/data_for_tests/data_gen.ml
+++ deku-p/src/core/crypto/tests/data_for_tests/data_gen.ml
@@ -1,13 +1,12 @@
-(* This script will generate all the data used in the Alg_intf tests 
+(* This script will generate all the data used in the Alg_intf tests
 
-Build in a project with tezos-crypto with the following dune file:
-(executable
- (name data_gen)
- (libraries tezos_crypto)
- (preprocess
-  (pps)))
-
- *)
+   Build in a project with tezos-crypto with the following dune file:
+   (executable
+    (name data_gen)
+    (libraries tezos_crypto)
+    (preprocess
+     (pps)))
+*)
 
 open Tezos_crypto
 
@@ -20,7 +19,7 @@ type ed25519_id = {
 let index = Atomic.make 0
 
 let get_incr () =
-  Atomic.incr index ;
+  Atomic.incr index;
   Atomic.get index
 
 let get_ed25519_id () =
@@ -33,26 +32,23 @@ let get_ed25519_id () =
      %!"
     (get_incr ())
     (Public_key_hash.to_b58check a)
-    (Public_key.to_b58check b)
-    (Secret_key.to_b58check c)
+    (Public_key.to_b58check b) (Secret_key.to_b58check c)
 
-let spc () = Format.eprintf "\n%!
+let spc () = Format.eprintf "\n%!"
 
-let generate_identities () = 
-  get_ed25519_id () ;
-  spc () ;
-  get_ed25519_id () ;
-  spc () ;
-  get_ed25519_id () ;
-  spc () ;
-  get_ed25519_id () ;
-  spc () ;
+let generate_identities () =
+  get_ed25519_id ();
+  spc ();
+  get_ed25519_id ();
+  spc ();
+  get_ed25519_id ();
+  spc ();
+  get_ed25519_id ();
+  spc ();
   get_ed25519_id ()
-  
 
 module Data_gen (Crypto : sig
   include S.SIGNATURE
-
   include S.RAW_DATA with type t := t
 
   type id = {
@@ -65,29 +61,29 @@ module Data_gen (Crypto : sig
 end) =
 struct
   open Crypto
-  
+
   module Skt_key = struct
     let secret_keys = List.map (fun id -> id.secret_key) ids
 
     let public_keys =
       List.map (fun sk -> Secret_key.to_public_key sk) secret_keys
-      
-    let compare_secret_keys = List.sort Secret_key.compare secret_key
 
+    let compare_secret_keys = List.sort Secret_key.compare secret_keys
   end
-  module Print_secret_key = struct 
+
+  module Print_secret_key = struct
     let print_public_keys () =
-      Format.printf "let public_keys = [\n%!" ;
+      Format.printf "let public_keys = [\n%!";
       List.iter
         (fun pk -> Format.printf "\"%s\";\n%!" (Public_key.to_b58check pk))
-        Skt_key.public_keys ;
-      Format.printf "]\n%!
+        Skt_key.public_keys;
+      Format.printf "]\n%!"
 
     let print_compare_secret_keys () =
-      Format.printf "let compared_secret_keys = [\n%!" ;
+      Format.printf "let compared_secret_keys = [\n%!";
       List.iter
         (fun sk -> Format.printf "\"%s\"\n%!;" (Secret_key.to_b58check sk))
-        Skt_key.compare_secret_keys ;
-      Format.printf "]\n%!
+        Skt_key.compare_secret_keys;
+      Format.printf "]\n%!"
   end
 end
