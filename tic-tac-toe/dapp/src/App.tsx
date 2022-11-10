c2a6996e9d52f1f689f3c2cf2ec46719cc053d61
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2022-11-10 15:35:00 +0100

    fix/tic-tac-toe: invokation

--- tic-tac-toe/dapp/src/App.tsx
+++ tic-tac-toe/dapp/src/App.tsx
@@ -10,6 +10,7 @@ import ConnectWallet from './components/Connect';
 import { DekuSigner } from '@marigold-dev/deku-toolkit/lib/utils/signers';
 import { fromBeaconSigner } from '@marigold-dev/deku-toolkit';
 import { DAppClient } from "@airgap/beacon-sdk";
+import { parse } from 'node:path/win32';
 
 const dekuRpc = "http://0.0.0.0:8080";
 const ligoRpc = "http://0.0.0.0:9090";
@@ -26,8 +27,10 @@ type gameState =
   | ["PlayerTurn", address]
   | ["Winner", address]
 
+type game = [cell, cell, cell, cell, cell, cell, cell, cell, cell];
+
 type storage = {
-  game: list<cell>,
+  game: game,
   players: {
     player1: address,
     player2: address
@@ -35,12 +38,27 @@ type storage = {
   gameState: gameState
 };
 
+
+const mapi : (fct:(i: nat, elt: cell) => cell , ls: game) => game = (f,ls) => {
+  return [
+    f(0 as nat, ls[0]),
+    f(1 as nat, ls[1]),
+    f(2 as nat, ls[2]),
+    f(3 as nat, ls[3]),
+    f(4 as nat, ls[4]),
+    f(5 as nat, ls[5]),
+    f(6 as nat, ls[6]),
+    f(7 as nat, ls[7]),
+    f(8 as nat, ls[8])
+  ]
+}
+
 const empty: storage = {
-  game: list([
+  game: [
     Cross(), Empty(), Circle(), 
     Empty(), Empty(), Empty(), 
     Empty(), Empty(), Empty()
-  ]),
+  ],
   players: {
     player1: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb" as address, // The two players are the same
     player2: "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb" as address,
@@ -54,23 +72,7 @@ type parameter =
 
 type return_ = [list<operation>, storage];
 
-const aux: <T, U> (fct: (i: nat, elt:T) => U, index:nat, ls:list<T>, acc:list<U>) => list<U> = (f, index, ls, acc) => {
-  return match(ls, list([
-    ([]: list<T>) => acc,
-    ([head, ...tl]: list<T>) => {
-      let next = f(index, head);
-      let nextAcc = list([next, ...acc]);
-      return aux(f, index + (1 as nat), tl, nextAcc)
-    }
-  ]))
-};
-
-const mapi: <T, U> (fct:(i: nat, elt: T) => U , ls: list<T>) => list<U> = (f,ls) => {
-  let acc: list<U> = list([]);
-  return aux(f, 0 as nat, ls, acc)
-}
-
-const updateCell = (cellId: nat, value:cell, game: list<cell>) => {
+const updateCell = (cellId: nat, value:cell, game: game) => {
   let apply = (index:nat, elt: cell) => {
     if(cellId != index) return elt
     return match(elt, {
@@ -96,7 +98,7 @@ const play = (player: address, cellId: nat, storage:storage): storage => {
   if(current_player != player) {return failwith("Not your turn")};
   if(cellId > (8 as nat)) {return failwith("Wrong cellId")};
   const value = getSymbol(player, storage);
-  const game = updateCell(cellId, value, storage.game);
+  const game = updateCell(cellId, value, game);
   return {game, players, gameState}
 }
 
@@ -111,17 +113,17 @@ const main = (parameter: parameter, storage: storage): return_ => {
 }
 `;
 
-const initialStorage: any = '(Pair (Pair { Left (Right Unit) ; Right Unit ; Left (Left Unit) ; Right Unit ; Right Unit ; Right Unit ; Right Unit ; Right Unit ; Right Unit } (Left "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")) "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb" "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")'
+const initialStorage: any = '(Pair(Pair(Pair(Pair(Pair(Pair(Left(Right Unit))(Right Unit))(Left(Left Unit))(Right Unit))(Pair(Right Unit)(Right Unit))(Right Unit)(Right Unit))(Right Unit))(Left "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")) "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb" "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")'
 
 const App = () => {
   const [state, setState] = useState<State | null>(null);
-  const [address, setAddress] = useState<string>("DK14GLwwWN2S3YP9cJtECh1satHj9C6MWVsG");
+  const [address, setAddress] = useState<string>("");
   const [user, setUser] = useState<{ signer: DekuSigner, address: string } | null>(null);
 
   const originate = () => {
     deku.originateContract({ kind: "jsligo", code: jsligo, initialStorage })
       .then(async ({ address, operation }) => {
-        const _ = await wait(dekuRpc, operation);
+        await wait(dekuRpc, operation);
         setAddress(address);
       })
       .catch(console.error)
@@ -129,8 +131,6 @@ const App = () => {
 
   const onClick = ({ id }: { id: number }) => {
     const move = play(id);
-    console.log(move);
-    console.log(move)
     deku.contract(address)
       .invoke(move)
       .then(console.log)
@@ -142,7 +142,16 @@ const App = () => {
     contract.getState()
       .then(parseState)
       .then(setState)
-      .catch(console.error);
+      .catch(console.error)
+  }, [address]);
+
+  useEffect(() => {
+    if (!address) return;
+    const contract = deku.contract(address);
+    contract.onNewState(state => {
+      const parsed = parseState(state);
+      setState(parsed);
+    })
   }, [address]);
 
   const connectInMemory = async (): Promise<{ signer: DekuSigner, address: string }> => {
--- tic-tac-toe/dapp/src/core/index.ts
+++ tic-tac-toe/dapp/src/core/index.ts
@@ -20,18 +20,45 @@ export interface State {
 
 // Parse the state retrieved from the deku-c-toolkit 
 export const parseState = (state: any) => {
-    const game: Array<Cell> = state[0][0].map((cell: any) => {
+    const cell1 = state[0][0][0][0][0][0];
+    const cell2 = state[0][0][0][0][0][1];
+    const cell3 = state[0][0][0][0][1][0];
+    const cell4 = state[0][0][0][0][1][1];
+    const cell5 = state[0][0][0][1][0][0];
+    const cell6 = state[0][0][0][1][0][1];
+
+    const cell7 = state[0][0][0][1][1][0];
+    const cell8 = state[0][0][0][1][1][1];
+    const cell9 = state[0][0][1]
+
+    const game = [
+        cell1,
+        cell2,
+        cell3,
+        cell4,
+        cell5,
+        cell6,
+        cell7,
+        cell8,
+        cell9,
+    ].map(cell => {
         if (cell && cell.left && cell.left.right === null) return "Cross";
         else if (cell && cell.right === null) return "Empty";
         else return "Circle"
-    });
-    const players = {
-        player1: state[1][0],
-        player2: state[1][1],
-    }
+    })
+
     const gameState: GameState = state[0][1].left
         ? { type: "PlayerTurn", payload: state[0][1].left }
         : { type: "Winner", payload: state[0][1].right }
+
+    const player1 = state[1][0]
+    const player2 = state[1][1];
+
+    const players = {
+        player1,
+        player2
+    }
+
     return {
         game,
         players,
