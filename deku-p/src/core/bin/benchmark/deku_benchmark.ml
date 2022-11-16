marigold-dev/deku

commit 2613c49cc312e459a214e17651e89b56a28fe126

move chain to data encoding (#948)
* encoding: use data encoding for blocks

* encoding: add data encoding for Map and Set

* encoding: add data encoding for Protocol

* encoding: add data encoding to Consensus

* encoding: add data encoding to Gossip

* encoding: add data encoding to Chain

* storage: use data encoding

* fixup! encoding: add data encoding for Protocol

Co-authored-by: Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>

deku-p/src/core/bin/benchmark/deku_benchmark.ml
@@ -1,9 +1,8 @@
+open Deku_crypto
 open Deku_stdlib
 open Deku_concepts
 open Deku_protocol
 open Deku_ledger
-open Deku_crypto
-open Deku_consensus
 
 let domains =
   match Sys.getenv_opt "DEKU_DOMAINS" with
@@ -11,6 +10,151 @@ let domains =
   | None -> 16
 
 let () = Format.printf "Using %d domains\n%!" domains
+
+module Util = struct
+  let benchmark ~runs f =
+    let times =
+      List.init runs (fun _n ->
+          let t1 = Unix.gettimeofday () in
+          let () = f () in
+          let t2 = Unix.gettimeofday () in
+          t2 -. t1)
+    in
+    let total = List.fold_left (fun total time -> total +. time) 0.0 times in
+    let average = total /. Float.of_int runs in
+    `Average average
+end
+
+module Zero_ops = struct
+  let default_ticket_id =
+    let address =
+      Deku_tezos.Contract_hash.of_b58 "KT1JQ5JQB4P1c8U8ACxfnodtZ4phDVMSDzgi"
+      |> Option.get
+    in
+    let data = Bytes.of_string "" in
+    Ticket_id.make (Tezos address) data
+
+  let generate () =
+    let secret = Ed25519.Secret.generate () in
+    let secret = Secret.Ed25519 secret in
+    Identity.make secret
+
+  let zero_operation ~level =
+    let identity = generate () in
+    let receiver =
+      let receiver = generate () in
+      let receiver = Identity.key_hash receiver in
+      Address.of_key_hash receiver
+    in
+    let nonce = Nonce.of_n N.zero in
+    let amount = Amount.of_n N.zero in
+    let ticket_id = default_ticket_id in
+    Operation.Signed.ticket_transfer ~identity ~level ~nonce ~receiver
+      ~ticket_id ~amount
+
+  let payload_of_operations operations =
+    (* TODO: this likely should not be here *)
+    List.map
+      (fun operation ->
+        let json = Operation.Signed.yojson_of_t operation in
+        Yojson.Safe.to_string json)
+      operations
+
+  let big_payload ~size ~level =
+    let () = Format.eprintf "preparing...\n%!" in
+    let operations = Parallel.init_p size (fun _ -> zero_operation ~level) in
+    payload_of_operations operations
+
+  (* TODO: parametrize over domains *)
+  let perform ~runs ~protocol ~level ~payload =
+    let () = Format.eprintf "running...\n%!" in
+
+    let (`Average average) =
+      Util.benchmark ~runs (fun () ->
+          let payload =
+            Protocol.prepare ~parallel:Parallel.filter_map_p ~payload
+          in
+          let _protocol, _receipts, _errors =
+            Protocol.apply ~current_level:level ~tezos_operations:[] ~payload
+              protocol
+          in
+          ())
+    in
+    Format.eprintf "average: %f\n" average;
+    average
+
+  let run () =
+    let protocol = Protocol.initial in
+    let level = Level.zero in
+    let payload = big_payload ~size:50000 ~level in
+    perform ~runs:10 ~protocol ~level ~payload
+end
+
+(* TODO: enable this *)
+(* module Block_production = struct
+     let default_ticket_id =
+       let address =
+         Deku_tezos.Contract_hash.of_b58 "KT1JQ5JQB4P1c8U8ACxfnodtZ4phDVMSDzgi"
+         |> Option.get
+       in
+       let data = Bytes.of_string "tuturu" in
+       Ticket_id.make address data
+
+     let producer = Zero_ops.generate ()
+     let sender = Zero_ops.generate ()
+
+     let receiver =
+       Zero_ops.generate () |> Identity.key_hash |> Address.of_key_hash
+
+     let operation ~nonce =
+       let level = Level.zero in
+       let nonce = Z.of_int nonce |> N.of_z |> Option.get |> Nonce.of_n in
+       let ticket_id = default_ticket_id in
+       let amount = Amount.of_n N.zero in
+       Operation.ticket_transfer ~identity:sender ~level ~nonce ~receiver
+         ~ticket_id ~amount
+
+     let operations ~size =
+       Parallel.init_p pool size (fun nonce -> operation ~nonce)
+
+     let produce_block ~operations =
+       let open Deku_consensus in
+       let level = Level.zero in
+       let (Block.Block { previous; _ }) = Genesis.block in
+       let _block =
+         Block.produce ~parallel_map:(Parallel.map_p pool) ~identity:producer
+           ~level ~previous ~operations ~tezos_operations:[]
+       in
+       ()
+
+     let perform ~runs ~size =
+       let () = Format.eprintf "running block production...\n%!" in
+       let operations = operations ~size in
+       let (`Average average) =
+         Util.benchmark ~runs (fun () -> produce_block ~operations)
+       in
+       average
+
+     let run () =
+       let size = 50000 in
+       let average = perform ~runs:5 ~size in
+       let tx_packed_per_sec = Float.of_int size /. average in
+       Format.eprintf "average run time: %3f. tx packed/s: %3f\n%!" average
+         tx_packed_per_sec;
+       average
+   end *)
+
+let _alpha () =
+  Eio_main.run @@ fun env -> Parallel.Pool.run ~env ~domains Zero_ops.run
+(* let pi = Parallel.Pool.run pool Block_production.run
+   let total = alpha +. pi
+   let () = Format.printf "alpha + pi: %3f\n%!" total *)
+
+open Deku_stdlib
+open Deku_crypto
+open Deku_concepts
+open Deku_protocol
+open Deku_consensus
 
 let identity =
   let secret = Ed25519.Secret.generate () in
@@ -24,86 +168,72 @@ let block ~default_block_size =
   Producer.produce ~identity ~default_block_size ~above ~withdrawal_handles_hash
     producer
 
-module type BENCH = sig
-  type t
+let bench f =
+  let t1 = Unix.gettimeofday () in
+  let () = f () in
+  let t2 = Unix.gettimeofday () in
+  t2 -. t1
 
-  val name : string
-  val item_message : string
-  val items : int
-  val prepare : unit -> t
-  val run : t -> unit
-end
+let bench msg ~items ~prepare run =
+  let runs = 10 in
+  let value = prepare () in
+  let results =
+    List.init runs (fun n ->
+        let time = bench (fun () -> run value) in
+        Format.eprintf "%s(%d): %.3f\n%!" msg n time;
+        time)
+  in
+  let total = List.fold_left (fun acc time -> acc +. time) 0.0 results in
+  let average = total /. Int.to_float runs in
+  let per_second = Int.to_float items /. average in
+  Format.eprintf "%s: %.3f\n%!" msg average;
+  Format.eprintf "%s: %.3f/s\n%!" msg per_second
 
-module Produce : BENCH = struct
-  type t = unit
-
-  let items = 1_000_000
-  let name = "produce"
-  let item_message = Format.sprintf "%d" items
-  let prepare () = ()
+let produce () =
+  let items = 1_000_000 in
+  let prepare () = () in
   (* 6kk/s on 8 domains *)
+  bench "produce" ~items ~prepare @@ fun () ->
+  let (_ : Block.t) = block ~default_block_size:items in
+  ()
 
-  let run () =
-    let (_ : Block.t) = block ~default_block_size:items in
-    ()
-end
+let produce_and_serialize () =
+  let items = 1_000_000 in
+  let prepare () = () in
+  bench "produce_and_serialize" ~items ~prepare @@ fun () ->
+  let block = block ~default_block_size:items in
+  let (_ : string) = Data_encoding.Binary.to_string_exn Block.encoding block in
+  ()
 
-module Block_encode : BENCH = struct
-  type t = Block.block
+let produce_and_serialize_and_parse () =
+  let items = 1_000_000 in
+  let prepare () = () in
+  bench "produce_and_serialize_and_parse" ~items ~prepare @@ fun () ->
+  let block = block ~default_block_size:items in
+  let (binary : string) =
+    Data_encoding.Binary.to_string_exn Block.encoding block
+  in
+  let (_ : Block.t) =
+    Data_encoding.Binary.of_string_exn Block.encoding binary
+  in
+  ()
 
-  let name = "block_encode"
-  let items = 1_000_000
-  let prepare () = block ~default_block_size:items
-  let item_message = Format.sprintf "%d" items
-
-  let run block =
-    let (_ : string list) = Block.encode block in
-    ()
-end
-
-module Block_decode : BENCH = struct
-  type t = string list
-
-  let name = "block_decode"
-  let items = 1_000_000
-  let item_message = Format.sprintf "block size of %d" items
-
-  let prepare () =
-    let block = block ~default_block_size:items in
-    Block.encode block
-
-  let run fragments =
-    let (_ : Block.t) = Block.decode fragments in
-    ()
-end
-
-module Prepare_and_decode = struct
-  type t = string
-
-  let name = "prepare_and_decode"
-  let items = 100_000
-  let item_message = Format.sprintf "block size of %d" items
-
+let prepare_and_decode () =
+  let items = 100_000 in
   let prepare () =
     let (Block.Block { payload; _ }) = block ~default_block_size:items in
+
     payload
-
-  let parallel = Parallel.filter_map_p
-
+  in
+  let parallel = Parallel.filter_map_p in
   (* 100k/s on 8 domains *)
-  let run payload =
-    let (Payload payload) = Payload.decode ~payload in
-    let (_ : Operation.Initial.t list) = Protocol.prepare ~parallel ~payload in
-    ()
-end
+  bench "prepare_and_decode" ~items ~prepare @@ fun payload ->
+  let (Payload payload) = Payload.decode ~payload in
+  let (_ : Operation.Initial.t list) = Protocol.prepare ~parallel ~payload in
+  ()
 
-module Verify : BENCH = struct
-  type t = Key.key * (BLAKE2b.t * Signature.signature) list
-
-  let name = "verify"
-  let items = 100_000
-  let item_message = Format.sprintf "%d signatures" items
-
+let verify () =
+  let items = 100_000 in
   let prepare () =
     let identity =
       let secret = Ed25519.Secret.generate () in
@@ -119,30 +249,23 @@ module Verify : BENCH = struct
           (hash, sign))
     in
     (key, items)
+  in
+  bench "verify" ~items ~prepare @@ fun (key, items) ->
+  let _units : unit list =
+    Parallel.map_p
+      (fun (hash, sign) -> assert (Signature.verify key sign hash))
+      items
+  in
+  ()
 
-  let run (key, items) =
-    let _units : unit list =
-      Parallel.map_p
-        (fun (hash, sign) -> assert (Signature.verify key sign hash))
-        items
-    in
-    ()
-end
-
-module Ledger_balance = struct
-  type t =
-    Ledger.ledger * (Address.address * Amount.amount * Ticket_id.ticket_id) list
-
-  let name = "ledger_balance"
-  let items = 100_000
-  let item_message = Format.sprintf "%d tickets" items
-
+let ledger_balance () =
+  let items = 100_000 in
   let address () =
     let secret = Ed25519.Secret.generate () in
     let secret = Secret.Ed25519 secret in
     let identity = Identity.make secret in
     Address.of_key_hash (Identity.key_hash identity)
-
+  in
   let prepare () =
     let items =
       Parallel.init_p items (fun n ->
@@ -170,35 +293,26 @@ module Ledger_balance = struct
         Ledger.initial items
     in
     (ledger, items)
+  in
+  bench "ledger_balance" ~items ~prepare @@ fun (ledger, items) ->
+  let (_ : unit) =
+    List.iter
+      (fun (sender, amount, ticket_id) ->
+        let balance = Ledger.balance sender ticket_id ledger in
+        (* TODO: this is a >= because of rng collision *)
+        assert (balance >= amount))
+      items
+  in
+  ()
 
-  let run (ledger, items) =
-    let (_ : unit) =
-      List.iter
-        (fun (sender, amount, ticket_id) ->
-          let balance = Ledger.balance sender ticket_id ledger in
-          (* TODO: this is a >= because of rng collision *)
-          assert (balance >= amount))
-        items
-    in
-    ()
-end
-
-module Ledger_transfer : BENCH = struct
-  type t =
-    Ledger.ledger
-    * (Address.address * Address.address * Amount.amount * Ticket_id.ticket_id)
-      list
-
-  let name = "ledger_transfer"
-  let items = 100_000
-  let item_message = Format.sprintf "%d transfers" items
-
+let ledger_transfer () =
+  let items = 100_000 in
   let address () =
     let secret = Ed25519.Secret.generate () in
     let secret = Secret.Ed25519 secret in
     let identity = Identity.make secret in
     Address.of_key_hash (Identity.key_hash identity)
-
+  in
   let prepare () =
     let items =
       Parallel.init_p items (fun n ->
@@ -226,56 +340,29 @@ module Ledger_transfer : BENCH = struct
         Ledger.initial items
     in
     (ledger, items)
-
-  let run (ledger, items) =
-    let (_ : Ledger.t) =
-      List.fold_left
-        (fun ledger (sender, receiver, amount, ticket_id) ->
-          Result.get_ok
-            (Ledger.transfer ~sender ~receiver ~amount ~ticket_id ledger))
-        ledger items
-    in
-    ()
-end
-
-let bench f =
-  let t1 = Unix.gettimeofday () in
-  let () = f () in
-  let t2 = Unix.gettimeofday () in
-  t2 -. t1
-
-let run_benchmark ~formatter ~writer (module Bench : BENCH) =
-  let open Bench in
-  let runs = 10 in
-  let value = prepare () in
-  let _ =
-    List.init runs (fun _ ->
-        let time = bench (fun () -> run value) in
-        let data = formatter ~name ~item_message ~data:time in
-        writer ~data)
+  in
+  bench "ledger_transfer" ~items ~prepare @@ fun (ledger, items) ->
+  let (_ : Ledger.t) =
+    List.fold_left
+      (fun ledger (sender, receiver, amount, ticket_id) ->
+        Result.get_ok
+          (Ledger.transfer ~sender ~receiver ~amount ~ticket_id ledger))
+      ledger items
   in
   ()
 
-let benches : (module BENCH) list =
+let benches =
   [
-    (module Produce);
-    (module Block_encode);
-    (module Block_decode);
-    (module Prepare_and_decode);
-    (module Verify);
-    (module Ledger_balance);
-    (module Ledger_transfer);
+    produce;
+    produce_and_serialize;
+    produce_and_serialize_and_parse;
+    prepare_and_decode;
+    verify;
+    ledger_balance;
+    ledger_transfer;
   ]
-
-let formatter ~name ~item_message ~data =
-  Format.sprintf "%s with %s: %.3f\n" name item_message data
-
-let writer ~data = Format.eprintf "%s%!" data
 
 let () =
   Eio_main.run @@ fun env ->
   Parallel.Pool.run ~env ~domains @@ fun () ->
-  List.iter
-    (fun (module Bench : BENCH) ->
-      run_benchmark ~formatter ~writer (module Bench))
-    benches
+  List.iter (fun bench -> bench ()) benches

deku-c/wasm-vm-ocaml/state.mli
@@ -8,3 +8,4 @@ val empty : t
 val add_contract : t -> Contract_address.t -> State_entry.t -> t
 val fetch_contract : t -> Contract_address.t -> State_entry.t
 val to_json_api : t -> Yojson.Safe.t
+val encoding : t Data_encoding.t

deku-p/src/core/chain/chain.ml
@@ -17,6 +17,18 @@ type chain =
 
 and t = chain [@@deriving yojson]
 
+let encoding =
+  let open Data_encoding in
+  conv
+    (fun (Chain
+           { gossip; protocol; consensus; producer; oldest_trusted; trusted }) ->
+      (gossip, protocol, consensus, producer, oldest_trusted, trusted))
+    (fun (gossip, protocol, consensus, producer, oldest_trusted, trusted) ->
+      Chain { gossip; protocol; consensus; producer; oldest_trusted; trusted })
+    (tup6 Gossip.encoding Protocol.encoding Consensus.encoding Producer.encoding
+       Level.encoding
+       (Level.Map.encoding Message.Network.encoding))
+
 type fragment =
   | Fragment_gossip of { fragment : Gossip.fragment }
   | Fragment_produce of {

deku-p/src/core/chain/chain.mli
@@ -15,6 +15,9 @@ type chain = private
     }
 
 type t = chain [@@deriving yojson]
+
+val encoding : chain Data_encoding.t
+
 type fragment
 type outcome
 

commit bd11c96443412bc6d8b45eb3d5a6ba70acc4a406

benchmark: fix regressions introduced by #948

deku-p/src/core/bin/benchmark/deku_benchmark.ml
@@ -1,8 +1,9 @@
-open Deku_crypto
 open Deku_stdlib
 open Deku_concepts
 open Deku_protocol
 open Deku_ledger
+open Deku_crypto
+open Deku_consensus
 
 let domains =
   match Sys.getenv_opt "DEKU_DOMAINS" with
@@ -10,151 +11,6 @@ let domains =
   | None -> 16
 
 let () = Format.printf "Using %d domains\n%!" domains
-
-module Util = struct
-  let benchmark ~runs f =
-    let times =
-      List.init runs (fun _n ->
-          let t1 = Unix.gettimeofday () in
-          let () = f () in
-          let t2 = Unix.gettimeofday () in
-          t2 -. t1)
-    in
-    let total = List.fold_left (fun total time -> total +. time) 0.0 times in
-    let average = total /. Float.of_int runs in
-    `Average average
-end
-
-module Zero_ops = struct
-  let default_ticket_id =
-    let address =
-      Deku_tezos.Contract_hash.of_b58 "KT1JQ5JQB4P1c8U8ACxfnodtZ4phDVMSDzgi"
-      |> Option.get
-    in
-    let data = Bytes.of_string "" in
-    Ticket_id.make (Tezos address) data
-
-  let generate () =
-    let secret = Ed25519.Secret.generate () in
-    let secret = Secret.Ed25519 secret in
-    Identity.make secret
-
-  let zero_operation ~level =
-    let identity = generate () in
-    let receiver =
-      let receiver = generate () in
-      let receiver = Identity.key_hash receiver in
-      Address.of_key_hash receiver
-    in
-    let nonce = Nonce.of_n N.zero in
-    let amount = Amount.of_n N.zero in
-    let ticket_id = default_ticket_id in
-    Operation.Signed.ticket_transfer ~identity ~level ~nonce ~receiver
-      ~ticket_id ~amount
-
-  let payload_of_operations operations =
-    (* TODO: this likely should not be here *)
-    List.map
-      (fun operation ->
-        let json = Operation.Signed.yojson_of_t operation in
-        Yojson.Safe.to_string json)
-      operations
-
-  let big_payload ~size ~level =
-    let () = Format.eprintf "preparing...\n%!" in
-    let operations = Parallel.init_p size (fun _ -> zero_operation ~level) in
-    payload_of_operations operations
-
-  (* TODO: parametrize over domains *)
-  let perform ~runs ~protocol ~level ~payload =
-    let () = Format.eprintf "running...\n%!" in
-
-    let (`Average average) =
-      Util.benchmark ~runs (fun () ->
-          let payload =
-            Protocol.prepare ~parallel:Parallel.filter_map_p ~payload
-          in
-          let _protocol, _receipts, _errors =
-            Protocol.apply ~current_level:level ~tezos_operations:[] ~payload
-              protocol
-          in
-          ())
-    in
-    Format.eprintf "average: %f\n" average;
-    average
-
-  let run () =
-    let protocol = Protocol.initial in
-    let level = Level.zero in
-    let payload = big_payload ~size:50000 ~level in
-    perform ~runs:10 ~protocol ~level ~payload
-end
-
-(* TODO: enable this *)
-(* module Block_production = struct
-     let default_ticket_id =
-       let address =
-         Deku_tezos.Contract_hash.of_b58 "KT1JQ5JQB4P1c8U8ACxfnodtZ4phDVMSDzgi"
-         |> Option.get
-       in
-       let data = Bytes.of_string "tuturu" in
-       Ticket_id.make address data
-
-     let producer = Zero_ops.generate ()
-     let sender = Zero_ops.generate ()
-
-     let receiver =
-       Zero_ops.generate () |> Identity.key_hash |> Address.of_key_hash
-
-     let operation ~nonce =
-       let level = Level.zero in
-       let nonce = Z.of_int nonce |> N.of_z |> Option.get |> Nonce.of_n in
-       let ticket_id = default_ticket_id in
-       let amount = Amount.of_n N.zero in
-       Operation.ticket_transfer ~identity:sender ~level ~nonce ~receiver
-         ~ticket_id ~amount
-
-     let operations ~size =
-       Parallel.init_p pool size (fun nonce -> operation ~nonce)
-
-     let produce_block ~operations =
-       let open Deku_consensus in
-       let level = Level.zero in
-       let (Block.Block { previous; _ }) = Genesis.block in
-       let _block =
-         Block.produce ~parallel_map:(Parallel.map_p pool) ~identity:producer
-           ~level ~previous ~operations ~tezos_operations:[]
-       in
-       ()
-
-     let perform ~runs ~size =
-       let () = Format.eprintf "running block production...\n%!" in
-       let operations = operations ~size in
-       let (`Average average) =
-         Util.benchmark ~runs (fun () -> produce_block ~operations)
-       in
-       average
-
-     let run () =
-       let size = 50000 in
-       let average = perform ~runs:5 ~size in
-       let tx_packed_per_sec = Float.of_int size /. average in
-       Format.eprintf "average run time: %3f. tx packed/s: %3f\n%!" average
-         tx_packed_per_sec;
-       average
-   end *)
-
-let _alpha () =
-  Eio_main.run @@ fun env -> Parallel.Pool.run ~env ~domains Zero_ops.run
-(* let pi = Parallel.Pool.run pool Block_production.run
-   let total = alpha +. pi
-   let () = Format.printf "alpha + pi: %3f\n%!" total *)
-
-open Deku_stdlib
-open Deku_crypto
-open Deku_concepts
-open Deku_protocol
-open Deku_consensus
 
 let identity =
   let secret = Ed25519.Secret.generate () in
@@ -168,72 +24,92 @@ let block ~default_block_size =
   Producer.produce ~identity ~default_block_size ~above ~withdrawal_handles_hash
     producer
 
-let bench f =
-  let t1 = Unix.gettimeofday () in
-  let () = f () in
-  let t2 = Unix.gettimeofday () in
-  t2 -. t1
+module type BENCH = sig
+  type t
 
-let bench msg ~items ~prepare run =
-  let runs = 10 in
-  let value = prepare () in
-  let results =
-    List.init runs (fun n ->
-        let time = bench (fun () -> run value) in
-        Format.eprintf "%s(%d): %.3f\n%!" msg n time;
-        time)
-  in
-  let total = List.fold_left (fun acc time -> acc +. time) 0.0 results in
-  let average = total /. Int.to_float runs in
-  let per_second = Int.to_float items /. average in
-  Format.eprintf "%s: %.3f\n%!" msg average;
-  Format.eprintf "%s: %.3f/s\n%!" msg per_second
+  val name : string
+  val item_message : string
+  val items : int
+  val prepare : unit -> t
+  val run : t -> unit
+end
 
-let produce () =
-  let items = 1_000_000 in
-  let prepare () = () in
+module Produce : BENCH = struct
+  type t = unit
+
+  let items = 1_000_000
+  let name = "produce"
+  let item_message = Format.sprintf "%d" items
+  let prepare () = ()
   (* 6kk/s on 8 domains *)
-  bench "produce" ~items ~prepare @@ fun () ->
-  let (_ : Block.t) = block ~default_block_size:items in
-  ()
 
-let produce_and_serialize () =
-  let items = 1_000_000 in
-  let prepare () = () in
-  bench "produce_and_serialize" ~items ~prepare @@ fun () ->
-  let block = block ~default_block_size:items in
-  let (_ : string) = Data_encoding.Binary.to_string_exn Block.encoding block in
-  ()
+  let run () =
+    let (_ : Block.t) = block ~default_block_size:items in
+    ()
+end
 
-let produce_and_serialize_and_parse () =
-  let items = 1_000_000 in
-  let prepare () = () in
-  bench "produce_and_serialize_and_parse" ~items ~prepare @@ fun () ->
-  let block = block ~default_block_size:items in
-  let (binary : string) =
-    Data_encoding.Binary.to_string_exn Block.encoding block
-  in
-  let (_ : Block.t) =
-    Data_encoding.Binary.of_string_exn Block.encoding binary
-  in
-  ()
+module Prepare_and_decode : BENCH = struct
+  type t = string
 
-let prepare_and_decode () =
-  let items = 100_000 in
+  let name = "prepare_and_decode"
+  let items = 100_000
+  let item_message = Format.sprintf "block size of %d" items
+
   let prepare () =
     let (Block.Block { payload; _ }) = block ~default_block_size:items in
-
     payload
-  in
-  let parallel = Parallel.filter_map_p in
-  (* 100k/s on 8 domains *)
-  bench "prepare_and_decode" ~items ~prepare @@ fun payload ->
-  let (Payload payload) = Payload.decode ~payload in
-  let (_ : Operation.Initial.t list) = Protocol.prepare ~parallel ~payload in
-  ()
 
-let verify () =
-  let items = 100_000 in
+  let parallel = Parallel.filter_map_p
+
+  (* 100k/s on 8 domains *)
+  let run payload =
+    let (Payload payload) = Payload.decode ~payload in
+    let (_ : Operation.Initial.t list) = Protocol.prepare ~parallel ~payload in
+    ()
+end
+
+module Produce_and_serialize : BENCH = struct
+  type t = unit
+
+  let name = "produce_and_serialize"
+  let items = 1_000_000
+  let item_message = Format.sprintf "%d" items
+  let prepare () = ()
+
+  let run () =
+    let block = block ~default_block_size:items in
+    let (_ : string) =
+      Data_encoding.Binary.to_string_exn Block.encoding block
+    in
+    ()
+end
+
+module Produce_and_serialize_and_parse : BENCH = struct
+  type t = unit
+
+  let name = "produce_and_serialize_and_parse"
+  let items = 1_000_000
+  let item_message = Format.sprintf "%d" items
+  let prepare () = ()
+
+  let run () =
+    let block = block ~default_block_size:items in
+    let (binary : string) =
+      Data_encoding.Binary.to_string_exn Block.encoding block
+    in
+    let (_ : Block.t) =
+      Data_encoding.Binary.of_string_exn Block.encoding binary
+    in
+    ()
+end
+
+module Verify : BENCH = struct
+  type t = Key.key * (BLAKE2b.t * Signature.signature) list
+
+  let name = "verify"
+  let items = 100_000
+  let item_message = Format.sprintf "%d signatures" items
+
   let prepare () =
     let identity =
       let secret = Ed25519.Secret.generate () in
@@ -249,23 +125,30 @@ let verify () =
           (hash, sign))
     in
     (key, items)
-  in
-  bench "verify" ~items ~prepare @@ fun (key, items) ->
-  let _units : unit list =
-    Parallel.map_p
-      (fun (hash, sign) -> assert (Signature.verify key sign hash))
-      items
-  in
-  ()
 
-let ledger_balance () =
-  let items = 100_000 in
+  let run (key, items) =
+    let _units : unit list =
+      Parallel.map_p
+        (fun (hash, sign) -> assert (Signature.verify key sign hash))
+        items
+    in
+    ()
+end
+
+module Ledger_balance = struct
+  type t =
+    Ledger.ledger * (Address.address * Amount.amount * Ticket_id.ticket_id) list
+
+  let name = "ledger_balance"
+  let items = 100_000
+  let item_message = Format.sprintf "%d tickets" items
+
   let address () =
     let secret = Ed25519.Secret.generate () in
     let secret = Secret.Ed25519 secret in
     let identity = Identity.make secret in
     Address.of_key_hash (Identity.key_hash identity)
-  in
+
   let prepare () =
     let items =
       Parallel.init_p items (fun n ->
@@ -293,26 +176,35 @@ let ledger_balance () =
         Ledger.initial items
     in
     (ledger, items)
-  in
-  bench "ledger_balance" ~items ~prepare @@ fun (ledger, items) ->
-  let (_ : unit) =
-    List.iter
-      (fun (sender, amount, ticket_id) ->
-        let balance = Ledger.balance sender ticket_id ledger in
-        (* TODO: this is a >= because of rng collision *)
-        assert (balance >= amount))
-      items
-  in
-  ()
 
-let ledger_transfer () =
-  let items = 100_000 in
+  let run (ledger, items) =
+    let (_ : unit) =
+      List.iter
+        (fun (sender, amount, ticket_id) ->
+          let balance = Ledger.balance sender ticket_id ledger in
+          (* TODO: this is a >= because of rng collision *)
+          assert (balance >= amount))
+        items
+    in
+    ()
+end
+
+module Ledger_transfer : BENCH = struct
+  type t =
+    Ledger.ledger
+    * (Address.address * Address.address * Amount.amount * Ticket_id.ticket_id)
+      list
+
+  let name = "ledger_transfer"
+  let items = 100_000
+  let item_message = Format.sprintf "%d transfers" items
+
   let address () =
     let secret = Ed25519.Secret.generate () in
     let secret = Secret.Ed25519 secret in
     let identity = Identity.make secret in
     Address.of_key_hash (Identity.key_hash identity)
-  in
+
   let prepare () =
     let items =
       Parallel.init_p items (fun n ->
@@ -340,29 +232,56 @@ let ledger_transfer () =
         Ledger.initial items
     in
     (ledger, items)
-  in
-  bench "ledger_transfer" ~items ~prepare @@ fun (ledger, items) ->
-  let (_ : Ledger.t) =
-    List.fold_left
-      (fun ledger (sender, receiver, amount, ticket_id) ->
-        Result.get_ok
-          (Ledger.transfer ~sender ~receiver ~amount ~ticket_id ledger))
-      ledger items
+
+  let run (ledger, items) =
+    let (_ : Ledger.t) =
+      List.fold_left
+        (fun ledger (sender, receiver, amount, ticket_id) ->
+          Result.get_ok
+            (Ledger.transfer ~sender ~receiver ~amount ~ticket_id ledger))
+        ledger items
+    in
+    ()
+end
+
+let bench f =
+  let t1 = Unix.gettimeofday () in
+  let () = f () in
+  let t2 = Unix.gettimeofday () in
+  t2 -. t1
+
+let run_benchmark ~formatter ~writer (module Bench : BENCH) =
+  let open Bench in
+  let runs = 10 in
+  let value = prepare () in
+  let _ =
+    List.init runs (fun _ ->
+        let time = bench (fun () -> run value) in
+        let data = formatter ~name ~item_message ~data:time in
+        writer ~data)
   in
   ()
 
-let benches =
+let benches : (module BENCH) list =
   [
-    produce;
-    produce_and_serialize;
-    produce_and_serialize_and_parse;
-    prepare_and_decode;
-    verify;
-    ledger_balance;
-    ledger_transfer;
+    (module Produce);
+    (module Prepare_and_decode);
+    (module Produce_and_serialize);
+    (module Produce_and_serialize_and_parse);
+    (module Verify);
+    (module Ledger_balance);
+    (module Ledger_transfer);
   ]
+
+let formatter ~name ~item_message ~data =
+  Format.sprintf "%s with %s: %.3f\n" name item_message data
+
+let writer ~data = Format.eprintf "%s%!" data
 
 let () =
   Eio_main.run @@ fun env ->
   Parallel.Pool.run ~env ~domains @@ fun () ->
-  List.iter (fun bench -> bench ()) benches
+  List.iter
+    (fun (module Bench : BENCH) ->
+      run_benchmark ~formatter ~writer (module Bench))
+    benches