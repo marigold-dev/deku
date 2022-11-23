f2763a094b21cc902b1d106fbc0afdf3154513b3
Daniel Hines <d4hines@gmail.com>
2022-11-23 13:45:20 -0500

    drop unused ocaml deku-cli

--- deku-p/src/cli/deku_cli.ml
+++
@@ -1,8 +0,0 @@
-open Cmdliner
-
-let () =
-  let cmd =
-    Cmd.group (Cmd.info "deku-cli")
-      [ Submit_transaction.cmd; Generate_identity.cmd ]
-  in
-  exit (Cmdliner.Cmd.eval ~catch:true cmd)
