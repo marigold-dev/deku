b45657d0bb9be262a6c07afa15c89ee202ed2791
Daniel Hines <d4hines@gmail.com>
2022-11-23 20:45:36 -0500

    drop support for deku-p cli
    Temporary solution until we add support for deku-p back to main.

--- deku-p/deku-cli/commands/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as make_mock_transaction } from "./mock-transaction";
-export { default as make_custom_transaction } from "./custom-transaction";
-export { default as make_generate_identity } from "./generate-identity";
