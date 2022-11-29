231c06f99f398b57d6cc7dd06f912769cdf9b8d7
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-11-29 13:49:06 +0100

    test: invokation

--- examples/tests/src/index.ts
+++ examples/tests/src/index.ts
@@ -1,6 +1,7 @@
 import { Command, Option } from 'commander';
 import { handleResult } from './utils';
 import Origination from "./origination";
+import Invokation from "./invokation";
 
 const program = new Command();
 
@@ -13,5 +14,12 @@ program.command("origination")
     .addOption(new Option('-l, --ligoRpc <ligo>').default('http://0.0.0.0:9090', 'http://0.0.0.0:9090').env("LIGO_RPC"))
     .addOption(new Option('-s, --secret <secret>').default('edsk38AUrkQJ9y48wxokKWdFedQpKpnCsMLrFjvAw9ZGJ5vrqXmYCo', 'edsk38AUrkQJ9y48wxokKWdFedQpKpnCsMLrFjvAw9ZGJ5vrqXmYCo').env("USER_SECRET"))
     .action(handleResult(Origination.run))
+
+program.command("invokation")
+    .addOption(new Option('-d, --dekuRpc <deku>').default('http://0.0.0.0:8080', 'http://0.0.0.0:8080').env("DEKU_RPC"))
+    .addOption(new Option('-l, --ligoRpc <ligo>').default('http://0.0.0.0:9090', 'http://0.0.0.0:9090').env("LIGO_RPC"))
+    .addOption(new Option('-s, --secret <secret>').default('edsk38AUrkQJ9y48wxokKWdFedQpKpnCsMLrFjvAw9ZGJ5vrqXmYCo', 'edsk38AUrkQJ9y48wxokKWdFedQpKpnCsMLrFjvAw9ZGJ5vrqXmYCo').env("USER_SECRET"))
+    .argument("<contract-address>", "address of the deku wasm contract")
+    .action(handleResult(Invokation.run))
 
 program.parse()
