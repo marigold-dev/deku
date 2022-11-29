16fd19607a4f961c4d09965a19782feee7b4d08a
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-11-29 11:43:16 +0100

    test: origination

---
+++ examples/tests/src/origination.ts
@@ -0,0 +1,24 @@
+import { DekuCClient, fromMemorySigner } from "@marigold-dev/deku";
+import { InMemorySigner } from "@taquito/signer";
+import {code, initialStorage, wait} from "./utils";
+
+const run = async ({dekuRpc, ligoRpc, secret}): Promise<string> => {
+    const signer = new InMemorySigner(secret);
+    const dekuSigner = fromMemorySigner(signer);
+    const deku = new DekuCClient({dekuRpc, ligoRpc, dekuSigner});
+    // Originate a contract
+    const {operation, address} = await deku.originateLigo({kind: "jsligo", code, initialStorage});
+    // The operation should be included
+    await wait(dekuRpc, operation);
+    // Retrieve the state of the contract
+    const contract = deku.contract(address);
+    const state = await contract.getState();
+    // The contract should have a state
+    if(state === null) throw `The contract ${address} has an empty state`;
+    if(state !== initialStorage) throw `Invalid initial storage ${state}, expected ${initialStorage}`;
+    return address;
+}
+
+export default {
+    run
+}
---
+++ examples/tests/src/utils.ts
@@ -0,0 +1,61 @@
+/**
+ * Exit the program when the promise raises an exception
+ * @param promise 
+ * @returns exit(1) is the promise failed, exit(0) otherwise
+ */
+export const handleResult = (promise) => (params) => promise(params).then(console.log).catch(err => {
+    console.error(err);
+    process.exit(1);
+});
+
+/**
+ * Wait for an operation to be included
+ * @param dekuRpc the rpc of the deku node
+ * @param operation the operation hash to wait
+ * @param tries the number of try before admitting the operation is not included
+ * @returns void if success, raises an exception otherwise
+ */
+export const wait = async (dekuRpc: string, operation, tries=10): Promise<void> => {
+    if(tries <= 0) throw `The operation ${operation} has not been applied`;
+    const res = await fetch(`${dekuRpc}/api/v1/operations/${operation}`);
+    if(res.ok) return;
+    await new Promise(resolve => setTimeout(resolve, 1000)); // 1000 is approximately the time for one block
+    return wait(dekuRpc, operation, tries -1);
+}
+
+/**
+ * Initial storage of the following contract
+ */
+export const initialStorage = 0;
+
+/**
+ * Counter example with 3 entrypoints:
+ *  - Increment(int) to increment the counter
+ *  - Decrement(int) to decrement the counter
+ *  - Reset() to reset to 0 the counter
+ */
+export const code = `
+type storage = int;
+
+const empty: storage = 0;
+
+type parameter =
+    | ["Increment", int]
+    | ["Decrement", int]
+    | ["Reset"];
+
+const increment = (storage: storage, delta: int) => storage + delta;
+const decrement = (storage: storage, delta: int) => storage - delta;
+const reset = () => empty;
+
+const main = (action: parameter, storage: storage) : [list<operation>, storage] => {
+    return [
+        list([]),
+        (match(action, {
+            Increment: delta => increment(storage, delta),
+            Decrement: delta => decrement(storage, delta),
+            Reset: () => reset(),
+        }))
+    ]
+}`;
+
