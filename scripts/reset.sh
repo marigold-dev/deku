90b35ed2e02b5ada4367b8bc414c4b04a918d2d9
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-12-05 16:10:39 +0100

    fix: remove api state in reset script

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -11,6 +11,7 @@ for N in 0 1 2 3; do
     rm -rf "./flextesa_chain/data/$N/api_vm_pipe_read"
     rm -rf "./flextesa_chain/data/$N/api_vm_pipe_write"
     rm -rf "./flextesa_chain/data/$N/deku_api.json"
+    rm -rf "./flextesa_chain/data/$N/deku_api.tmp.json"
 done
 
 rm -rf /tmp/api_database.db
