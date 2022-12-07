00b2d41013fca7cffcced778c9ac40d1237b1616
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-12-07 16:02:48 +0100

    fix: client get state

--- deku-c/client/src/deku-c/contract.ts
+++ deku-c/client/src/deku-c/contract.ts
@@ -149,14 +149,13 @@ export class Contract {
    * Returns the data of the contract as a wasm-vm object
    * @returns an object
    */
-  async getRawInfos(): Promise<{ [x: string]: JSONType } | null> {
-    const response: { [key: string]: string } =
-      (await this.deku.getVmState()) as { [key: string]: string };
+  async getRawInfos(): Promise<{ [key: string]: JSONType } | null> {
+    const response: { [key: string]: JSONType } =
+      (await this.deku.getVmState()) as { [key: string]: JSONType };
     if (response === null) return null;
     const state = response[this.address];
-    if (state === null || state === undefined) return null;
-    const slashRemoved = state.replaceAll('\\"', '"');
-    return JSON.parse(slashRemoved);
+    if (state === null) return null;
+    return state as { [key: string]: JSONType };
   }
 
   /**
