07e11b31e61689005128a807301e79cfb89f96e0
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-12-08 11:42:03 +0100

    refacto: yojson is no more needed in the stdlib

--- deku-p/src/core/stdlib/uri_ext.mli
+++ deku-p/src/core/stdlib/uri_ext.mli
@@ -2,8 +2,5 @@ include module type of struct
   include Uri
 end
 
-val t_of_yojson : Yojson.Safe.t -> t
-val yojson_of_t : t -> Yojson.Safe.t
-
 val cmdliner_converter :
   (string -> [> `Ok of t | `Error of string ]) * (Format.formatter -> t -> unit)
