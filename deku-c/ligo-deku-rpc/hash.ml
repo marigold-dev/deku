6c495ff83ed3cb02e5bedc8ecd85b2d8fac50084
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2022-12-22 11:34:02 +0100

    fix: ligo deku rpc
    Saving files to filesystem can lead to issues when the same contract is
    compiled approximately at the same time.
    
    This behaviour can happen when doing the quickstart or via the tester
    which is compiled/originated every minutes
    
    A solution is to have a unique filename even if two contracts are equal
    they will have a different filename
    
    This filename is compute the a nonce and the hash of the contract
    
    Then to avoid a a filesystem overflow the files are deleted

--- deku-c/ligo-deku-rpc/hash.ml
+++ deku-c/ligo-deku-rpc/hash.ml
@@ -2,8 +2,11 @@ let alphabet =
   Base64.make_alphabet
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+_"
 
-let make content =
+let make content nonce =
+  let nonce =
+    nonce |> string_of_int |> Hex.of_string |> Hex.to_bytes |> Cstruct.of_bytes
+  in
   let content = Cstruct.of_string content in
-  Mirage_crypto.Hash.MD5.digest content
-  |> Cstruct.to_string
+  Cstruct.append content nonce
+  |> Mirage_crypto.Hash.MD5.digest |> Cstruct.to_string
   |> Base64.encode_string ~pad:false ~alphabet
